d2289ae23dd20aba38f9ba5b8ab075404958c6ac
Robin Tail <robin_tail@me.com>
2021-06-01 21:47:15 +0200

    tsconfig: exclude -> include. Moving license tool to the corresponding dir.

--- tools/generate-license.ts
+++ tools/generate-license.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as fs from 'fs';
-import * as manifest from './package.json';
+import * as manifest from '../package.json';
 
 const ownLicense = `
 MIT License
