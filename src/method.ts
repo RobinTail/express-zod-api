453570c528c8d7f745d693a204c8bd8a7adbeb27
Robin Tail <robin_tail@me.com>
2021-06-05 23:09:03 +0200

    RouteMethods: allow multiple methods in assigned Endpoint, but at least the corresponding one.

--- src/method.ts
+++ src/method.ts
@@ -8,9 +8,34 @@ export type MethodsDefinition<M extends Method> = {
   method: M;
 };
 
+class RouteMethodsError extends Error {}
+
 export class RouteMethods {
-  constructor(public readonly methods: Partial<{
-    [K in Method]: Endpoint<any, any, any, any, K>
-  }>) {
+  constructor(public readonly methods: {
+    [K in Method]?: Endpoint<any, any, any, any, K> | Endpoint<any, any, any, any, Method>;
+  }) {
+    Object.keys(methods).forEach((key: keyof typeof methods) => {
+      if (key in methods) {
+        const endpointMethods = methods[key]?.getMethods() || [];
+        if (!endpointMethods.includes(key)) {
+          throw new RouteMethodsError(
+            `The endpoint assigned to the ${key} parameter must have at least this method in its specification.\n` +
+            'Example:\n\n' +
+            'new RouteMethods({\n' +
+            `  ${key}: endpointsFactory.build({\n` +
+            `    methods: ['${key}', ` + (
+              (methods[key]?.getMethods() || [])
+                .map((m) => `'${m}'`)
+                .join(', ')
+              || '...'
+            ) + ']\n' +
+            `    // or method: '${key}'` +
+            '    ...\n' +
+            '    })\n' +
+            '});\n'
+          );
+        }
+      }
+    });
   }
 }
