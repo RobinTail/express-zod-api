99ed81659cfbd4423f71bb1d831ddc88d2a4f385
Robin Tail <robin_tail@me.com>
2021-06-05 23:30:02 +0200

    Minor clarifications (2).

--- example/v1/index.ts
+++ example/v1/index.ts
@@ -5,8 +5,9 @@ import {setUserEndpoint} from './set-user';
 export const v1Routing: Routing = {
   // syntax 1: methods are defined within the endpoint
   getUser: getUserEndpoint,
+
   // syntax 2: methods are defined within the route
   setUser: new RouteMethods({
-    post: setUserEndpoint // the endpoint should have the same method specified in .build()
+    post: setUserEndpoint // the Endpoint should have at least the same method specified in .build()
   })
 };
