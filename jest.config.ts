c83d990cfe551b24af79aa467045e6e57a28e0c9
Robin Tail <robin_tail@me.com>
2021-06-13 10:45:12 +0200

    Combine tests and collect coverage.

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,8 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
+  forceExit: true,
+  collectCoverage: true,
 };
 
 export default config;
