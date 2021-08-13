e94b570f8a0bc84472aba72b9076d254eb9aabda
Robin Tail <robin_tail@me.com>
2021-08-13 19:18:14 +0200

    Tests for createConfig().

---
+++ tests/unit/config-type.spec.ts
@@ -0,0 +1,34 @@
+import {Express} from 'express';
+import {createConfig} from '../../src';
+
+describe('ConfigType', () => {
+  describe('createConfig()', () => {
+    test('should create a config with server', () => {
+      const argument = {
+        server: {
+          listen: 3333,
+        },
+        cors: true,
+        logger: {
+          level: 'debug' as const,
+          color: false,
+        }
+      };
+      const config = createConfig(argument);
+      expect(config).toEqual(argument);
+    });
+
+    test('should create a config with app', () => {
+      const argument = {
+        app: jest.fn() as unknown as Express,
+        cors: true,
+        logger: {
+          level: 'debug' as const,
+          color: false,
+        }
+      };
+      const config = createConfig(argument);
+      expect(config).toEqual(argument);
+    });
+  });
+});
