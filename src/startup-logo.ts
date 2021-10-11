e131d52b66dc42124e6dd10a8205ee40ba17b5c1
Robin Tail <robin_tail@me.com>
2021-10-11 03:11:32 +0200

    Doing the same without chalk.

--- src/startup-logo.ts
+++ src/startup-logo.ts
@@ -1,32 +1,23 @@
-import chalk from 'chalk';
-
 // ANSI font attribution
 // Colossal.flf (Jonathon - jon@mq.edu.au), 8 June 1994
 
 export const getStartupLogo = () => {
-  const proud = chalk.italic('Proudly supports transgender community.');
-  const slogan = chalk.italic('Start your API server with I/O schema validation and custom middlewares in minutes.');
-  const thanks = chalk.italic('Thank you for choosing Express Zod API for your project.');
-
-  return [
-    chalk.blue(`\n
-8888888888                                                          8888888888P              888             d8888 8888888b. 8888888 
-888                                                                       d88P               888            d88888 888   Y88b  888`),
-    chalk.magentaBright(`
-888                                                                      d88P                888           d88P888 888    888  888
-8888888    888  888 88888b.  888d888 .d88b.  .d8888b  .d8888b           d88P    .d88b.   .d88888          d88P 888 888   d88P  888`),
-    chalk.white(`
-888        \`Y8bd8P' 888 "88b 888P"  d8P  Y8b 88K      88K              d88P    d88""88b d88" 888         d88P  888 8888888P"   888   
-888          X88K   888  888 888    88888888 "Y8888b. "Y8888b.        d88P     888  888 888  888        d88P   888 888         888`),
-    chalk.magentaBright(`
-888        .d8""8b. 888 d88P 888    Y8b.          X88      X88       d88P      Y88..88P Y88b 888       d8888888888 888         888   
-8888888888 888  888 88888P"  888     "Y8888   88888P'  88888P'      d8888888888 "Y88P"   "Y88888      d88P     888 888       8888888`),
-    chalk.blue(`
-                    888
-                    888                                                                      ${proud}
-                    888                          ${slogan}`),
-    chalk.grey(`
-                                                                            ${thanks}`),
-    chalk.reset('\n\n')
-  ].join('');
+  return `
+[34m[39m
+[34m[39m
+[34m8888888888                                                          8888888888P              888             d8888 8888888b. 8888888 [39m
+[34m888                                                                       d88P               888            d88888 888   Y88b  888[39m[95m[39m
+[95m888                                                                      d88P                888           d88P888 888    888  888[39m
+[95m8888888    888  888 88888b.  888d888 .d88b.  .d8888b  .d8888b           d88P    .d88b.   .d88888          d88P 888 888   d88P  888[39m[37m[39m
+[37m888        \`Y8bd8P' 888 "88b 888P"  d8P  Y8b 88K      88K              d88P    d88""88b d88" 888         d88P  888 8888888P"   888   [39m
+[37m888          X88K   888  888 888    88888888 "Y8888b. "Y8888b.        d88P     888  888 888  888        d88P   888 888         888[39m[95m[39m
+[95m888        .d8""8b. 888 d88P 888    Y8b.          X88      X88       d88P      Y88..88P Y88b 888       d8888888888 888         888   [39m
+[95m8888888888 888  888 88888P"  888     "Y8888   88888P'  88888P'      d8888888888 "Y88P"   "Y88888      d88P     888 888       8888888[39m[34m[39m
+[34m                    888[39m
+[34m                    888                                                                      [3mProudly supports transgender community.[23m[39m
+[34m                    888                          [3mStart your API server with I/O schema validation and custom middlewares in minutes.[23m[39m[90m[39m
+[90m                                                                            [3mThank you for choosing Express Zod API for your project.[23m[39m[0m[0m
+[0m[0m
+[0m[0m
+`.trim();
 };
