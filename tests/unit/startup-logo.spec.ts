50eb6c57db1389352cef4754c79c8d40510bcce4
Robin Tail <robin_tail@me.com>
2021-10-11 02:42:46 +0200

    Fixing the test.

--- tests/unit/startup-logo.spec.ts
+++ tests/unit/startup-logo.spec.ts
@@ -3,7 +3,7 @@ import {getStartupLogo} from '../../src/startup-logo';
 describe('Startup logo', () => {
   describe('getStartupLogo()', () => {
     test('should return the logo', () => {
-      expect(getStartupLogo()).toMatchSnapshot();
+      expect(getStartupLogo().split('\n').length).toBe(16);
     });
   });
 });
