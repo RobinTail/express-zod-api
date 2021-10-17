495d9e2ae3d6778b9aa4db4bd6bf2ae3bf799339
Robin Tail <robin_tail@me.com>
2021-10-17 13:13:18 +0200

    Single file for routing.

--- example/routing.ts
+++ example/routing.ts
@@ -1,6 +1,25 @@
-import {Routing} from '../src';
-import {v1Routing} from './v1';
+import {DependsOnMethod, Routing} from '../src';
+import {fileUploadEndpoint} from './endpoints/file-upload';
+import {getUserEndpoint} from './endpoints/get-user';
+import {sendAvatarEndpoint} from './endpoints/send-avatar';
+import {setUserEndpoint} from './endpoints/set-user';
+import {streamAvatarEndpoint} from './endpoints/stream-avatar';
 
 export const routing: Routing = {
-  v1: v1Routing
+  v1: {
+    // syntax 1: methods are defined within the endpoint
+    getUser: getUserEndpoint,
+
+    // syntax 2: methods are defined within the route
+    setUser: new DependsOnMethod({
+      post: setUserEndpoint // the Endpoint should have at least the same method specified in .build()
+    }),
+
+    // custom result handler examples with a file serving
+    avatar: sendAvatarEndpoint,
+    stream: streamAvatarEndpoint,
+
+    // file upload
+    upload: fileUploadEndpoint,
+  }
 };
