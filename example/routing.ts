701c286d1ef73e621e357abe69c87ee46c03f780
Robin Tail <robin_tail@me.com>
2021-10-17 20:07:43 +0200

    Better routing in example.

--- example/routing.ts
+++ example/routing.ts
@@ -6,20 +6,24 @@ import {setUserEndpoint} from './endpoints/set-user';
 import {streamAvatarEndpoint} from './endpoints/stream-avatar';
 
 export const routing: Routing = {
+  // syntax 1: methods are defined within the endpoint
   v1: {
-    // syntax 1: methods are defined within the endpoint
-    getUser: getUserEndpoint,
-
-    // syntax 2: methods are defined within the route
-    setUser: new DependsOnMethod({
-      post: setUserEndpoint // the Endpoint should have at least the same method specified in .build()
-    }),
+    user: {
+      retrieve: getUserEndpoint, // path: /v1/user/retrieve
+    },
 
     // custom result handler examples with a file serving
-    avatar: sendAvatarEndpoint,
-    stream: streamAvatarEndpoint,
+    avatar: {
+      send: sendAvatarEndpoint,
+      stream: streamAvatarEndpoint,
+      upload: fileUploadEndpoint, // file upload example
+    },
+  },
 
-    // file upload
-    upload: fileUploadEndpoint,
+  // syntax 2: methods are defined within the route
+  v2: {
+    user: new DependsOnMethod({
+      post: setUserEndpoint // the Endpoint should have at least the same method specified in .build()
+    }),
   }
 };
--- tests/system/example.spec.ts
+++ tests/system/example.spec.ts
@@ -35,7 +35,7 @@ describe('Example', () => {
     });
 
     test('Should handle OPTIONS request', async () => {
-      const response = await fetch('http://localhost:8090/v1/setUser?test=123&id=50', {
+      const response = await fetch('http://localhost:8090/v2/user?test=123&id=50', {
         method: 'OPTIONS',
       });
       expect(response.status).toBe(200);
@@ -51,7 +51,7 @@ describe('Example', () => {
     });
 
     test('Should handle valid POST request', async () => {
-      const response = await fetch('http://localhost:8090/v1/setUser?test=123&id=50', {
+      const response = await fetch('http://localhost:8090/v2/user?test=123&id=50', {
         method: 'POST',
         headers: {
           token: '456',
@@ -72,13 +72,13 @@ describe('Example', () => {
           timestamp: expect.any(Number)
         }
       });
-      await waitFor(() => /v1\/setUser/.test(out));
+      await waitFor(() => /v2\/user/.test(out));
       await waitFor(() => /50, 123, 456/.test(out));
       expect(true).toBeTruthy();
     });
 
     test('Should handle valid GET request', async () => {
-      const response = await fetch('http://localhost:8090/v1/getUser?test=123&id=50');
+      const response = await fetch('http://localhost:8090/v1/user/retrieve?test=123&id=50');
       expect(response.status).toBe(200);
       const json = await response.json();
       expect(json).toEqual({
@@ -88,13 +88,13 @@ describe('Example', () => {
           name: 'John Doe'
         }
       });
-      await waitFor(() => /v1\/getUser/.test(out));
+      await waitFor(() => /v1\/user\/retrieve/.test(out));
       await waitFor(() => /50, method get/.test(out));
       expect(true).toBeTruthy();
     });
 
     test('Should send an image with a correct header', async () => {
-      const response = await fetch('http://localhost:8090/v1/avatar?userId=123');
+      const response = await fetch('http://localhost:8090/v1/avatar/send?userId=123');
       expect(response.status).toBe(200);
       expect(response.headers.has('Content-type')).toBeTruthy();
       expect(response.headers.get('Content-type')).toBe('image/svg+xml; charset=utf-8');
@@ -104,7 +104,7 @@ describe('Example', () => {
     });
 
     test('Should stream an image with a correct header', async () => {
-      const response = await fetch('http://localhost:8090/v1/stream?userId=123');
+      const response = await fetch('http://localhost:8090/v1/avatar/stream?userId=123');
       expect(response.status).toBe(200);
       expect(response.headers.has('Content-type')).toBeTruthy();
       expect(response.headers.get('Content-type')).toBe('image/svg+xml');
@@ -124,7 +124,7 @@ describe('Example', () => {
       data.append('arr[0]', 456);
       data.append('arr[1]', 789);
       data.append('obj[some]', 'thing');
-      const response = await fetch('http://localhost:8090/v1/upload', {
+      const response = await fetch('http://localhost:8090/v1/avatar/upload', {
         method: 'POST',
         headers: {
           'Content-Type': `${mimeMultipart}; boundary=${data.getBoundary()}`,
@@ -138,14 +138,14 @@ describe('Example', () => {
 
   describe('Negative', () => {
     test('GET request should fail on missing input param', async () => {
-      const response = await fetch('http://localhost:8090/v1/getUser?test=123');
+      const response = await fetch('http://localhost:8090/v1/user/retrieve?test=123');
       expect(response.status).toBe(400);
       const json = await response.json();
       expect(json).toMatchSnapshot();
     });
 
     test('GET request should fail on specific value in handler implementation', async () => {
-      const response = await fetch('http://localhost:8090/v1/getUser?test=123&id=101');
+      const response = await fetch('http://localhost:8090/v1/user/retrieve?test=123&id=101');
       expect(response.status).toBe(404);
       const json = await response.json();
       expect(json).toEqual({
@@ -159,7 +159,7 @@ describe('Example', () => {
     });
 
     test('POST request should fail on auth middleware key check', async () => {
-      const response = await fetch('http://localhost:8090/v1/setUser?test=123&id=50', {
+      const response = await fetch('http://localhost:8090/v2/user', {
         method: 'POST',
         headers: {
           token: '456',
@@ -182,7 +182,7 @@ describe('Example', () => {
     });
 
     test('POST request should fail on auth middleware token check', async () => {
-      const response = await fetch('http://localhost:8090/v1/setUser?test=123&id=50', {
+      const response = await fetch('http://localhost:8090/v2/user', {
         method: 'POST',
         headers: {
           token: '123',
@@ -205,7 +205,7 @@ describe('Example', () => {
     });
 
     test('POST request should fail on schema validation', async () => {
-      const response = await fetch('http://localhost:8090/v1/setUser?test=123&id=50', {
+      const response = await fetch('http://localhost:8090/v2/user', {
         method: 'POST',
         headers: {
           token: '456',
@@ -223,7 +223,7 @@ describe('Example', () => {
     });
 
     test('POST request should fail on specific value in handler implementation', async () => {
-      const response = await fetch('http://localhost:8090/v1/setUser?test=123&id=50', {
+      const response = await fetch('http://localhost:8090/v2/user', {
         method: 'POST',
         headers: {
           token: '456',
