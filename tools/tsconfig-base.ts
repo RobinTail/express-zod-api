c38070eca5ac7dce9bb3ec43eb5471b49f105dda
Robin Tail <robin_tail@me.com>
2022-01-03 21:03:29 +0100

    Minor: todo in tool.

--- tools/tsconfig-base.ts
+++ tools/tsconfig-base.ts
@@ -1,4 +1,4 @@
 export const getTSConfigBase = () => {
   const nodeVersion = process.versions.node.split(".").shift();
-  return nodeVersion === "17" ? "16" : nodeVersion;
+  return nodeVersion === "17" ? "16" : nodeVersion; // @todo remove this when @tsconfig/node17 will be released
 };
