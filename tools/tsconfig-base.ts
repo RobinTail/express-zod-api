4df4fb8ac751307dc38734d6012db0c177a9c7de
Anna Bocharova <robin_tail@me.com>
2022-01-07 21:17:37 +0100

    Supporting Node 17 (#263)
    * Enabling and testing Node 17.

--- tools/tsconfig-base.ts
+++ tools/tsconfig-base.ts
@@ -1,3 +1,4 @@
 export const getTSConfigBase = () => {
-  return process.versions.node.split(".").shift();
+  const nodeVersion = process.versions.node.split(".").shift();
+  return nodeVersion === "17" ? "16" : nodeVersion; // @todo remove this when @tsconfig/node17 will be released
 };
