ad67a02000aeb39b948e3a1f217c5c579f86cc35
Robin Tail <robin_tail@me.com>
2022-01-22 17:56:01 +0100

    Converting to classes.

--- src/date-io-schema.ts
+++ src/date-io-schema.ts
@@ -1,34 +1,107 @@
-import { ZodDate, ZodString } from "zod";
+import {
+  addIssueToContext,
+  INVALID,
+  ParseInput,
+  ParseReturnType,
+  ZodIssueCode,
+  ZodParsedType,
+  ZodType,
+  ZodTypeDef,
+} from "zod";
 
-type RawCreateParams = Parameters<typeof ZodString.create>[0];
-type DateInCreateParams = RawCreateParams & {
-  invalidDateFormatErrorMessage?: string; // default is "Invalid date format"
-  invalidDateErrorMessage?: string; // default is "Invalid date"
+// simple regex for ISO date, supports the following formats:
+// 2021-01-01T00:00:00.000Z
+// 2021-01-01T00:00:00Z
+// 2021-01-01T00:00:00
+// 2021-01-01
+const isoDateRegex = /^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}:\d{2}(\.\d{3})?)?Z?$/;
+
+const zodDateInKind = "ZodDateIn";
+declare type ZodDateInCheck = {
+  kind: "iso";
+  message?: string;
 };
-type DateOutCreateParams = RawCreateParams & {
-  transformer?: (date: Date) => string; // default is .toISOString()
-};
 
-export const dateIn = (params: DateInCreateParams) =>
-  ZodString.create(params)
-    // simple regex for ISO date, supports the following formats:
-    // 2021-01-01T00:00:00.000Z
-    // 2021-01-01T00:00:00Z
-    // 2021-01-01T00:00:00
-    // 2021-01-01
-    .regex(
-      /^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}:\d{2}(\.\d{3})?)?Z?$/,
-      params.invalidDateFormatErrorMessage || "Invalid date format"
-    )
-    // transforming the incoming string to a Date
-    .transform((str) => new Date(str))
-    // checking that the date is valid
-    .refine(
-      (date) => !isNaN(date.getTime()),
-      params.invalidDateErrorMessage || "Invalid date"
-    );
+export interface ZodDateInDef extends ZodTypeDef {
+  typeName: typeof zodDateInKind;
+  checks: ZodDateInCheck[];
+}
+export class ZodDateIn extends ZodType<Date, ZodDateInDef, string> {
+  _parse(input: ParseInput): ParseReturnType<Date> {
+    const { status, ctx } = this._processInputParams(input);
+    if (ctx.parsedType !== ZodParsedType.string) {
+      addIssueToContext(ctx, {
+        code: ZodIssueCode.invalid_type,
+        expected: ZodParsedType.string,
+        received: ctx.parsedType,
+      });
+      return INVALID;
+    }
 
-export const dateOut = (params: DateOutCreateParams) =>
-  ZodDate.create(params).transform(
-    params.transformer || ((date) => date.toISOString())
-  );
+    for (const check of this._def.checks) {
+      if (check.kind === "iso") {
+        if (!isoDateRegex.test(ctx.data)) {
+          addIssueToContext(ctx, {
+            code: ZodIssueCode.custom,
+            message: check.message,
+          });
+          status.dirty();
+        }
+      }
+    }
+
+    const date = new Date(ctx.data);
+
+    if (isNaN(date.getTime())) {
+      addIssueToContext(ctx, {
+        code: ZodIssueCode.invalid_date,
+      });
+      return INVALID;
+    }
+
+    return { status: status.value, value: date };
+  }
+
+  static create = () =>
+    new ZodDateIn({
+      typeName: zodDateInKind,
+      checks: [{ kind: "iso", message: "Invalid date format" }], // @todo optional?
+    });
+}
+
+const zodDateOutKind = "ZodDateOut";
+export interface ZodDateOutDef extends ZodTypeDef {
+  typeName: typeof zodDateOutKind;
+  transformer: (date: Date) => string;
+}
+export class ZodDateOut extends ZodType<string, ZodDateOutDef, Date> {
+  _parse(input: ParseInput): ParseReturnType<string> {
+    const { status, ctx } = this._processInputParams(input);
+    if (ctx.parsedType !== ZodParsedType.date) {
+      addIssueToContext(ctx, {
+        code: ZodIssueCode.invalid_type,
+        expected: ZodParsedType.date,
+        received: ctx.parsedType,
+      });
+      return INVALID;
+    }
+
+    try {
+      return { status: status.value, value: this._def.transformer(ctx.data) };
+    } catch (e) {
+      if (e instanceof Error) {
+        addIssueToContext(ctx, {
+          code: ZodIssueCode.custom,
+          message: e.message,
+        });
+      }
+      return INVALID;
+    }
+  }
+
+  static create = () =>
+    new ZodDateOut({
+      typeName: zodDateOutKind,
+      transformer: (date) => date.toISOString(),
+    });
+}
--- src/extend-zod.ts
+++ src/extend-zod.ts
@@ -1,4 +1,5 @@
 import { ZodDate } from "zod";
+import { ZodDateIn, ZodDateOut } from "./date-io-schema";
 import { ZodFile } from "./file-schema";
 import { ZodUpload } from "./upload-schema";
 
@@ -8,5 +9,5 @@ export const upload = ZodUpload.create;
 
 /** @deprecated Please use z.dateIn() or z.dateOut() */
 export const date = ZodDate.create;
-
-export { dateIn, dateOut } from "./date-io-schema";
+export const dateIn = ZodDateIn.create;
+export const dateOut = ZodDateOut.create;
