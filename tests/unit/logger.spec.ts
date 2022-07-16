a70f26f678a9501f94a2d7357f1a4194ea33bbc7
Robin Tail <robin_tail@me.com>
2022-07-16 14:53:28 +0200

    Asserting call params in test.

--- tests/unit/logger.spec.ts
+++ tests/unit/logger.spec.ts
@@ -19,6 +19,7 @@ describe("Logger", () => {
         color: false,
       };
       const logger = createLogger(loggerConfig);
+      const transform = jest.spyOn(logger.transports[0].format!, "transform");
       expect(logger.silent).toBeTruthy();
       expect(logger.isErrorEnabled()).toBeTruthy();
       expect(logger.isWarnEnabled()).toBeTruthy();
@@ -26,6 +27,8 @@ describe("Logger", () => {
       expect(logger.isVerboseEnabled()).toBeFalsy();
       expect(logger.isDebugEnabled()).toBeFalsy();
       expect(logger.isSillyEnabled()).toBeFalsy();
+      logger.error("test");
+      expect(transform).toBeCalledTimes(0);
     });
 
     test("Should create warn logger", () => {
@@ -42,19 +45,18 @@ describe("Logger", () => {
       expect(logger.isDebugEnabled()).toBeFalsy();
       expect(logger.isSillyEnabled()).toBeFalsy();
       logger.warn("testing warn message", { withMeta: true });
-      expect(transform).toHaveBeenCalledWith(
-        {
-          level: "warn",
-          [LEVEL]: "warn",
-          timestamp: "2022-01-01T00:00:00.000Z",
-          [SPLAT]: [{ withMeta: true }],
-          withMeta: true,
-          message: "testing warn message",
-          [MESSAGE]:
-            '2022-01-01T00:00:00.000Z warn: testing warn message {"withMeta":true}',
-        },
-        {}
-      );
+      expect(transform).toBeCalled();
+      const params = transform.mock.calls[0][0];
+      expect(params).toEqual({
+        level: "warn",
+        [LEVEL]: "warn",
+        timestamp: "2022-01-01T00:00:00.000Z",
+        [SPLAT]: [{ withMeta: true }],
+        withMeta: true,
+        message: "testing warn message",
+        [MESSAGE]:
+          '2022-01-01T00:00:00.000Z warn: testing warn message {"withMeta":true}',
+      });
     });
 
     test("Should create debug logger", () => {
@@ -71,22 +73,19 @@ describe("Logger", () => {
       expect(logger.isDebugEnabled()).toBeTruthy();
       expect(logger.isSillyEnabled()).toBeFalsy();
       logger.debug("testing debug message", { withColorful: "output" });
-      expect(transform).toHaveBeenCalledWith(
-        {
-          level: chalk.blue("debug"),
-          [LEVEL]: "debug",
-          timestamp: "2022-01-01T00:00:00.000Z",
-          [SPLAT]: [{ withColorful: "output" }],
-          withColorful: "output",
-          message: "testing debug message",
-          [MESSAGE]: `2022-01-01T00:00:00.000Z ${chalk.blue(
-            "debug"
-          )}: testing debug message { withColorful: ${chalk.green(
-            "'output'"
-          )} }`,
-        },
-        {}
-      );
+      expect(transform).toBeCalled();
+      const params = transform.mock.calls[0][0];
+      expect(params).toEqual({
+        level: chalk.blue("debug"),
+        [LEVEL]: "debug",
+        timestamp: "2022-01-01T00:00:00.000Z",
+        [SPLAT]: [{ withColorful: "output" }],
+        withColorful: "output",
+        message: "testing debug message",
+        [MESSAGE]: `2022-01-01T00:00:00.000Z ${chalk.blue(
+          "debug"
+        )}: testing debug message { withColorful: ${chalk.green("'output'")} }`,
+      });
     });
 
     test("Should manage profiling", () => {
@@ -99,19 +98,18 @@ describe("Logger", () => {
       logger.profile("long-test");
       MockDate.set("2022-01-01T00:00:00.554Z");
       logger.profile("long-test");
-      expect(transform).toHaveBeenCalledWith(
-        {
-          durationMs: 554,
-          level: chalk.green("info"),
-          [LEVEL]: "info",
-          timestamp: "2022-01-01T00:00:00.554Z",
-          message: "long-test",
-          [MESSAGE]: `2022-01-01T00:00:00.554Z ${chalk.green(
-            "info"
-          )}: long-test duration: 554ms`,
-        },
-        {}
-      );
+      expect(transform).toBeCalled();
+      const params = transform.mock.calls[0][0];
+      expect(params).toEqual({
+        durationMs: 554,
+        level: chalk.green("info"),
+        [LEVEL]: "info",
+        timestamp: "2022-01-01T00:00:00.554Z",
+        message: "long-test",
+        [MESSAGE]: `2022-01-01T00:00:00.554Z ${chalk.green(
+          "info"
+        )}: long-test duration: 554ms`,
+      });
     });
 
     test("Should handle empty message", () => {
@@ -128,18 +126,17 @@ describe("Logger", () => {
       expect(logger.isDebugEnabled()).toBeTruthy();
       expect(logger.isSillyEnabled()).toBeFalsy();
       logger.error({ someData: "test" });
-      expect(transform).toHaveBeenCalledWith(
-        {
-          level: chalk.red("error"),
-          [LEVEL]: "error",
-          timestamp: "2022-01-01T00:00:00.000Z",
-          message: { someData: "test" },
-          [MESSAGE]: `2022-01-01T00:00:00.000Z ${chalk.red(
-            "error"
-          )}: [No message] { someData: ${chalk.green("'test'")} }`,
-        },
-        {}
-      );
+      expect(transform).toBeCalled();
+      const params = transform.mock.calls[0][0];
+      expect(params).toEqual({
+        level: chalk.red("error"),
+        [LEVEL]: "error",
+        timestamp: "2022-01-01T00:00:00.000Z",
+        message: { someData: "test" },
+        [MESSAGE]: `2022-01-01T00:00:00.000Z ${chalk.red(
+          "error"
+        )}: [No message] { someData: ${chalk.green("'test'")} }`,
+      });
     });
   });
 });
