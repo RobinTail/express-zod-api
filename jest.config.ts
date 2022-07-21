93c17085535ee561601fe41fadac772f5f9f16bf
Robin Tail <robin_tail@me.com>
2022-07-21 20:05:54 +0200

    Configuring ts-jest for ESM.

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-  preset: "ts-jest",
   testEnvironment: "node",
   verbose: true,
   forceExit: true,
@@ -9,6 +8,17 @@ const config: Config.InitialOptions = {
   collectCoverageFrom: ["src/**"],
   coverageReporters: ["json-summary", "text", "html", "lcov"],
   testTimeout: 10000,
+
+  // ts-jest ESM support:
+  preset: "ts-jest/presets/default-esm",
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
 
 export default config;
