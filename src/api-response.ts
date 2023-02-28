662fb8c73c56bd9a736f1e676fcf9ada1bcf72d7
Robin Tail <robin_tail@me.com>
2023-02-28 21:36:08 +0100

    Introducing the overload for createApiResponse().

--- src/api-response.ts
+++ src/api-response.ts
@@ -6,12 +6,40 @@ export type ApiResponse<A = z.ZodTypeAny> = {
   mimeTypes: string[];
 };
 
-export const createApiResponse = <S extends z.ZodTypeAny>(
+interface ApiResponseCreationProps<S extends z.ZodTypeAny> {
+  schema: S;
+  mimeTypes: MimeDefinition;
+}
+
+/**
+ * @deprecated Use the overload below that accepts object
+ * @todo remove in v9
+ * */
+export function createApiResponse<S extends z.ZodTypeAny>(
   schema: S,
-  mimeTypes: MimeDefinition = mimeJson
-) => {
+  mimeTypes?: MimeDefinition
+): ApiResponse<S>;
+
+export function createApiResponse<S extends z.ZodTypeAny>(
+  props: ApiResponseCreationProps<S>
+): ApiResponse<S>;
+
+export function createApiResponse<S extends z.ZodTypeAny>(
+  param1: S | ApiResponseCreationProps<S>,
+  param2: MimeDefinition | undefined = mimeJson
+): ApiResponse<S> {
+  if (param1 instanceof z.ZodType) {
+    // @todo remove this block in v9
+    return {
+      schema: param1,
+      mimeTypes: typeof param2 === "string" ? [param2] : param2,
+    };
+  }
   return {
-    schema,
-    mimeTypes: typeof mimeTypes === "string" ? [mimeTypes] : mimeTypes,
-  } as ApiResponse<S>;
-};
+    schema: param1.schema,
+    mimeTypes:
+      typeof param1.mimeTypes === "string"
+        ? [param1.mimeTypes]
+        : param1.mimeTypes,
+  };
+}
