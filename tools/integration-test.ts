ad00e4486ab0579e65d3a0be579318f2852bf4a0
Robin Tail <robin_tail@me.com>
2023-03-08 21:26:01 +0100

    Add zod into ESM and intergration package generator.

--- tools/integration-test.ts
+++ tools/integration-test.ts
@@ -16,7 +16,8 @@ const packageJson = `
     "express-zod-api": "../../dist",
     "ts-node": "10.9.1",
     "typescript": "4.9.4",
-    "@types/node": "*"
+    "@types/node": "*",
+    "zod": "^3.21.4"
   }
 }
 `;
