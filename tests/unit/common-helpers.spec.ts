cdeeaa1a7f7c8a2fe89d4ba3c8601006f9316d49
Robin Tail <robin_tail@me.com>
2023-03-20 19:53:31 +0100

    Also check tuples.

--- tests/unit/common-helpers.spec.ts
+++ tests/unit/common-helpers.spec.ts
@@ -347,17 +347,17 @@ describe("Common Helpers", () => {
         )
       ).toEqual([123, 456]);
     });
-    test("Issue #892: should handle examples of arrays", () => {
-      expect(
-        getExamples(
-          withMeta(z.array(z.number().int())).example([1, 2]).example([3, 4]),
-          false
-        )
-      ).toEqual([
-        [1, 2],
-        [3, 4],
-      ]);
-    });
+    test.each([z.array(z.number().int()), z.tuple([z.number(), z.number()])])(
+      "Issue #892: should handle examples of arrays and tuples %#",
+      (schema) => {
+        expect(
+          getExamples(withMeta(schema).example([1, 2]).example([3, 4]), false)
+        ).toEqual([
+          [1, 2],
+          [3, 4],
+        ]);
+      }
+    );
   });
 
   describe("combinations()", () => {
