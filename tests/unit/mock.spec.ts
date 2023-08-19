ccf34f22d759b9bf058cea98d4d03403bf20e162
Anna Bocharova <robin_tail@me.com>
2023-08-19 13:57:04 +0200

    Adding `setHeader`, `header` and `send` methods to mocked response (#1098)
    * Adding setHeader, header and send methods to mocked response for testEndpoint().
    
    * Changelog: upcoming v11.5.0.

--- tests/unit/mock.spec.ts
+++ tests/unit/mock.spec.ts
@@ -1,8 +1,43 @@
 import { z } from "zod";
-import { defaultEndpointsFactory, testEndpoint } from "../../src";
+import {
+  createMiddleware,
+  defaultEndpointsFactory,
+  testEndpoint,
+} from "../../src";
 
 describe("Mock", () => {
   describe("testEndpoint()", () => {
+    test("Should test the endpoint", async () => {
+      const endpoint = defaultEndpointsFactory
+        .addMiddleware(
+          createMiddleware({
+            input: z.object({}),
+            middleware: async ({ response }) => {
+              response
+                .setHeader("X-Some", "header")
+                .header("X-Another", "header as well")
+                .send("this is just for testing mocked methods");
+              return {};
+            },
+          }),
+        )
+        .build({
+          method: "get",
+          input: z.object({}),
+          output: z.object({}),
+          handler: async () => ({}),
+        });
+      const { responseMock } = await testEndpoint({ endpoint });
+      expect(responseMock.setHeader).toHaveBeenCalledWith("X-Some", "header");
+      expect(responseMock.header).toHaveBeenCalledWith(
+        "X-Another",
+        "header as well",
+      );
+      expect(responseMock.send).toHaveBeenCalledWith(
+        "this is just for testing mocked methods",
+      );
+    });
+
     test("Should throw an error in case Jest is not installed", async () => {
       const endpoint = defaultEndpointsFactory.build({
         method: "get",
