e63f9843a6f25a1d8eef46707317c9b344b13a9e
Anna Bocharova <robin_tail@me.com>
2023-08-24 22:44:31 +0200

    Upgrading Typescript to 5.2. (#1118)
    * Upgrading Typescript to 5.2.
    
    * Flattening the typescript.
    
    * Fix deprecated overload.

--- tools/integration-test.ts
+++ tools/integration-test.ts
@@ -16,7 +16,7 @@ const packageJson = `
     "@tsconfig/node${tsconfigBase}": "latest",
     "express-zod-api": "../../dist",
     "ts-node": "10.9.1",
-    "typescript": "5.1.5",
+    "typescript": "5.2.2",
     "@types/node": "*"
   }
 }
