c77d074837f74986257f60e1251799bf05cdde91
Robin Tail <robin_tail@me.com>
2023-08-30 22:26:41 +0200

    Adjusting the test.

--- tests/unit/startup-logo.spec.ts
+++ tests/unit/startup-logo.spec.ts
@@ -3,8 +3,11 @@ import { getStartupLogo } from "../../src/startup-logo";
 describe("Startup logo", () => {
   describe("getStartupLogo()", () => {
     test("should return the logo", () => {
-      console.log(getStartupLogo(), getStartupLogo().split("\n"));
-      expect(getStartupLogo().split("\n").length).toBe(16);
+      const logo = getStartupLogo();
+      expect(logo.split("\n").length).toBeGreaterThanOrEqual(12);
+      expect(logo).toMatch(
+        /Thank you for choosing Express Zod API for your project/,
+      );
     });
   });
 });
