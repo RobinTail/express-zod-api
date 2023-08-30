6f676e53a9860d2082637622d4419cb8c7078ae0
Robin Tail <robin_tail@me.com>
2023-08-30 22:09:02 +0200

    Temp: debugging the CI.

--- tests/unit/startup-logo.spec.ts
+++ tests/unit/startup-logo.spec.ts
@@ -3,6 +3,7 @@ import { getStartupLogo } from "../../src/startup-logo";
 describe("Startup logo", () => {
   describe("getStartupLogo()", () => {
     test("should return the logo", () => {
+      console.log(getStartupLogo(), getStartupLogo().split("\n"));
       expect(getStartupLogo().split("\n").length).toBe(16);
     });
   });
