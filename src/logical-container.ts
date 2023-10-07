8996e00fd6328b2f9311f8b3a4234b98c94fc238
Robin Tail <robin_tail@me.com>
2023-10-07 23:37:59 +0200

    Flattening the implementation using additional helpers.

--- src/logical-container.ts
+++ src/logical-container.ts
@@ -11,11 +11,16 @@ export type LogicalContainer<T> =
 const isObject = (subject: unknown): subject is object =>
   typeof subject === "object" && subject !== null;
 
+const isLogicalOr = (subject: unknown): subject is LogicalOr<unknown> =>
+  isObject(subject) && "or" in subject;
+
+const isLogicalAnd = (subject: unknown): subject is LogicalAnd<unknown> =>
+  isObject(subject) && "and" in subject;
+
 /** @desc combines several LogicalAnds into a one */
 const flattenAnds = <T>(subject: (T | LogicalAnd<T>)[]): LogicalAnd<T> => ({
   and: subject.reduce<T[]>(
-    (agg, item) =>
-      agg.concat(isObject(item) && "and" in item ? item.and : item),
+    (agg, item) => agg.concat(isLogicalAnd(item) ? item.and : item),
     [],
   ),
 });
@@ -25,26 +30,21 @@ export const mapLogicalContainer = <T, S>(
   container: LogicalContainer<T>,
   fn: (subject: T) => S,
 ): LogicalContainer<S> => {
-  if (isObject(container)) {
-    if ("and" in container) {
-      return {
-        and: container.and.map((entry) =>
-          isObject(entry) && "or" in entry
-            ? { or: entry.or.map(fn) }
-            : fn(entry),
-        ),
-      };
-    }
-    if ("or" in container) {
-      return {
-        or: container.or.map((entry) =>
-          isObject(entry) && "and" in entry
-            ? { and: entry.and.map(fn) }
-            : fn(entry),
-        ),
-      };
-    }
+  if (isLogicalAnd(container)) {
+    return {
+      and: container.and.map((entry) =>
+        isLogicalOr(entry) ? { or: entry.or.map(fn) } : fn(entry),
+      ),
+    };
   }
+  if (isLogicalOr(container)) {
+    return {
+      or: container.or.map((entry) =>
+        isLogicalAnd(entry) ? { and: entry.and.map(fn) } : fn(entry),
+      ),
+    };
+  }
+
   return fn(container);
 };
 
@@ -54,10 +54,7 @@ export const andToOr = <T>(
 ): LogicalOr<T | LogicalAnd<T>> => {
   return subject.and.reduce<LogicalOr<T | LogicalAnd<T>>>(
     (acc, item) => {
-      const combs = combinations(
-        acc.or,
-        isObject(item) && "or" in item ? item.or : [item],
-      );
+      const combs = combinations(acc.or, isLogicalOr(item) ? item.or : [item]);
       if (combs.type === "single") {
         acc.or.push(...combs.value);
       } else {
@@ -76,38 +73,28 @@ export const combineContainers = <T>(
   a: LogicalContainer<T>,
   b: LogicalContainer<T>,
 ): LogicalContainer<T> => {
-  if (isObject(a)) {
-    if ("and" in a) {
-      if (isObject(b)) {
-        if ("and" in b) {
-          return flattenAnds([a, b]);
-        }
-        if ("or" in b) {
-          return combineContainers(andToOr(a), b);
-        }
-      }
+  if (isLogicalAnd(a)) {
+    if (isLogicalAnd(b)) {
       return flattenAnds([a, b]);
     }
-    if ("or" in a) {
-      if (isObject(b)) {
-        if ("and" in b) {
-          return combineContainers(b, a);
-        }
-        if ("or" in b) {
-          const combs = combinations(a.or, b.or);
-          return {
-            or:
-              combs.type === "single"
-                ? combs.value
-                : combs.value.map(flattenAnds),
-          };
-        }
-      }
-      return combineContainers(a, { and: [b] });
+    if (isLogicalOr(b)) {
+      return combineContainers(andToOr(a), b);
     }
+    return flattenAnds([a, b]);
   }
 
-  if (isObject(b) && ("and" in b || "or" in b)) {
+  if (isLogicalOr(a)) {
+    if (isLogicalAnd(b)) {
+      return combineContainers(b, a);
+    }
+    if (isLogicalOr(b)) {
+      const { type, value } = combinations(a.or, b.or);
+      return { or: type === "single" ? value : value.map(flattenAnds) };
+    }
+    return combineContainers(a, { and: [b] });
+  }
+
+  if (isLogicalAnd(b) || isLogicalOr(b)) {
     return combineContainers(b, a);
   }
 
