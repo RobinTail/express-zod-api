63b4ab2df37314434f7f27cf71c06d569457bfed
Robin Tail <robin_tail@me.com>
2023-10-26 23:27:17 +0200

    Better implementation for awaiting subprocess exit.

--- tests/system/example.spec.ts
+++ tests/system/example.spec.ts
@@ -20,7 +20,7 @@ describe("Example", () => {
   afterAll(async () => {
     example.stdout.removeListener("data", listener);
     example.kill();
-    await waitFor(() => example.killed);
+    await waitFor(() => example.exitCode !== null);
   });
 
   afterEach(() => {
