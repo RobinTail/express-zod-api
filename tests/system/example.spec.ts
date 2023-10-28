1e5efe8404af12f5bcdc17c18a4e8f0834779244
Robin Tail <robin_tail@me.com>
2023-10-28 19:59:37 +0200

    Moving system example client test to system example test.

--- tests/system/example.spec.ts
+++ tests/system/example.spec.ts
@@ -1,4 +1,11 @@
 import { ChildProcessWithoutNullStreams, spawn } from "node:child_process";
+import { expectType } from "tsd";
+import { config } from "../../example/config";
+import {
+  ExpressZodAPIClient,
+  Implementation,
+  jsonEndpoints,
+} from "../../example/example.client";
 import { mimeMultipart } from "../../src/mime";
 import { waitFor } from "../helpers";
 import { createHash } from "node:crypto";
@@ -316,6 +323,42 @@ describe("Example", () => {
         "application/json; charset=utf-8",
       );
       expect(await response.json()).toMatchSnapshot();
+    });
+  });
+
+  describe("Client", () => {
+    const createDefaultImplementation =
+      (host: string): Implementation =>
+      async (method, path, params) => {
+        const searchParams =
+          method === "get" ? `?${new URLSearchParams(params)}` : "";
+        const response = await fetch(`${host}${path}${searchParams}`, {
+          method: method.toUpperCase(),
+          headers:
+            method === "get"
+              ? undefined
+              : { "Content-Type": "application/json" },
+          body: method === "get" ? undefined : JSON.stringify(params),
+        });
+        if (`${method} ${path}` in jsonEndpoints) {
+          return response.json();
+        }
+        return response.text();
+      };
+
+    const client = new ExpressZodAPIClient(
+      createDefaultImplementation(`http://localhost:${config.server.listen}`),
+    );
+
+    test("Should perform the request with a positive response", async () => {
+      const response = await client.provide("get", "/v1/user/retrieve", {
+        id: "10",
+      });
+      expect(response).toMatchSnapshot();
+      expectType<
+        | { status: "success"; data: { id: number; name: string } }
+        | { status: "error"; error: { message: string } }
+      >(response);
     });
   });
 });
