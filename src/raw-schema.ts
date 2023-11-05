9a60c3eab6f0eb391b54e8ae044ba1322cb86eb6
Robin Tail <robin_tail@me.com>
2023-11-05 21:11:44 +0100

    Ref: Removing excessive helper.

--- src/raw-schema.ts
+++ src/raw-schema.ts
@@ -16,14 +16,11 @@ export interface ZodRawDef extends ZodTypeDef {
   typeName: typeof zodRawKind;
 }
 
-const isBuffer = (subject: unknown): subject is Buffer =>
-  Buffer.isBuffer(subject);
-
 // @todo consider reusing existing ZodFile schema instead
 export class ZodRaw extends ZodType<Buffer, ZodRawDef> {
   _parse(input: ParseInput): ParseReturnType<Buffer> {
     const { ctx } = this._processInputParams(input);
-    if (ctx.parsedType !== ZodParsedType.object || !isBuffer(ctx.data)) {
+    if (ctx.parsedType !== ZodParsedType.object || !Buffer.isBuffer(ctx.data)) {
       addIssueToContext(ctx, {
         code: ZodIssueCode.custom,
         message: `Expected raw data (Buffer), received ${ctx.parsedType}`,
