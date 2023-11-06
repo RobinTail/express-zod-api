194d4dde70be1f6aa56fdbda4f1ecd1259b98e57
Robin Tail <robin_tail@me.com>
2023-11-06 07:48:34 +0100

    z.raw() shorthand.

--- example/endpoints/accept-raw.ts
+++ example/endpoints/accept-raw.ts
@@ -5,7 +5,7 @@ import { taggedEndpointsFactory } from "../factories";
 export const rawAcceptingEndpoint = taggedEndpointsFactory.build({
   method: "post",
   tag: "files",
-  input: z.object({ raw: ez.file().buffer() }), // requires to enable raw option in server config
+  input: ez.raw(), // requires to enable raw option in server config
   output: z.object({ length: z.number() }),
   handler: async ({ input: { raw } }) => ({
     length: raw.length,
