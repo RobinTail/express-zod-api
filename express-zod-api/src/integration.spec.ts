3739de9c76b2eeed0ef05face5e8b9d21aec0776
Robin Tail <robin_tail@me.com>
2023-12-04 19:34:47 +0100

    Restoring coverage for Integration in sources test.

--- express-zod-api/src/integration.spec.ts
+++ express-zod-api/src/integration.spec.ts
@@ -2,12 +2,39 @@ import { z } from "zod";
 import { Integration, defaultEndpointsFactory } from "./index";
 
 describe("API Integration Generator", () => {
+  test("Should support types variant and handle recirsive schemas", () => {
+    const recursiveSchema: z.ZodTypeAny = z.lazy(() =>
+      z.object({
+        name: z.string(),
+        features: recursiveSchema,
+      }),
+    );
+
+    const client = new Integration({
+      variant: "types",
+      routing: {
+        v1: {
+          test: defaultEndpointsFactory.build({
+            method: "post",
+            input: z.object({
+              features: recursiveSchema,
+            }),
+            output: z.object({}),
+            handler: async () => ({}),
+          }),
+        },
+      },
+    });
+    expect(client.print()).toMatchSnapshot();
+  });
+
   test("Should treat optionals the same way as z.infer() by default", () => {
     const client = new Integration({
       routing: {
         v1: {
           "test-with-dashes": defaultEndpointsFactory.build({
             method: "post",
+            tags: ["one", "two"],
             input: z.object({
               opt: z.string().optional(),
             }),
