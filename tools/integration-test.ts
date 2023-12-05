56579f90a3e4ae3f622b75e8d9338d7b5da5fcfa
Robin Tail <robin_tail@me.com>
2023-12-05 21:49:16 +0100

    Ref: moving issue 952 test to a dedicated env. Also improving performance by using link in packages.

--- tools/integration-test.ts
+++ tools/integration-test.ts
@@ -3,9 +3,5 @@ import { extractReadmeQuickStart } from "./extract-quick-start";
 
 const quickStart = extractReadmeQuickStart();
 
-/** @link https://github.com/RobinTail/express-zod-api/issues/952 */
-const issue952 = quickStart.replace(/const/g, "export const");
-
 const dir = "./tests/integration";
 writeFileSync(`${dir}/quick-start.ts`, quickStart.trim());
-writeFileSync(`${dir}/issue952.ts`, issue952.trim());
