2ee16662df0952b4bfcfef0c2c9cdbdece93f18d
Anna Bocharova <robin_tail@me.com>
2023-12-05 20:46:14 +0100

    Ref: inclusive, stable, extendible `tsconfig`s (#1354)
    Based on my findings in #1351

--- tools/esm-test.ts
+++ tools/esm-test.ts
@@ -1,9 +1,6 @@
 import { writeFileSync } from "node:fs";
 import { givePort } from "../tests/helpers";
 import { extractReadmeQuickStart } from "./extract-quick-start";
-import { getTSConfigBase } from "./tsconfig-base";
-
-const tsconfigBase = getTSConfigBase();
 
 const packageJson = `
 {
@@ -14,22 +11,11 @@ const packageJson = `
   },
   "type": "module",
   "dependencies": {
-    "@tsconfig/node${tsconfigBase}": "latest",
     "express-zod-api": "../../dist",
     "@swc/core": "^1.3.92",
     "@swc-node/register": "^1.6.8",
     "typescript": "^5.2.2",
     "@types/node": "*"
-  }
-}
-`;
-
-const tsConfigJson = `
-{
-  "extends": "@tsconfig/node${tsconfigBase}/tsconfig.json",
-  "compilerOptions": {
-    "module": "ES2022",
-    "moduleResolution": "Bundler"
   }
 }
 `;
@@ -41,5 +27,4 @@ const quickStart = extractReadmeQuickStart().replace(
 
 const dir = "./tests/esm";
 writeFileSync(`${dir}/package.json`, packageJson.trim());
-writeFileSync(`${dir}/tsconfig.json`, tsConfigJson.trim());
 writeFileSync(`${dir}/quick-start.ts`, quickStart.trim());
--- tools/integration-test.ts
+++ tools/integration-test.ts
@@ -1,8 +1,5 @@
 import { writeFileSync } from "node:fs";
 import { extractReadmeQuickStart } from "./extract-quick-start";
-import { getTSConfigBase } from "./tsconfig-base";
-
-const tsconfigBase = getTSConfigBase();
 
 const packageJson = `
 {
@@ -12,19 +9,12 @@ const packageJson = `
     "postinstall": "cp dist.package.json node_modules/express-zod-api/package.json"
   },
   "dependencies": {
-    "@tsconfig/node${tsconfigBase}": "latest",
     "express-zod-api": "../../dist",
     "@swc/core": "^1.3.92",
     "@swc-node/register": "^1.6.8",
     "typescript": "^5.2.2",
     "@types/node": "*"
   }
-}
-`;
-
-const tsConfigJson = `
-{
-  "extends": "@tsconfig/node${tsconfigBase}/tsconfig.json"
 }
 `;
 
@@ -35,6 +25,5 @@ const issue952 = quickStart.replace(/const/g, "export const");
 
 const dir = "./tests/integration";
 writeFileSync(`${dir}/package.json`, packageJson.trim());
-writeFileSync(`${dir}/tsconfig.json`, tsConfigJson.trim());
 writeFileSync(`${dir}/quick-start.ts`, quickStart.trim());
 writeFileSync(`${dir}/issue952.ts`, issue952.trim());
