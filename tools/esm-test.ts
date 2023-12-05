a9b50776742ccdad64f0103ab1dbfbfae8b057c4
Robin Tail <robin_tail@me.com>
2023-12-05 19:33:00 +0100

    Removing obvious redundancies I found during working on #1351, cherry-picked.

--- tools/esm-test.ts
+++ tools/esm-test.ts
@@ -1,4 +1,4 @@
-import { readFileSync, writeFileSync } from "node:fs";
+import { writeFileSync } from "node:fs";
 import { givePort } from "../tests/helpers";
 import { extractReadmeQuickStart } from "./extract-quick-start";
 import { getTSConfigBase } from "./tsconfig-base";
@@ -34,21 +34,8 @@ const tsConfigJson = `
 }
 `;
 
-const readme = readFileSync("README.md", "utf-8");
-const quickStartSection = readme.match(/# Quick start(.+?)\n#\s[A-Z]+/s);
-
-if (!quickStartSection) {
-  throw new Error("Can not find Quick Start section");
-}
-
-const tsParts = quickStartSection[1].match(/```typescript(.+?)```/gis);
-
-if (!tsParts) {
-  throw new Error("Can not find typescript code samples");
-}
-
 const quickStart = extractReadmeQuickStart().replace(
-  /8090/g,
+  `${givePort("example")}`,
   `${givePort("esm")}`,
 );
 
