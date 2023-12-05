454febade3c04f900ef0f52a5cdd82374938ec29
Anna Bocharova <robin_tail@me.com>
2023-12-05 21:30:13 +0100

    Ref: Stable `package.json` for integration tests (#1355)
    Based on my findings in #1351

--- tools/esm-test.ts
+++ tools/esm-test.ts
@@ -2,29 +2,10 @@ import { writeFileSync } from "node:fs";
 import { givePort } from "../tests/helpers";
 import { extractReadmeQuickStart } from "./extract-quick-start";
 
-const packageJson = `
-{
-  "name": "express-zod-api-esm-test",
-  "version": "1.0.0",
-  "scripts": {
-    "postinstall": "cp dist.package.json node_modules/express-zod-api/package.json"
-  },
-  "type": "module",
-  "dependencies": {
-    "express-zod-api": "../../dist",
-    "@swc/core": "^1.3.92",
-    "@swc-node/register": "^1.6.8",
-    "typescript": "^5.2.2",
-    "@types/node": "*"
-  }
-}
-`;
-
 const quickStart = extractReadmeQuickStart().replace(
   `${givePort("example")}`,
   `${givePort("esm")}`,
 );
 
 const dir = "./tests/esm";
-writeFileSync(`${dir}/package.json`, packageJson.trim());
 writeFileSync(`${dir}/quick-start.ts`, quickStart.trim());
--- tools/integration-test.ts
+++ tools/integration-test.ts
@@ -1,29 +1,11 @@
 import { writeFileSync } from "node:fs";
 import { extractReadmeQuickStart } from "./extract-quick-start";
 
-const packageJson = `
-{
-  "name": "express-zod-api-integration-test",
-  "version": "1.0.0",
-  "scripts": {
-    "postinstall": "cp dist.package.json node_modules/express-zod-api/package.json"
-  },
-  "dependencies": {
-    "express-zod-api": "../../dist",
-    "@swc/core": "^1.3.92",
-    "@swc-node/register": "^1.6.8",
-    "typescript": "^5.2.2",
-    "@types/node": "*"
-  }
-}
-`;
-
 const quickStart = extractReadmeQuickStart();
 
 /** @link https://github.com/RobinTail/express-zod-api/issues/952 */
 const issue952 = quickStart.replace(/const/g, "export const");
 
 const dir = "./tests/integration";
-writeFileSync(`${dir}/package.json`, packageJson.trim());
 writeFileSync(`${dir}/quick-start.ts`, quickStart.trim());
 writeFileSync(`${dir}/issue952.ts`, issue952.trim());
