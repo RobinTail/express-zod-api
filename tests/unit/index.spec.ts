8d1ab95b15b5f79be51e2427b1cb50eb63d1c30e
Robin Tail <robin_tail@me.com>
2023-12-12 00:07:22 +0100

    Introducing constraints on exposed types and interfaces for Issue 952.

--- tests/unit/index.spec.ts
+++ tests/unit/index.spec.ts
@@ -1,5 +1,34 @@
+import { Express } from "express";
+import { expectType } from "tsd";
+import { z } from "zod";
 import * as entrypoint from "../../src";
+import {
+  AppConfig,
+  BasicSecurity,
+  BearerSecurity,
+  CommonConfig,
+  CookieSecurity,
+  CustomHeaderSecurity,
+  FlatObject,
+  IOSchema,
+  InputSecurity,
+  LoggerOverrides,
+  Metadata,
+  Method,
+  MiddlewareDefinition,
+  MockOverrides,
+  OAuth2Security,
+  OpenIdSecurity,
+  ResultHandlerDefinition,
+  Routing,
+  ServerConfig,
+  ZodDateInDef,
+  ZodDateOutDef,
+  ZodFileDef,
+  ZodUploadDef,
+} from "../../src";
 import { describe, expect, test } from "vitest";
+import { vi } from "vitest";
 
 describe("Index Entrypoint", () => {
   describe("exports", () => {
@@ -16,6 +45,48 @@ describe("Index Entrypoint", () => {
       } else {
         expect(entity).toMatchSnapshot();
       }
+    });
+
+    test("Issue 952, 1182, 1269: should expose certain types and interfaces", () => {
+      expectType<MockOverrides>(vi.fn());
+      expectType<Method>("get");
+      expectType<IOSchema>(z.object({}));
+      expectType<FlatObject>({});
+      expectType<LoggerOverrides>({});
+      expectType<Routing>({});
+      expectType<Metadata<z.ZodTypeAny>>({ examples: [] });
+      expectType<ZodDateInDef>({ typeName: "ZodDateIn" });
+      expectType<ZodDateOutDef>({ typeName: "ZodDateOut" });
+      expectType<ZodFileDef>({ typeName: "ZodFile", type: "" });
+      expectType<ZodUploadDef>({ typeName: "ZodUpload" });
+      expectType<CommonConfig>({ cors: true, logger: { level: "silent" } });
+      expectType<AppConfig>({
+        app: {} as Express,
+        cors: true,
+        logger: { level: "silent" },
+      });
+      expectType<ServerConfig>({
+        server: { listen: 8090 },
+        logger: { level: "silent" },
+        cors: false,
+      });
+      expectType<MiddlewareDefinition<IOSchema<"strip">, {}, {}, string>>({
+        type: "proprietary",
+        input: z.object({}),
+        middleware: vi.fn(),
+      });
+      expectType<ResultHandlerDefinition<z.ZodTypeAny, z.ZodTypeAny>>({
+        getPositiveResponse: vi.fn(),
+        getNegativeResponse: vi.fn(),
+        handler: vi.fn(),
+      });
+      expectType<BasicSecurity>({ type: "basic" });
+      expectType<BearerSecurity>({ type: "bearer" });
+      expectType<CookieSecurity>({ type: "cookie", name: "" });
+      expectType<CustomHeaderSecurity>({ type: "header", name: "" });
+      expectType<InputSecurity<string>>({ type: "input", name: "" });
+      expectType<OAuth2Security<string>>({ type: "oauth2" });
+      expectType<OpenIdSecurity>({ type: "openid", url: "" });
     });
   });
 });
