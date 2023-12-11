f2e73e57bd70ba4edaf82a64df8a3cb4865912d5
Robin Tail <robin_tail@me.com>
2023-12-11 23:35:40 +0100

    Ref: minor, comment for example entrypoint on await.

--- example/index.ts
+++ example/index.ts
@@ -2,4 +2,9 @@ import { createServer } from "../src";
 import { config } from "./config";
 import { routing } from "./routing";
 
+/**
+ * "await" is only needed for using entities retuned from this method.
+ * If you can not use await (on the top level of CJS), use IIFE wrapper:
+ * @example (async () => { await ... })()
+ * */
 await createServer(config, routing);
