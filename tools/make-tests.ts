3c79c977d06adff38dfbb25eb91fc6ea7ddac2ee
Robin Tail <robin_tail@me.com>
2023-12-12 20:08:26 +0100

    Removing special port for ESM test.

--- tools/make-tests.ts
+++ tools/make-tests.ts
@@ -1,17 +1,11 @@
 import { extractReadmeQuickStart } from "./extract-quick-start";
 import { writeFile } from "node:fs/promises";
-import { givePort } from "../tests/helpers";
 
 const quickStart = await extractReadmeQuickStart();
 
 /** @link https://github.com/RobinTail/express-zod-api/issues/952 */
 const issue952QuickStart = quickStart.replace(/const/g, "export const");
 
-const esmQuickStart = quickStart.replace(
-  `${givePort("example")}`,
-  `${givePort("esm")}`,
-);
-
 await writeFile("./tests/cjs/quick-start.ts", quickStart);
 await writeFile("./tests/issue952/quick-start.ts", issue952QuickStart);
-await writeFile("./tests/esm/quick-start.ts", esmQuickStart);
+await writeFile("./tests/esm/quick-start.ts", quickStart);
