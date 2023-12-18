c6b677a7dc79cb2b4b0fe6b50f250323dd70be3e
Robin Tail <robin_tail@me.com>
2023-12-18 11:20:02 +0100

    Generating two specifications with CI test.

--- example/generate-documentation.ts
+++ example/generate-documentation.ts
@@ -4,16 +4,32 @@ import { config } from "./config";
 import { routing } from "./routing";
 import manifest from "../package.json";
 
-const documentation = await createDocumentation({
+const common = {
   routing,
   config,
   version: manifest.version,
   title: "Example API",
   serverUrl: "https://example.com",
+};
+
+const openApi30 = await createDocumentation({
+  oas: "3.0",
+  ...common,
 });
 
 await writeFile(
-  "example/example.documentation.yaml",
+  "example/example.documentation-3.0.yaml",
+  openApi30.print(),
+  "utf-8",
+);
+
+const documentation = await createDocumentation({
+  oas: "3.1",
+  ...common,
+});
+
+await writeFile(
+  "example/example.documentation-3.1.yaml",
   documentation.print(),
   "utf-8",
 );
