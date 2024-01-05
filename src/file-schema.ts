c8df2c525ea31b9afce03933fa766c08907ee351
Robin Tail <robin_tail@me.com>
2024-01-05 13:34:09 +0100

    Ref: extracting DeprecatedMethods inteface.

--- src/file-schema.ts
+++ src/file-schema.ts
@@ -7,36 +7,41 @@ export const zodFileKind = "ZodFile";
 const base64Regex =
   /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
 
-export const file = (type?: "string" | "buffer" | "base64" | "binary") => {
+type Narrowing = "string" | "buffer" | "base64" | "binary";
+
+/** @todo remove in v17 */
+interface DeprecatedMethods extends Record<Narrowing, () => z.ZodType> {
+  /** @deprecated use ez.file("buffer") instead */
+  buffer: () => z.ZodType<Buffer>;
+  /** @deprecated use ez.file("string") instead */
+  string: () => z.ZodString;
+  /** @deprecated use ez.file("base64") instead */
+  base64: () => z.ZodString;
+  /** @deprecated use ez.file("binary") instead */
+  binary: () => z.ZodString | z.ZodType<Buffer>;
+}
+
+export const file = (type?: Narrowing) => {
   const justString = z.string();
+  const base64String = justString.regex(base64Regex, {
+    message: "Does not match base64 encoding",
+  });
   const schema = withMeta(
     type === "buffer"
       ? bufferSchema
       : type === "base64"
-        ? justString.regex(base64Regex, {
-            message: "Does not match base64 encoding",
-          })
+        ? base64String
         : type === "binary"
           ? bufferSchema.or(justString)
           : justString,
   );
   schema._def[metaProp].proprietaryKind = zodFileKind;
-  /** @todo remove in v17 */
+  /** @todo remove this hack in v17 */
   (schema as any).buffer = () => file("buffer");
   (schema as any).string = () => file("string");
   (schema as any).base64 = () => file("base64");
   (schema as any).binary = () => file("binary");
-  /** @todo remove extra methods in v17 */
-  return schema as typeof schema & {
-    /** @deprecated use ez.file("buffer") instead */
-    buffer: () => z.ZodType<Buffer>;
-    /** @deprecated use ez.file("string") instead */
-    string: () => z.ZodString;
-    /** @deprecated use ez.file("base64") instead */
-    base64: () => z.ZodString;
-    /** @deprecated use ez.file("binary") instead */
-    binary: () => z.ZodString | z.ZodType<Buffer>;
-  };
+  return schema as typeof schema & DeprecatedMethods;
 };
 
 /** Shorthand for z.object({ raw: ez.file("buffer") }) */
