a222e09338cccdea42f6b21d96cde5cb06a02451
Robin Tail <robin_tail@me.com>
2024-01-21 09:55:21 +0100

    Using flip to simplify getCustomHeaders().

--- src/common-helpers.ts
+++ src/common-helpers.ts
@@ -1,7 +1,7 @@
 import { Request } from "express";
 import { isHttpError } from "http-errors";
 import { createHash } from "node:crypto";
-import { map, mergeAll, pickBy, pipe, reject, xprod } from "ramda";
+import { flip, map, mergeAll, pickBy, pipe, reject, xprod } from "ramda";
 import { z } from "zod";
 import { CommonConfig, InputSource, InputSources } from "./config-type";
 import { InputValidationError, OutputValidationError } from "./errors";
@@ -38,7 +38,7 @@ export const isCustomHeader = (name: string): name is `x-${string}` =>
 
 /** @see https://nodejs.org/api/http.html#messageheaders */
 export const getCustomHeaders = (headers: FlatObject): FlatObject =>
-  pickBy((...args) => isCustomHeader(args[1]), headers);
+  pickBy(/* v,k =>*/ flip(/* k,v =>*/ isCustomHeader), headers);
 
 export const getInput = (
   request: Request,
