a5f6a51560005007569a9b3d9065c55cb9200b91
Anna Bocharova <robin_tail@me.com>
2024-01-26 20:21:25 +0100

    Ref: extracting examples enumerator (#1512)
    DNRY

--- src/documentation-helpers.ts
+++ src/documentation-helpers.ts
@@ -622,43 +622,35 @@ export const depictLazy: Depicter<z.ZodLazy<z.ZodTypeAny>> = ({
 export const depictRaw: Depicter<RawSchema> = ({ next, schema }) =>
   next({ schema: schema.shape.raw });
 
+const enumerateExamples = (examples: unknown[]): ExamplesObject | undefined =>
+  examples.length
+    ? zipObj(
+        range(1, examples.length + 1).map((idx) => `example${idx}`),
+        map(objOf("value"), examples),
+      )
+    : undefined;
+
 export const depictExamples = (
   schema: z.ZodTypeAny,
   isResponse: boolean,
   omitProps: string[] = [],
-): ExamplesObject | undefined => {
-  const examples = pipe(
+): ExamplesObject | undefined =>
+  pipe(
     getExamples,
     map(when((subj) => z.object({}).safeParse(subj).success, omit(omitProps))),
-    map(objOf("value")),
+    enumerateExamples,
   )({ schema, variant: isResponse ? "parsed" : "original", validate: true });
-  if (examples.length === 0) {
-    return undefined;
-  }
-  return zipObj(
-    range(1, examples.length + 1).map((idx) => `example${idx}`),
-    examples,
-  );
-};
 
 export const depictParamExamples = (
   schema: z.ZodTypeAny,
   param: string,
-): ExamplesObject | undefined => {
-  const examples = pipe(
+): ExamplesObject | undefined =>
+  pipe(
     getExamples,
     filter<FlatObject>(has(param)),
     pluck(param),
-    map(objOf("value")),
+    enumerateExamples,
   )({ schema, variant: "original", validate: true });
-  if (examples.length === 0) {
-    return undefined;
-  }
-  return zipObj(
-    range(1, examples.length + 1).map((idx) => `example${idx}`),
-    examples,
-  );
-};
 
 export const extractObjectSchema = (
   subject: IOSchema,
