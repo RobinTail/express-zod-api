a825e90e2adfda4784a03e3fdf1338d6a331d8be
Robin Tail <robin_tail@me.com>
2024-02-04 13:30:36 +0100

    Ref: flattening the implementation.

--- src/sockets.ts
+++ src/sockets.ts
@@ -35,30 +35,29 @@ export const createSockets = <
         const payload = def.output ? init(params) : params;
         const ack = def.output ? last(params) : undefined;
         const inputValidation = def.input.safeParse(payload);
-        if (inputValidation.success) {
-          logger.debug("parsed input", inputValidation.data);
-          const output = await def.handler(...inputValidation.data);
-          if (!def.output) {
-            return; // no ack
-          }
-          const outputValidation = def.output.safeParse(output);
-          if (outputValidation.success) {
-            logger.debug("parsed output", outputValidation.data);
-            // @todo use z.function() validation
-            if (typeof ack === "function") {
-              ack(outputValidation.data);
-            }
-            return;
-          }
+        if (!inputValidation.success) {
+          return logger.error(
+            `${event} payload validation error`,
+            new InputValidationError(inputValidation.error),
+          );
+        }
+        logger.debug("parsed input", inputValidation.data);
+        const output = await def.handler(...inputValidation.data);
+        if (!def.output) {
+          return; // no ack
+        }
+        const outputValidation = def.output.safeParse(output);
+        if (!outputValidation.success) {
           return logger.error(
             `${event} output validation error`,
             new OutputValidationError(outputValidation.error),
           );
         }
-        logger.error(
-          `${event} payload validation error`,
-          new InputValidationError(inputValidation.error),
-        );
+        logger.debug("parsed output", outputValidation.data);
+        // @todo use z.function() validation
+        if (typeof ack === "function") {
+          ack(outputValidation.data);
+        }
       });
     }
     socket.on("disconnect", () => {
