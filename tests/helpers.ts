b28c901555a22bee94e179ecf67b355c031928cc
Robin Tail <robin_tail@me.com>
2024-02-25 15:48:50 +0100

    Easier serialization for schemas in tests.

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,6 +1,4 @@
-import { map } from "ramda";
 import { z } from "zod";
-import { SchemaHandler, walkSchema } from "../src/schema-walker";
 
 let lastGivenPort = 8010;
 const reservedPorts = {
@@ -31,62 +29,5 @@ export const waitFor = async (cb: () => boolean) =>
     }, 100);
   });
 
-export const serializeSchemaForTest = (
-  subject: z.ZodTypeAny,
-): Record<string, any> => {
-  const onSomeUnion: SchemaHandler<
-    | z.ZodUnion<z.ZodUnionOptions>
-    | z.ZodDiscriminatedUnion<string, z.ZodDiscriminatedUnionOption<string>[]>,
-    object
-  > = ({ schema, next }) => ({
-    options: Array.from(schema.options.values()).map(next),
-  });
-  const onOptionalOrNullable: SchemaHandler<
-    z.ZodOptional<z.ZodTypeAny> | z.ZodNullable<z.ZodTypeAny>,
-    object
-  > = ({ schema, next }) => ({ value: next(schema.unwrap()) });
-  const onPrimitive = () => ({});
-  return walkSchema({
-    schema: subject,
-    rules: {
-      ZodNull: onPrimitive,
-      ZodNumber: onPrimitive,
-      ZodString: onPrimitive,
-      ZodBoolean: onPrimitive,
-      ZodUnion: onSomeUnion,
-      ZodDiscriminatedUnion: onSomeUnion,
-      ZodOptional: onOptionalOrNullable,
-      ZodNullable: onOptionalOrNullable,
-      ZodIntersection: ({ schema, next }) => ({
-        left: next(schema._def.left),
-        right: next(schema._def.right),
-      }),
-      ZodObject: ({ schema, next }) => ({ shape: map(next, schema.shape) }),
-      ZodEffects: ({ schema, next }) => ({ value: next(schema._def.schema) }),
-      ZodRecord: ({ schema, next }) => ({
-        keys: next(schema.keySchema),
-        values: next(schema.valueSchema),
-      }),
-      ZodArray: ({ schema, next }) => ({ items: next(schema.element) }),
-      ZodLiteral: ({ schema }) => ({ value: schema.value }),
-      ZodDefault: ({ schema, next }) => ({
-        value: next(schema._def.innerType),
-        default: schema._def.defaultValue(),
-      }),
-      ZodReadonly: ({ schema, next }) => next(schema._def.innerType),
-      ZodCatch: ({ schema, next }) => ({
-        value: next(schema._def.innerType),
-        fallback: schema._def.defaultValue(),
-      }),
-      ZodPipeline: ({ schema, next }) => ({
-        from: next(schema._def.in),
-        to: next(schema._def.out),
-      }),
-    },
-    onEach: ({ schema }) => ({ _type: schema._def.typeName }),
-    onMissing: ({ schema }) => {
-      console.warn(`There is no serializer for ${schema._def.typeName}`);
-      return {};
-    },
-  });
-};
+export const serializeSchemaForTest = (subject: z.ZodTypeAny) =>
+  JSON.parse(JSON.stringify(subject));
