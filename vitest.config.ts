38d42671b3e23fef301b445a0ed64c6b4b8d8741
Robin Tail <robin_tail@me.com>
2024-03-23 07:51:45 +0100

    Force color level 2 in CI.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     env: {
-      FORCE_COLOR: "1",
+      FORCE_COLOR: "2",
     },
     testTimeout: 10000,
     reporters: "basic",
