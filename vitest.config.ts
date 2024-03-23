97776233dd3e26b1e7d9c29d2a9e566ea220de8a
Robin Tail <robin_tail@me.com>
2024-03-23 11:32:19 +0100

    Removing crutches for vitest.

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    env: {
-      FORCE_COLOR: "1",
-    },
     testTimeout: 10000,
     reporters: "basic",
     coverage: {
