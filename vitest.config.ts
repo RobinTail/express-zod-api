ad593582d794d41495c7c87fdd9d3bcd482a41a2
Robin Tail <robin_tail@me.com>
2024-03-23 07:55:18 +0100

    Revert "Force color level 2 in CI."
    This reverts commit 38d42671b3e23fef301b445a0ed64c6b4b8d8741.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     env: {
-      FORCE_COLOR: "2",
+      FORCE_COLOR: "1",
     },
     testTimeout: 10000,
     reporters: "basic",
