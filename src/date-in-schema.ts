a07fa790ed6a2ca2b5de3212bdaf482604267651
Robin Tail <robin_tail@me.com>
2024-04-21 09:19:16 +0200

    Ref: flattening union for ez.dateIn().

--- src/date-in-schema.ts
+++ src/date-in-schema.ts
@@ -8,10 +8,7 @@ export const dateIn = () => {
   const base = z.string();
   const hasDateMethod = typeof base.date === "function";
   const schema = hasDateMethod
-    ? base
-        .date()
-        .or(base.datetime())
-        .or(base.datetime({ local: true }))
+    ? z.union([base.date(), base.datetime(), base.datetime({ local: true })])
     : base.regex(isoDateRegex); // @todo remove after min zod v3.23 (v19)
 
   return proprietary(
