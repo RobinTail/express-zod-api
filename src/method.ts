f39c37e18d7d9867c154042e5ea3bab0492cb1b1
Anna Bocharova <robin_tail@me.com>
2024-05-06 21:25:59 +0200

    Expressing `Method` via `IRouter` (#1735)
    This serves two purposes:
    
    - Enforcing constraints on its actual usage in `walkRouting()`,
    - Should ease adoption of the new `QUERY` method in the future, see:
      - https://github.com/expressjs/express/issues/5615
      - https://github.com/nodejs/node/issues/51562

--- src/method.ts
+++ src/method.ts
@@ -1,3 +1,13 @@
-export type Method = "get" | "post" | "put" | "delete" | "patch";
-export const methods: Method[] = ["get", "post", "put", "delete", "patch"];
-export type AuxMethod = "options";
+import type { IRouter } from "express";
+
+export const methods = [
+  "get",
+  "post",
+  "put",
+  "delete",
+  "patch",
+] satisfies Array<keyof IRouter>;
+
+export type Method = (typeof methods)[number];
+
+export type AuxMethod = Extract<keyof IRouter, "options">;
