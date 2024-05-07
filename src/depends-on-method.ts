9d5ef09bd581ee746265b2fe77ed17ac4aced457
Robin Tail <robin_tail@me.com>
2024-05-07 11:40:22 +0200

    Freezing DependsOnMethod::pairs.

--- src/depends-on-method.ts
+++ src/depends-on-method.ts
@@ -3,14 +3,16 @@ import { AbstractEndpoint } from "./endpoint";
 import { Method } from "./method";
 
 export class DependsOnMethod {
-  public readonly pairs: [Method, AbstractEndpoint][];
+  public readonly pairs: ReadonlyArray<[Method, AbstractEndpoint]>;
   public readonly firstEndpoint: AbstractEndpoint | undefined;
   public readonly siblingMethods: ReadonlyArray<Method>;
 
   constructor(endpoints: Partial<Record<Method, AbstractEndpoint>>) {
-    this.pairs = toPairs(endpoints).filter(
-      (pair): pair is [Method, AbstractEndpoint] =>
-        pair !== undefined && pair[1] !== undefined,
+    this.pairs = Object.freeze(
+      toPairs(endpoints).filter(
+        (pair): pair is [Method, AbstractEndpoint] =>
+          pair !== undefined && pair[1] !== undefined,
+      ),
     );
     this.firstEndpoint = head(this.pairs)?.[1];
     this.siblingMethods = Object.freeze(
