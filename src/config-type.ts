5f3ecb3cdf142c72440109a75e0485b7cf2d9d2d
Robin Tail <robin_tail@me.com>
2024-05-08 11:53:48 +0200

    Revert exporting ChildLoggerProvider.

--- src/config-type.ts
+++ src/config-type.ts
@@ -28,7 +28,7 @@ export type TagsConfig<TAG extends string> = Record<
   string | { description: string; url?: string }
 >;
 
-export type ChildLoggerProvider = (params: {
+type ChildLoggerProvider = (params: {
   request: Request;
   parent: AbstractLogger;
 }) => AbstractLogger | Promise<AbstractLogger>;
