d172bc7c3f6db0ba1f617a67a3a060c518219b72
Robin Tail <robin_tail@me.com>
2024-05-09 19:27:16 +0200

    Ref: unexposing Metadata (no longer needed after removing withMeta).

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,6 @@ export type { LoggerOverrides } from "./logger";
 export type { FlatObject } from "./common-helpers";
 export type { Method } from "./method";
 export type { IOSchema } from "./io-schema";
-export type { Metadata } from "./metadata";
 export type { CommonConfig, AppConfig, ServerConfig } from "./config-type";
 export type { MiddlewareDefinition } from "./middleware";
 export type { ResultHandlerDefinition } from "./result-handler";
