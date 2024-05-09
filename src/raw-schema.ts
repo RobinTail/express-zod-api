71c4aa13968561571600c761e97cfc03654bc76c
Robin Tail <robin_tail@me.com>
2024-05-09 11:22:36 +0200

    Fixed the type inferrence for extra shape in ez.raw().

--- src/raw-schema.ts
+++ src/raw-schema.ts
@@ -4,10 +4,10 @@ import { file } from "./file-schema";
 export const ezRawBrand = Symbol.for("Raw");
 
 /** Shorthand for z.object({ raw: ez.file("buffer") }) */
-export const raw = (extra?: z.ZodRawShape) =>
+export const raw = <S extends z.ZodRawShape>(extra: S = {} as S) =>
   z
     .object({ raw: file("buffer") })
-    .extend(extra || {})
+    .extend(extra)
     .brand(ezRawBrand);
 
 export type RawSchema = ReturnType<typeof raw>;
