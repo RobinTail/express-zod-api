b4acf2da0aa7ba4e91bbabc2de1807f9a4492fe1
Robin Tail <robin_tail@me.com>
2024-05-14 23:59:48 +0200

    Installing prettier config.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
     files: ["**/*.ts"],
     rules: {
