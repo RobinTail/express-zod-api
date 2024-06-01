002bd858f31f056d5f5d9ec9cb23590735640934
Robin Tail <robin_tail@me.com>
2024-06-01 14:43:51 +0200

    All imports test.

--- tests/unit/migration.spec.ts
+++ tests/unit/migration.spec.ts
@@ -8,9 +8,9 @@ describe("Migration", () => {
   test("should pass ESLint rule tester", () => {
     tester.run("v20", migration.plugins["ez-migration"].rules.v20, {
       valid: [
-        {
-          code: `import { BuiltinLogger } from "express-zod-api"`,
-        },
+        { code: `import { BuiltinLogger } from "express-zod-api"` },
+        { code: `import { ResultHandler } from "express-zod-api"` },
+        { code: `import { Middleware } from "express-zod-api"` },
       ],
       invalid: [
         {
@@ -19,6 +19,22 @@ describe("Migration", () => {
             { message: `Change import "createLogger" to "BuiltinLogger".` },
           ],
           output: `import { BuiltinLogger } from "express-zod-api"`,
+        },
+        {
+          code: `import { createResultHandler } from "express-zod-api"`,
+          errors: [
+            {
+              message: `Change import "createResultHandler" to "ResultHandler".`,
+            },
+          ],
+          output: `import { ResultHandler } from "express-zod-api"`,
+        },
+        {
+          code: `import { createMiddleware } from "express-zod-api"`,
+          errors: [
+            { message: `Change import "createMiddleware" to "Middleware".` },
+          ],
+          output: `import { Middleware } from "express-zod-api"`,
         },
       ],
     });
