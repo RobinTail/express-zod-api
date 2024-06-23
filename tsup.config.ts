48f7ccb81967bb7f181ef850168614c255aa16fa
Robin Tail <robin_tail@me.com>
2024-06-23 23:09:39 +0200

    TSUP: removing downgrade of dynamic imports in the CJS build config.

--- tsup.config.ts
+++ tsup.config.ts
@@ -15,15 +15,6 @@ export default defineConfig([
     ...commons,
     entry: ["src/index.ts"],
     esbuildOptions: (options, { format }) => {
-      options.supported = {};
-      if (format === "cjs") {
-        /**
-         * Downgrade dynamic imports for CJS even they are actually supported, but still are problematic for Jest
-         * @example jest with ts-jest
-         * @link https://github.com/evanw/esbuild/issues/2651
-         */
-        options.supported["dynamic-import"] = false;
-      }
       options.define = {
         "process.env.TSUP_BUILD": `"v${version} (${format.toUpperCase()})"`,
       };
