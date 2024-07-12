b5a33e32b7b2468d1aa4460f6adf9dee5842709d
Robin Tail <robin_tail@me.com>
2024-07-12 20:04:02 +0200

    Moving and refactoring quick start examples exracter.

--- tools/make-tests.ts
+++ tools/make-tests.ts
@@ -1,7 +1,19 @@
-import { extractReadmeQuickStart } from "./extract-quick-start";
-import { writeFile } from "node:fs/promises";
+import assert from "node:assert/strict";
+import { readFile, writeFile } from "node:fs/promises";
 
-const quickStart = await extractReadmeQuickStart();
+export const extractQuickStartFromReadme = async () => {
+  const readme = await readFile("README.md", "utf-8");
+  const quickStartSection = readme.match(/# Quick start(.+?)\n#\s[A-Z]+/s);
+  assert(quickStartSection, "Can not find Quick Start section");
+  const tsParts = quickStartSection[1].match(/```typescript(.+?)```/gis);
+  assert(tsParts, "Can not find typescript code samples");
+  return tsParts
+    .map((part) => part.split("\n").slice(1, -1).join("\n"))
+    .join("\n\n")
+    .trim();
+};
+
+const quickStart = await extractQuickStartFromReadme();
 
 /** @link https://github.com/RobinTail/express-zod-api/issues/952 */
 const issue952QuickStart = quickStart.replace(/const/g, "export const");
