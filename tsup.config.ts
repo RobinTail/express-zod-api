b1f40037d285bfeecb3eae7abf3c8e437814d3d6
Robin Tail <robin_tail@me.com>
2024-07-22 22:39:46 +0200

    Minor: jsdoc.

--- tsup.config.ts
+++ tsup.config.ts
@@ -33,6 +33,10 @@ export default defineConfig([
     ...commons,
     entry: { index: "src/migration.ts" },
     outDir: "migration",
-    cjsInterop: true, // @see https://github.com/arethetypeswrong/arethetypeswrong.github.io/blob/main/docs/problems/MissingExportEquals.md
+    /**
+     * This replaces "export { _default as default }" with "export = _default" in the CJS DTS build
+     * @link https://github.com/arethetypeswrong/arethetypeswrong.github.io/blob/main/docs/problems/MissingExportEquals.md
+     * */
+    cjsInterop: true,
   },
 ]);
