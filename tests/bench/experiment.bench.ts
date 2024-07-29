3d51250f7c794dedf7ca3c8f5157ef55096c021a
Anna Bocharova <robin_tail@me.com>
2024-07-29 22:27:07 +0200

    Performance tuning related to `ramda` (#1946)

--- tests/bench/experiment.bench.ts
+++ tests/bench/experiment.bench.ts
@@ -1,12 +1,52 @@
 import { bench, describe } from "vitest";
-import { formatDuration } from "../../src/logger-helpers";
+import { z } from "zod";
+import { depictExamples } from "../../src/documentation-helpers";
+import "../../src/zod-plugin";
 
-describe.each([0.555, 555, 55555555])("Experiment", (ms) => {
+describe.skip.each([true, false])("Experiment %s", (isResponse) => {
   bench("original", () => {
-    formatDuration(ms);
+    depictExamples(
+      z
+        .object({
+          one: z.string().transform((v) => v.length),
+          two: z.number().transform((v) => `${v}`),
+          three: z.boolean(),
+        })
+        .example({
+          one: "test",
+          two: 123,
+          three: true,
+        })
+        .example({
+          one: "test2",
+          two: 456,
+          three: false,
+        }),
+      isResponse,
+      ["three"],
+    );
   });
 
   bench("featured", () => {
-    formatDuration(ms);
+    depictExamples(
+      z
+        .object({
+          one: z.string().transform((v) => v.length),
+          two: z.number().transform((v) => `${v}`),
+          three: z.boolean(),
+        })
+        .example({
+          one: "test",
+          two: 123,
+          three: true,
+        })
+        .example({
+          one: "test2",
+          two: 456,
+          three: false,
+        }),
+      isResponse,
+      ["three"],
+    );
   });
 });
