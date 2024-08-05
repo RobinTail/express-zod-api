92bd9e449dbf6d83a48e1f0000d8baf495cf60da
Robin Tail <robin_tail@me.com>
2024-08-06 00:02:22 +0200

    Using path() to lookup optional peers.

--- eslint.config.js
+++ eslint.config.js
@@ -6,15 +6,16 @@ import prettierRules from "eslint-plugin-prettier/recommended";
 import unicornPlugin from "eslint-plugin-unicorn";
 
 import { readFile } from "node:fs/promises";
-import { reject, startsWith, partition } from "ramda";
+import { reject, startsWith, partition, path, flip } from "ramda";
 
 // @todo consider "import with" starting Node 18.20 and 20.10
 const manifest = JSON.parse(await readFile("./package.json", "utf-8"));
 
 const unlistedPeers = ["eslint", "prettier"];
 const excludeTypes = reject(startsWith("@types/"));
+const lookup = flip(path)(manifest);
 const allPeers = excludeTypes(Object.keys(manifest.peerDependencies));
-const isOptional = (name) => manifest.peerDependenciesMeta[name]?.optional;
+const isOptional = (name) => lookup(["peerDependenciesMeta", name, "optional"]);
 const [optPeers, reqPeers] = partition(isOptional, allPeers);
 const production = Object.keys(manifest.dependencies);
 const allowed = production.concat(reqPeers);
