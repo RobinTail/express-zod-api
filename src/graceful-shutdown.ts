5acba78ecd01bce16c9a3b04fb1fc9c36e5efdc8
Robin Tail <robin_tail@me.com>
2024-09-17 22:30:02 +0200

    FIX: skipping hasHttpServer() on encrypted sockets.

--- src/graceful-shutdown.ts
+++ src/graceful-shutdown.ts
@@ -1,18 +1,24 @@
 import http from "node:http";
 import https from "node:https";
-import type { Duplex } from "node:stream";
+import type { Socket } from "node:net";
+import type { TLSSocket } from "node:tls";
 import { setInterval } from "node:timers/promises";
 
 const hasResponse = (
-  socket: Duplex,
+  socket: Socket,
 ): socket is typeof socket & { _httpMessage: http.ServerResponse } =>
   "_httpMessage" in socket &&
   socket._httpMessage instanceof http.ServerResponse;
 
 const hasHttpServer = (
-  socket: Duplex,
+  socket: Socket,
 ): socket is typeof socket & { server: http.Server } =>
   "server" in socket && socket.server instanceof http.Server;
+
+const isEncrypted = (socket: Socket): socket is TLSSocket =>
+  "encrypted" in socket &&
+  typeof socket.encrypted === "boolean" &&
+  socket.encrypted;
 
 const onRequest: http.RequestListener = ({}, res) =>
   void (!res.headersSent && res.setHeader("connection", "close"));
@@ -24,11 +30,11 @@ export const graceful = ({
   server: http.Server | https.Server;
   timeout?: number;
 }) => {
-  const sockets = new Set<Duplex>();
+  const sockets = new Set<Socket>();
 
   let terminating: Promise<void> | undefined;
 
-  const onConnection = (socket: Duplex) =>
+  const onConnection = (socket: Socket) =>
     void (terminating
       ? socket.destroy()
       : sockets.add(socket.once("close", () => void sockets.delete(socket))));
@@ -42,13 +48,13 @@ export const graceful = ({
         void server.close((error) => (error ? reject(error) : resolve())),
     );
 
-  const destroySocket = (socket: Duplex) =>
+  const destroySocket = (socket: Socket) =>
     void sockets.delete(socket.destroy());
 
   const workflow = async () => {
     server.on("request", onRequest);
     for (const socket of sockets) {
-      if (hasHttpServer(socket)) {
+      if (isEncrypted(socket) || hasHttpServer(socket)) {
         if (hasResponse(socket)) {
           if (!socket._httpMessage.headersSent)
             socket._httpMessage.setHeader("connection", "close");
