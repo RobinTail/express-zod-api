448f4976bc8a5b418ee50d8a1eaf49c70d691caf
Anna Bocharova <anna.bocharova@deliveryhero.com>
2024-09-18 10:20:21 +0200

    Ref: extracting helpers, organizing.

---
+++ src/graceful-helpers.ts
@@ -0,0 +1,22 @@
+import http from "node:http";
+import type { Socket } from "node:net";
+import type { TLSSocket } from "node:tls";
+
+export const hasResponse = (
+  socket: Socket,
+): socket is typeof socket & { _httpMessage: http.ServerResponse } =>
+  "_httpMessage" in socket &&
+  socket._httpMessage instanceof http.ServerResponse;
+
+export const hasHttpServer = (
+  socket: Socket,
+): socket is typeof socket & { server: http.Server } =>
+  "server" in socket && socket.server instanceof http.Server;
+
+export const isEncrypted = (socket: Socket): socket is TLSSocket =>
+  "encrypted" in socket &&
+  typeof socket.encrypted === "boolean" &&
+  socket.encrypted;
+
+export const weAreClosed: http.RequestListener = ({}, res) =>
+  void (!res.headersSent && res.setHeader("connection", "close"));
--- src/graceful-shutdown.ts
+++ src/graceful-shutdown.ts
@@ -2,27 +2,13 @@ import http from "node:http";
 import https from "node:https";
 import { setInterval } from "node:timers/promises";
 import type { Socket } from "node:net";
-import type { TLSSocket } from "node:tls";
 import type { ActualLogger } from "./logger-helpers";
-
-const hasResponse = (
-  socket: Socket,
-): socket is typeof socket & { _httpMessage: http.ServerResponse } =>
-  "_httpMessage" in socket &&
-  socket._httpMessage instanceof http.ServerResponse;
-
-const hasHttpServer = (
-  socket: Socket,
-): socket is typeof socket & { server: http.Server } =>
-  "server" in socket && socket.server instanceof http.Server;
-
-const isEncrypted = (socket: Socket): socket is TLSSocket =>
-  "encrypted" in socket &&
-  typeof socket.encrypted === "boolean" &&
-  socket.encrypted;
-
-const weAreClosed: http.RequestListener = ({}, res) =>
-  void (!res.headersSent && res.setHeader("connection", "close"));
+import {
+  hasHttpServer,
+  hasResponse,
+  isEncrypted,
+  weAreClosed,
+} from "./graceful-helpers";
 
 export const monitor = ({
   server,
@@ -33,9 +19,15 @@ export const monitor = ({
   timeout?: number;
   logger?: ActualLogger;
 }) => {
-  const sockets = new Set<Socket>();
-
   let pending: Promise<void> | undefined;
+  const sockets = new Set<Socket>();
+  const destroy = (socket: Socket) => void sockets.delete(socket.destroy());
+
+  const disconnect = (socket: Socket) =>
+    void (hasResponse(socket)
+      ? !socket._httpMessage.headersSent &&
+        socket._httpMessage.setHeader("connection", "close")
+      : destroy(socket));
 
   const watch = (socket: Socket) =>
     void (pending
@@ -50,14 +42,6 @@ export const monitor = ({
       (resolve, reject) =>
         void server.close((error) => (error ? reject(error) : resolve())),
     );
-
-  const destroy = (socket: Socket) => void sockets.delete(socket.destroy());
-
-  const disconnect = (socket: Socket) =>
-    void (hasResponse(socket)
-      ? !socket._httpMessage.headersSent &&
-        socket._httpMessage.setHeader("connection", "close")
-      : destroy(socket));
 
   const workflow = async () => {
     server.on("request", weAreClosed);
