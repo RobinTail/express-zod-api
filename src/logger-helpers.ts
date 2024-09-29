38a9da858ed24828b77256cf9cb268502c2bc4d4
Robin Tail <robin_tail@me.com>
2024-09-29 12:09:34 +0200

    Feat: using Intl units formatting, expressing picoseconds through nanoseconds.

--- src/logger-helpers.ts
+++ src/logger-helpers.ts
@@ -26,32 +26,36 @@ export const isLoggerInstance = (subject: unknown): subject is AbstractLogger =>
   isObject(subject) &&
   Object.keys(severity).some((method) => method in subject);
 
-/**
- * @todo consider Intl units when Node 18 dropped (microsecond unit is missing, picosecond is not in list)
- * @link https://tc39.es/ecma402/#table-sanctioned-single-unit-identifiers
- * */
-const makeNumberFormat = (fraction = 0) =>
+/** @link https://tc39.es/ecma402/#table-sanctioned-single-unit-identifiers */
+const makeNumberFormat = (unit: string, fraction = 0) =>
   Intl.NumberFormat(undefined, {
     useGrouping: false,
     minimumFractionDigits: 0,
     maximumFractionDigits: fraction,
+    style: "unit",
+    unitDisplay: "long",
+    unit,
   });
 
 // creating them once increases the performance significantly
-const intFormat = makeNumberFormat();
-const floatFormat = makeNumberFormat(2);
+const pcFormat = makeNumberFormat("nanosecond", 3);
+const nsFormat = makeNumberFormat("nanosecond");
+const mcFormat = makeNumberFormat("microsecond");
+const msFormat = makeNumberFormat("millisecond");
+const sFormat = makeNumberFormat("second", 2);
+const mFormat = makeNumberFormat("minute", 2);
 
 // not using R.cond for performance optimization
-const pickTimeUnit = (ms: number): [string, number, Intl.NumberFormat] => {
-  if (ms < 1e-6) return ["picosecond", ms / 1e-9, intFormat];
-  if (ms < 1e-3) return ["nanosecond", ms / 1e-6, intFormat];
-  if (ms < 1) return ["microsecond", ms / 1e-3, intFormat];
-  if (ms < 1e3) return ["millisecond", ms, intFormat];
-  if (ms < 6e4) return ["second", ms / 1e3, floatFormat];
-  return ["minute", ms / 6e4, floatFormat];
+const pickTimeUnit = (ms: number): [number, Intl.NumberFormat] => {
+  if (ms < 1e-6) return [ms / 1e-6, pcFormat];
+  if (ms < 1e-3) return [ms / 1e-6, nsFormat];
+  if (ms < 1) return [ms / 1e-3, mcFormat];
+  if (ms < 1e3) return [ms, msFormat];
+  if (ms < 6e4) return [ms / 1e3, sFormat];
+  return [ms / 6e4, mFormat];
 };
 
 export const formatDuration = (durationMs: number) => {
-  const [unit, converted, formatter] = pickTimeUnit(durationMs);
-  return `${formatter.format(converted)} ${unit}${converted > 1 ? "s" : ""}`;
+  const [converted, formatter] = pickTimeUnit(durationMs);
+  return formatter.format(converted);
 };
