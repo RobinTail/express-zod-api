4b2f0ac8b6d6bd707ee52f7f84d99daf9d79358e
Robin Tail <robin_tail@me.com>
2024-10-01 21:02:48 +0200

    Minor: shortening.

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,11 +5,11 @@ import { renderUnicodeCompact } from "uqr";
 
 const minNode = semver.minVersion(engines.node)!;
 
-const qrOptions = { minVersion: 4, maxVersion: 4, boostEcc: true };
-const qrDocs = renderUnicodeCompact(
-  `https://ez.robintail.cz/v${version}`,
-  qrOptions,
-);
+const qrDocs = renderUnicodeCompact(`https://ez.robintail.cz/v${version}`, {
+  minVersion: 4,
+  maxVersion: 4,
+  boostEcc: true,
+});
 
 const commons: Options = {
   format: ["cjs", "esm"],
