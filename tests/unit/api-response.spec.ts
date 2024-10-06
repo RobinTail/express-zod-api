2317a93dc162c69c0a38aa3c96a27d9a401b75b4
Robin Tail <robin_tail@me.com>
2024-10-07 00:02:03 +0200

    Ref: rm redundant check.

--- tests/unit/api-response.spec.ts
+++ tests/unit/api-response.spec.ts
@@ -1,11 +1,9 @@
-import { keys } from "ramda";
 import { defaultStatusCodes } from "../../src/api-response";
 
 describe("ApiResponse", () => {
   describe("defaultStatusCodes", () => {
     test("should be 200 and 400", () => {
       expect(defaultStatusCodes).toMatchSnapshot();
-      expect(keys(defaultStatusCodes)).toEqual(["positive", "negative"]);
     });
   });
 });
