b2308aac9518d674f9c6c3825edcd9ca5dab746c
Robin Tail <robin_tail@me.com>
2024-10-07 15:47:41 +0200

    Readme: contributors section.

---
+++ tools/contributors.ts
@@ -0,0 +1,36 @@
+const users = new Set([
+  "rottmann",
+  "boarush",
+  "daniel-white",
+  "kotsmile",
+  "arlyon",
+  "elee1766",
+  "danclaytondev",
+  "huyhoang160593",
+  "sarahssharkey",
+  "shawncarr",
+  "alindsay55661",
+  "john-schmitz",
+  "bobgubko",
+  "miki725",
+  "dev-m1-macbook",
+  "McMerph",
+  "shroudedcode",
+  "maxcohn",
+  "VideoSystemsTech",
+  "TheWisestOne",
+  "lazylace37",
+  "leosuncin",
+  "kirdk",
+  "rayzr522",
+]);
+const size = "50px";
+
+const markdown = Array.from(users)
+  .map(
+    (user) =>
+      `[<img src="https://github.com/${user}.png" alt="@${user}" style="width:${size};border-radius:50%" />](https://github.com/${user})`,
+  )
+  .join("\n");
+
+console.log(markdown);
