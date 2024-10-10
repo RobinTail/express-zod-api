2479fa7597feeedf3e23805ccf44879737810e31
Robin Tail <robin_tail@me.com>
2024-10-10 13:48:23 +0200

    Adjusting compatibility test.

--- tests/compat/migration.spec.ts
+++ tests/compat/migration.spec.ts
@@ -1,8 +1,9 @@
 import { readFile } from "node:fs/promises";
 
+/** @todo update the pretest and assertion with actual code for v21 */
 describe("Migration", () => {
   test("should fix the import", async () => {
     const fixed = await readFile("./sample.ts", "utf-8");
-    expect(fixed).toMatch(/BuiltinLogger/);
+    expect(fixed).toMatch(/express-zod-api/);
   });
 });
