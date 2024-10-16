05c3d98d37d11ee3197217a14250384382f1f308
Robin Tail <robin_tail@me.com>
2024-10-16 10:48:42 +0200

    TDD: The test that does not work.

---
+++ tests/system/http2.spec.ts
@@ -0,0 +1,27 @@
+import { createConfig, createServer } from "../../src";
+import { givePort, signCert } from "../helpers";
+import { fetch, Agent } from "undici";
+
+describe("HTTP2", async () => {
+  const port = givePort();
+  const config = createConfig({
+    http2: { listen: port, options: signCert() },
+    cors: true,
+    startupLogo: false,
+    logger: { level: "info" },
+  });
+  const {
+    servers: [server],
+  } = await createServer(config, {});
+  await vi.waitFor(() => assert(server.listening), { timeout: 1e4 });
+
+  test("should handle requests", async () => {
+    const response = await fetch(`https://localhost:${port}`, {
+      dispatcher: new Agent({
+        connect: { rejectUnauthorized: false },
+        allowH2: true,
+      }),
+    });
+    console.log(response);
+  });
+});
