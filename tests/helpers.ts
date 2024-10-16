e27050900ab391cb995de51272f91ec8e446a95b
Robin Tail <robin_tail@me.com>
2024-10-16 08:13:21 +0200

    Moving SSL certificate self-signing function into testing helpers.

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,3 +1,4 @@
+import forge from "node-forge";
 import { map, when, equals, nAry } from "ramda";
 import { z } from "zod";
 import { ezFileBrand } from "../src/file-schema";
@@ -10,6 +11,45 @@ const disposer = (function* () {
 
 export const givePort = (test?: "example", rsvd = 8090): number =>
   test ? rsvd : when(equals(rsvd), nAry(0, givePort))(disposer.next().value);
+
+const certAttr = [
+  { name: "commonName", value: "localhost" },
+  { name: "countryName", value: "DE" },
+  { name: "organizationName", value: "ExpressZodAPI" },
+  { shortName: "OU", value: "DEV" },
+];
+const certExt = [
+  { name: "basicConstraints", cA: true },
+  { name: "extKeyUsage", serverAuth: true, clientAuth: true },
+  { name: "subjectAltName", altNames: [{ type: 2, value: "localhost" }] },
+  {
+    name: "keyUsage",
+    keyCertSign: true,
+    digitalSignature: true,
+    nonRepudiation: true,
+    keyEncipherment: true,
+    dataEncipherment: true,
+  },
+];
+
+export const signCert = () => {
+  (forge as any).options.usePureJavaScript = true;
+  const keys = forge.pki.rsa.generateKeyPair(2048);
+  const cert = forge.pki.createCertificate();
+  cert.publicKey = keys.publicKey;
+  cert.serialNumber = "01";
+  cert.validity.notBefore = new Date();
+  cert.validity.notAfter = new Date();
+  cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 1);
+  cert.setSubject(certAttr);
+  cert.setIssuer(certAttr);
+  cert.setExtensions(certExt);
+  cert.sign(keys.privateKey, forge.md.sha256.create());
+  return {
+    cert: forge.pki.certificateToPem(cert),
+    key: forge.pki.privateKeyToPem(keys.privateKey),
+  };
+};
 
 export const serializeSchemaForTest = (
   subject: z.ZodTypeAny,
