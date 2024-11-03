c2acf21293f2453a46c184357437834d48f1edab
Robin Tail <robin_tail@me.com>
2024-11-03 17:13:27 +0100

    Revert "Ref: simpler http error enforcing in createParserFailureHandler()."
    This reverts commit 93244bc95ff1632bd4d23be40d3f2d4bbe878744.

--- src/server-helpers.ts
+++ src/server-helpers.ts
@@ -38,7 +38,7 @@ export const createParserFailureHandler =
     return errorHandler.execute({
       error: isHttpError(error)
         ? error
-        : createHttpError(400, ensureError(error)),
+        : createHttpError(400, ensureError(error).message),
       request,
       response,
       input: null,
