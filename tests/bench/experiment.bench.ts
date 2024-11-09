de540a1a4b5b695f52fbbba9bed146b4585849d9
Robin Tail <robin_tail@me.com>
2024-11-09 14:10:11 +0100

    Performance tuning for getInput().

--- tests/bench/experiment.bench.ts
+++ tests/bench/experiment.bench.ts
@@ -1,51 +1,13 @@
-import { SchemaObject, SchemaObjectType } from "openapi3-ts/oas31";
 import { bench } from "vitest";
 
-const current = (prev: SchemaObject): SchemaObjectType[] => {
-  const current = typeof prev.type === "string" ? [prev.type] : prev.type || [];
-  return current.includes("null") ? current : current.concat("null");
-};
+const objects = [{ a: 1 }, { b: 2 }, { c: 3, a: 2 }];
 
-const featured = ({
-  type,
-}: SchemaObject): SchemaObjectType | SchemaObjectType[] => {
-  if (type === "null") return type;
-  if (typeof type === "string") return [type, "null"];
-  return type ? [...new Set(type).add("null")] : "null";
-};
-
-describe.each<SchemaObject>([
-  { type: "string" },
-  { type: ["string"] },
-  { type: undefined },
-  { type: "null" },
-  { type: ["string", "null"] },
-  { type: ["string", "number", "boolean", "null"] },
-])("Experiment for makeNullable %s", (subject) => {
+describe("Experiment for reducer", () => {
   bench("current", () => {
-    current(subject);
+    objects.reduce((agg, obj) => ({ ...agg, ...obj }), {});
   });
 
   bench("featured", () => {
-    featured(subject);
-  });
-});
-
-describe.skip("Set", () => {
-  bench("instance undefined", () => {
-    new Set(undefined);
-  });
-
-  bench("instance from array", () => {
-    new Set(["string"]);
-  });
-
-  const test = new Set();
-  bench("adding", () => {
-    test.add("null");
-  });
-
-  bench("spread", () => {
-    return void [...test];
+    objects.reduce((agg, obj) => Object.assign(agg, obj), {});
   });
 });
