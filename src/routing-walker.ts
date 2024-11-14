f3a55912d23d83c2b80b035daed1356c83afad3a
Robin Tail <robin_tail@me.com>
2024-11-14 22:45:11 +0100

    Featured implementation.

--- src/routing-walker.ts
+++ src/routing-walker.ts
@@ -1,4 +1,6 @@
 import assert from "node:assert/strict";
+import { toPairs } from "ramda";
+import { number } from "zod";
 import { DependsOnMethod } from "./depends-on-method";
 import { AbstractEndpoint } from "./endpoint";
 import { RoutingError } from "./errors";
@@ -54,6 +56,42 @@ export const walkRouting = ({
       }
     } else {
       walkRouting({ onEndpoint, onStatic, routing: element, parentPath: path });
+    }
+  }
+};
+
+// @todo ensure trim()
+export const walkRouting2 = ({
+  routing,
+  onEndpoint,
+  onStatic,
+}: RoutingWalkerParams) => {
+  const stack = toPairs(routing);
+  while (stack.length) {
+    const [path, element] = stack.pop()!;
+    if (element instanceof AbstractEndpoint) {
+      const methods = element.getMethods() || ["get"];
+      for (const method of methods) onEndpoint(element, path, method);
+    } else if (element instanceof ServeStatic) {
+      if (onStatic) element.apply(path, onStatic);
+    } else if (element instanceof DependsOnMethod) {
+      for (const [method, endpoint, siblingMethods] of element.entries) {
+        const supportedMethods = endpoint.getMethods();
+        assert(
+          !supportedMethods || supportedMethods.includes(method),
+          new RoutingError(
+            `Endpoint assigned to ${method} method of ${path} must support ${method} method.`,
+          ),
+        );
+        onEndpoint(endpoint, path, method, siblingMethods);
+      }
+    } else {
+      const pairs = toPairs(element);
+      const prefixed = pairs.map<(typeof pairs)[number]>(([segment, item]) => [
+        `${path}/${segment}`,
+        item,
+      ]);
+      stack.push(...prefixed);
     }
   }
 };
--- tests/bench/experiment.bench.ts
+++ tests/bench/experiment.bench.ts
@@ -1,6 +1,6 @@
 import { bench } from "vitest";
 import { DependsOnMethod } from "../../src";
-import { walkRouting } from "../../src/routing-walker";
+import { walkRouting, walkRouting2 } from "../../src/routing-walker";
 
 const routing = {
   a: {
@@ -21,11 +21,17 @@ const routing = {
         },
       },
     },
+    k: { l: {} },
+    m: {},
   },
 };
 
 describe("Experiment for routing walker", () => {
   bench("current", () => {
     walkRouting({ routing, onEndpoint: vi.fn() });
+  });
+
+  bench("featured", () => {
+    walkRouting2({ routing, onEndpoint: vi.fn() });
   });
 });
