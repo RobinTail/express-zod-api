RobinTail/express-zod-api

commit 1aaa26a4802ace0e0a0367a89c3a33104fd2e0e1

Ref: using `node:assert/strict` instead of `if + throw` (#1371)
Reducing branch complexity that way.
⚠️ Requires to get rid of Jest: #1369

src/routing-walker.ts
@@ -1,3 +1,4 @@
+import assert from "node:assert/strict";
 import { DependsOnMethod } from "./depends-on-method";
 import { AbstractEndpoint } from "./endpoint";
 import { RoutingError } from "./errors";
@@ -26,11 +27,13 @@ export const walkRouting = ({
 }: RoutingWalkerParams) => {
   Object.entries(routing).forEach(([segment, element]) => {
     segment = segment.trim();
-    if (segment.match(/\//)) {
-      throw new RoutingError(
+    assert.doesNotMatch(
+      segment,
+      /\//,
+      new RoutingError(
         `The entry '${segment}' must avoid having slashes — use nesting instead.`,
-      );
-    }
+      ),
+    );
     const path = `${parentPath || ""}${segment ? `/${segment}` : ""}`;
     if (element instanceof AbstractEndpoint) {
       const methods: (Method | AuxMethod)[] = element.getMethods().slice();
@@ -46,11 +49,12 @@ export const walkRouting = ({
       }
     } else if (element instanceof DependsOnMethod) {
       Object.entries(element.endpoints).forEach(([method, endpoint]) => {
-        if (!endpoint.getMethods().includes(method as Method)) {
-          throw new RoutingError(
+        assert(
+          endpoint.getMethods().includes(method as Method),
+          new RoutingError(
             `Endpoint assigned to ${method} method of ${path} must support ${method} method.`,
-          );
-        }
+          ),
+        );
         onEndpoint(endpoint, path, method as Method);
       });
       if (hasCors && Object.keys(element.endpoints).length > 0) {

example/endpoints/retrieve-user.ts
@@ -1,4 +1,5 @@
 import createHttpError from "http-errors";
+import assert from "node:assert/strict";
 import { z } from "zod";
 import { taggedEndpointsFactory } from "../factories";
 import { methodProviderMiddleware } from "../middlewares";
@@ -37,9 +38,7 @@ export const retrieveUserEndpoint = taggedEndpointsFactory
     handler: async ({ input: { id }, options: { method }, logger }) => {
       logger.debug(`Requested id: ${id}, method ${method}`);
       const name = "John Doe";
-      if (id > 100) {
-        throw createHttpError(404, "User not found");
-      }
+      assert(id <= 100, createHttpError(404, "User not found"));
       return {
         id,
         name,

example/endpoints/update-user.ts
@@ -1,4 +1,5 @@
 import createHttpError from "http-errors";
+import assert from "node:assert/strict";
 import { z } from "zod";
 import { ez, withMeta } from "../../src";
 import { keyAndTokenAuthenticatedEndpointsFactory } from "../factories";
@@ -41,9 +42,7 @@ export const updateUserEndpoint =
       logger,
     }) => {
       logger.debug(`id, key and token: ${id}, ${key}, ${token}`);
-      if (id > 100) {
-        throw createHttpError(404, "User not found");
-      }
+      assert(id <= 100, createHttpError(404, "User not found"));
       return {
         createdAt: new Date("2022-01-22"),
         name,

example/middlewares.ts
@@ -1,4 +1,5 @@
 import createHttpError from "http-errors";
+import assert from "node:assert/strict";
 import { z } from "zod";
 import { Method, createMiddleware, withMeta } from "../src";
 
@@ -18,12 +19,12 @@ export const authMiddleware = createMiddleware({
   }),
   middleware: async ({ input: { key }, request, logger }) => {
     logger.debug("Checking the key and token...");
-    if (key !== "123") {
-      throw createHttpError(401, "Invalid key");
-    }
-    if (request.headers.token !== "456") {
-      throw createHttpError(401, "Invalid token");
-    }
+    assert.equal(key, "123", createHttpError(401, "Invalid key"));
+    assert.equal(
+      request.headers.token,
+      "456",
+      createHttpError(401, "Invalid token"),
+    );
     return { token: request.headers.token };
   },
 });

src/documentation-helpers.ts
@@ -1,3 +1,4 @@
+import assert from "node:assert/strict";
 import {
   ContentObject,
   ExampleObject,
@@ -132,12 +133,13 @@ export const depictAny: Depicter<z.ZodAny> = () => ({
 });
 
 export const depictUpload: Depicter<ZodUpload> = (ctx) => {
-  if (ctx.isResponse) {
-    throw new DocumentationError({
+  assert(
+    !ctx.isResponse,
+    new DocumentationError({
       message: "Please use z.upload() only for input.",
       ...ctx,
-    });
-  }
+    }),
+  );
   return {
     type: "string",
     format: "binary",
@@ -243,12 +245,13 @@ export const depictNull: Depicter<z.ZodNull> = () => ({
 });
 
 export const depictDateIn: Depicter<ZodDateIn> = (ctx) => {
-  if (ctx.isResponse) {
-    throw new DocumentationError({
+  assert(
+    !ctx.isResponse,
+    new DocumentationError({
       message: "Please use z.dateOut() for output.",
       ...ctx,
-    });
-  }
+    }),
+  );
   return {
     description: "YYYY-MM-DDTHH:mm:ss.sssZ",
     type: "string",
@@ -261,12 +264,13 @@ export const depictDateIn: Depicter<ZodDateIn> = (ctx) => {
 };
 
 export const depictDateOut: Depicter<ZodDateOut> = (ctx) => {
-  if (!ctx.isResponse) {
-    throw new DocumentationError({
+  assert(
+    ctx.isResponse,
+    new DocumentationError({
       message: "Please use z.dateIn() for input.",
       ...ctx,
-    });
-  }
+    }),
+  );
   return {
     description: "YYYY-MM-DDTHH:mm:ss.sssZ",
     type: "string",
@@ -278,16 +282,17 @@ export const depictDateOut: Depicter<ZodDateOut> = (ctx) => {
 };
 
 /** @throws DocumentationError */
-export const depictDate: Depicter<z.ZodDate> = (ctx) => {
-  throw new DocumentationError({
-    message: `Using z.date() within ${
-      ctx.isResponse ? "output" : "input"
-    } schema is forbidden. Please use z.date${
-      ctx.isResponse ? "Out" : "In"
-    }() instead. Check out the documentation for details.`,
-    ...ctx,
-  });
-};
+export const depictDate: Depicter<z.ZodDate> = (ctx) =>
+  assert.fail(
+    new DocumentationError({
+      message: `Using z.date() within ${
+        ctx.isResponse ? "output" : "input"
+      } schema is forbidden. Please use z.date${
+        ctx.isResponse ? "Out" : "In"
+      }() instead. Check out the documentation for details.`,
+      ...ctx,
+    }),
+  );
 
 export const depictBoolean: Depicter<z.ZodBoolean> = () => ({
   type: "boolean",
@@ -625,14 +630,15 @@ export const extractObjectSchema = (
       .map((option) => extractObjectSchema(option, ctx))
       .reduce((acc, option) => acc.merge(option.partial()), z.object({}));
   } else if (subject instanceof z.ZodEffects) {
-    if (hasTopLevelTransformingEffect(subject)) {
-      throw new DocumentationError({
+    assert(
+      !hasTopLevelTransformingEffect(subject),
+      new DocumentationError({
         message: `Using transformations on the top level of ${
           ctx.isResponse ? "response" : "input"
         } schema is not allowed.`,
         ...ctx,
-      });
-    }
+      }),
+    );
     objectSchema = extractObjectSchema(subject._def.schema, ctx); // object refinement
   } else {
     // intersection
@@ -773,15 +779,13 @@ export const onEach: Depicter<z.ZodTypeAny, "each"> = ({
   };
 };
 
-export const onMissing: Depicter<z.ZodTypeAny, "last"> = ({
-  schema,
-  ...ctx
-}) => {
-  throw new DocumentationError({
-    message: `Zod type ${schema.constructor.name} is unsupported.`,
-    ...ctx,
-  });
-};
+export const onMissing: Depicter<z.ZodTypeAny, "last"> = ({ schema, ...ctx }) =>
+  assert.fail(
+    new DocumentationError({
+      message: `Zod type ${schema.constructor.name} is unsupported.`,
+      ...ctx,
+    }),
+  );
 
 export const excludeParamsFromDepiction = (
   depicted: SchemaObject | ReferenceObject,

src/documentation.ts
@@ -1,3 +1,4 @@
+import assert from "node:assert/strict";
 import {
   OpenApiBuilder,
   OperationObject,
@@ -75,14 +76,15 @@ export class Documentation extends OpenApiBuilder {
     userDefinedOperationId?: string,
   ) {
     if (userDefinedOperationId) {
-      if (userDefinedOperationId in this.lastOperationIdSuffixes) {
-        throw new DocumentationError({
+      assert(
+        !(userDefinedOperationId in this.lastOperationIdSuffixes),
+        new DocumentationError({
           message: `Duplicated operationId: "${userDefinedOperationId}"`,
           method,
           isResponse: false,
           path,
-        });
-      }
+        }),
+      );
       this.lastOperationIdSuffixes[userDefinedOperationId] = 1;
       return userDefinedOperationId;
     }

src/endpoint.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import assert from "node:assert/strict";
 import { z } from "zod";
 import { ApiResponse } from "./api-response";
 import { CommonConfig } from "./config-type";
@@ -131,11 +132,12 @@ export class Endpoint<
       { name: "input schema", schema: inputSchema },
       { name: "output schema", schema: outputSchema },
     ].forEach(({ name, schema }) => {
-      if (hasTopLevelTransformingEffect(schema)) {
-        throw new IOSchemaError(
+      assert(
+        !hasTopLevelTransformingEffect(schema),
+        new IOSchemaError(
           `Using transformations on the top level of endpoint ${name} is not allowed.`,
-        );
-      }
+        ),
+      );
     });
     this.#handler = handler;
     this.#resultHandler = resultHandler;

src/middleware.ts
@@ -7,6 +7,7 @@ import { IOSchema } from "./io-schema";
 import { LogicalContainer } from "./logical-container";
 import { Security } from "./security";
 import { AbstractLogger } from "./logger";
+import assert from "node:assert/strict";
 
 interface MiddlewareParams<IN, OPT> {
   input: IN;
@@ -50,11 +51,12 @@ export const createMiddleware = <
 >(
   props: MiddlewareCreationProps<IN, OPT, OUT, SCO>,
 ): MiddlewareDefinition<IN, OPT, OUT, SCO> => {
-  if (hasTopLevelTransformingEffect(props.input)) {
-    throw new IOSchemaError(
+  assert(
+    !hasTopLevelTransformingEffect(props.input),
+    new IOSchemaError(
       "Using transformations on the top level of middleware input schema is not allowed.",
-    );
-  }
+    ),
+  );
   return {
     ...props,
     type: "proprietary",

commit bc8e9eb0bd5981975d6f4dc7725ce9b5aa190b93

Traverse without recursion (#2168)
Avoiding recursion should decrease memory consumption by replacing the
utilization of call stack to a stack of entities to process

https://www.nearform.com/insights/tracking-memory-allocation-node-js/

```
   ✓ Experiment for routing walker (2) 1247ms
     name             hz     min     max    mean     p75     p99    p995    p999     rme  samples
   · current    3,691.20  0.2053  2.7257  0.2709  0.2885  0.3817  0.4519  2.1826  ±1.49%     1846
   · featured  50,253.26  0.0123  5.8032  0.0199  0.0165  0.0423  0.1116  2.7895  ±7.10%    25127   fastest
```

Turned out that the performance is mostly affected by `assert(..., new
Error())` pattern, partially introduced by
https://github.com/RobinTail/express-zod-api/pull/1371

eslint.config.js
@@ -45,6 +45,11 @@ export default [
           selector: "ImportDeclaration[source.value=/assert/]",
           message: "assert is slow, use throw",
         },
+        {
+          selector:
+            "CallExpression > MemberExpression[object.name='Object'][property.name='entries']",
+          message: "Object.entries() is 2x slower than R.toPairs()",
+        },
       ],
     },
   },

src/routing-walker.ts
@@ -1,3 +1,4 @@
+import { toPairs } from "ramda";
 import { DependsOnMethod } from "./depends-on-method";
 import { AbstractEndpoint } from "./endpoint";
 import { RoutingError } from "./errors";
@@ -17,22 +18,25 @@ export interface RoutingWalkerParams {
   parentPath?: string;
 }
 
-export const walkRouting = ({
-  routing,
-  onEndpoint,
-  onStatic,
-  parentPath,
-}: RoutingWalkerParams) => {
-  const pairs = Object.entries(routing).map(
-    ([key, value]) => [key.trim(), value] as const,
-  );
-  for (const [segment, element] of pairs) {
+const makePairs = (subject: Routing, parent?: string) =>
+  toPairs(subject).map(([segment, item]) => {
     if (segment.includes("/")) {
       throw new RoutingError(
         `The entry '${segment}' must avoid having slashes — use nesting instead.`,
       );
     }
-    const path = `${parentPath || ""}${segment ? `/${segment}` : ""}`;
+    const trimmed = segment.trim();
+    return [`${parent || ""}${trimmed ? `/${trimmed}` : ""}`, item] as const;
+  });
+
+export const walkRouting = ({
+  routing,
+  onEndpoint,
+  onStatic,
+}: RoutingWalkerParams) => {
+  const stack = makePairs(routing);
+  while (stack.length) {
+    const [path, element] = stack.shift()!;
     if (element instanceof AbstractEndpoint) {
       const methods = element.getMethods() || ["get"];
       for (const method of methods) onEndpoint(element, path, method);
@@ -49,7 +53,7 @@ export const walkRouting = ({
         onEndpoint(endpoint, path, method, siblingMethods);
       }
     } else {
-      walkRouting({ onEndpoint, onStatic, routing: element, parentPath: path });
+      stack.push(...makePairs(element, path));
     }
   }
 };

tests/bench/experiment.bench.ts
@@ -1,37 +1,36 @@
-import assert from "node:assert/strict";
 import { bench } from "vitest";
-import { RoutingError } from "../../src";
+import { retrieveUserEndpoint } from "../../example/endpoints/retrieve-user";
+import { DependsOnMethod } from "../../src";
+import { walkRouting } from "../../src/routing-walker";
 
-describe.each([false, true])("Experiment for errors %s", (ass) => {
-  const notAss = !ass;
+const routing = {
+  a: {
+    b: {
+      c: {
+        d: {
+          e: {
+            f: {
+              g: {
+                h: {
+                  i: {
+                    j: new DependsOnMethod({
+                      post: retrieveUserEndpoint,
+                    }),
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    k: { l: {} },
+    m: {},
+  },
+};
 
-  bench("assert with text", () => {
-    try {
-      assert(ass, "text");
-    } catch {}
-  });
-
-  bench("assert with Error", () => {
-    try {
-      assert(ass, new Error());
-    } catch {}
-  });
-
-  bench("assert with custom error", () => {
-    try {
-      assert(ass, new RoutingError());
-    } catch {}
-  });
-
-  bench("throwing Error", () => {
-    try {
-      if (notAss) throw new Error();
-    } catch {}
-  });
-
-  bench("throwing custom error", () => {
-    try {
-      if (notAss) throw new RoutingError();
-    } catch {}
+describe("Experiment for routing walker", () => {
+  bench("featured", () => {
+    walkRouting({ routing, onEndpoint: vi.fn() });
   });
 });