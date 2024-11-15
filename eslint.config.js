dfd9d72d6da222228fcfbc0e624b9d19f1133360
Robin Tail <robin_tail@me.com>
2024-11-15 14:21:10 +0100

    Ref, rev: using Object.entries() in traverse instead of toPairs().

--- eslint.config.js
+++ eslint.config.js
@@ -45,11 +45,6 @@ export default [
           selector: "ImportDeclaration[source.value=/assert/]",
           message: "assert is slow, use throw",
         },
-        {
-          selector:
-            "CallExpression > MemberExpression[object.name='Object'][property.name='entries']",
-          message: "Object.entries() is 2x slower than R.toPairs()",
-        },
       ],
     },
   },
