a01fb25c8e56d2fcc37cb1a9e47df11b597c847d
Robin Tail <robin_tail@me.com>
2024-11-24 23:57:05 +0100

    Retaining Provider type.

--- example/example.client.ts
+++ example/example.client.ts
@@ -243,6 +243,9 @@ export class ExpressZodAPIClient {
   }
 }
 
+/** @deprecated will be removed in v22 */
+export type Provider = ExpressZodAPIClient["provide"];
+
 // Usage example:
 /*
 export const exampleImplementation: Implementation = async (
--- src/integration.ts
+++ src/integration.ts
@@ -37,7 +37,12 @@ import { Routing } from "./routing";
 import { walkRouting } from "./routing-walker";
 import { HandlingRules } from "./schema-walker";
 import { zodToTs } from "./zts";
-import { ZTSContext, createTypeAlias, printNode } from "./zts-helpers";
+import {
+  ZTSContext,
+  createTypeAlias,
+  printNode,
+  addJsDocComment,
+} from "./zts-helpers";
 import type Prettier from "prettier";
 
 type IOKind = "input" | "response" | ResponseVariant;
@@ -582,11 +587,26 @@ export class Integration {
       [providerOverload1, providerOverload2, actualProvider],
     );
 
+    // @todo remove in v22
+    const providerType = addJsDocComment(
+      makePublicType(
+        this.ids.providerType,
+        f.createIndexedAccessTypeNode(
+          f.createTypeReferenceNode(this.ids.clientClass),
+          f.createLiteralTypeNode(
+            f.createStringLiteral(this.ids.provideMethod.text),
+          ),
+        ),
+      ),
+      "@deprecated will be removed in v22",
+    );
+
     this.program.push(
       jsonEndpointsConst,
       endpointTagsConst,
       implementationType,
       clientClass,
+      providerType,
     );
 
     // method: method.toUpperCase()
