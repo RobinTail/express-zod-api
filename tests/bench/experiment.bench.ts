c1995d5b490d9c57fa13b2228cbd1ef278d176ad
Robin Tail <robin_tail@me.com>
2024-12-23 16:29:03 +0100

    Exp: thunk is faster than memoize with a fixed arg.

--- tests/bench/experiment.bench.ts
+++ tests/bench/experiment.bench.ts
@@ -1,39 +1,19 @@
-import { chain, prop } from "ramda";
-import ts from "typescript";
+import { thunkify } from "ramda";
 import { bench } from "vitest";
-import { f } from "../../src/integration-helpers";
+import { isProduction } from "../../src/common-helpers";
 
-export const current = (nodes: ts.TypeLiteralNode[]) =>
-  f.createTypeLiteralNode(nodes.flatMap(({ members }) => members));
-
-export const feat = (nodes: ts.TypeLiteralNode[]) =>
-  f.createTypeLiteralNode(chain(prop("members"), nodes));
-
-describe("Experiment on flatMap", () => {
-  const subj = [
-    f.createTypeLiteralNode([
-      f.createPropertySignature(
-        undefined,
-        "test1",
-        undefined,
-        f.createTypeReferenceNode("test1"),
-      ),
-    ]),
-    f.createTypeLiteralNode([
-      f.createPropertySignature(
-        undefined,
-        "test2",
-        undefined,
-        f.createTypeReferenceNode("test2"),
-      ),
-    ]),
-  ];
-
-  bench("flatMap", () => {
-    current(subj);
+describe("Experiment on memo/thunk", () => {
+  bench("current", () => {
+    isProduction();
   });
 
-  bench("chain+prop", () => {
-    feat(subj);
+  bench("control", () => {
+    return void (process.env.NODE_ENV === "production");
+  });
+
+  const feat = thunkify(() => process.env.NODE_ENV === "production");
+
+  bench("feat", () => {
+    feat();
   });
 });
