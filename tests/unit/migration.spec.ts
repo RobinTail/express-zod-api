RobinTail/express-zod-api

commit 15beb1049721ffa182e4557448ece497da084273

Deprecating `MethodPath` in favour  of `Request` (naming) (#2275)

example/example.client.ts
@@ -399,7 +399,10 @@ export interface Response {
     | NegativeResponse["get /v1/events/time"];
 }
 
-export type MethodPath = keyof Input;
+export type Request = keyof Input;
+
+/** @deprecated use Request instead */
+export type MethodPath = Request;
 
 /** @deprecated use content-type header of an actual response */
 export const jsonEndpoints = {
@@ -442,7 +445,7 @@ export class ExpressZodAPIClient {
   ): Promise<
     `${M} ${P}` extends keyof Response ? Response[`${M} ${P}`] : unknown
   >;
-  public provide<K extends MethodPath>(
+  public provide<K extends Request>(
     request: K,
     params: Input[K],
   ): Promise<Response[K]>;

src/integration.ts
@@ -122,6 +122,8 @@ export class Integration {
   protected ids = {
     pathType: f.createIdentifier("Path"),
     methodType: f.createIdentifier("Method"),
+    requestType: f.createIdentifier("Request"),
+    /** @todo remove in v22 */
     methodPathType: f.createIdentifier("MethodPath"),
     inputInterface: f.createIdentifier("Input"),
     posResponseInterface: f.createIdentifier("PositiveResponse"),
@@ -315,11 +317,19 @@ export class Integration {
     for (const { id, props } of this.interfaces)
       this.program.push(makeInterface(id, props, { isPublic: true }));
 
-    // export type MethodPath = keyof Input;
+    // export type Request = keyof Input;
     this.program.push(
-      makeType(this.ids.methodPathType, makeKeyOf(this.ids.inputInterface), {
+      makeType(this.ids.requestType, makeKeyOf(this.ids.inputInterface), {
         isPublic: true,
       }),
+    );
+    // export type MethodPath = Request;
+    this.program.push(
+      makeType(
+        this.ids.methodPathType,
+        f.createTypeReferenceNode(this.ids.requestType),
+        { isPublic: true, comment: "@deprecated use Request instead" },
+      ),
     );
 
     if (variant === "types") return;
@@ -464,9 +474,7 @@ export class Integration {
         ),
       }),
       undefined, // overload
-      makeTypeParams({
-        K: this.ids.methodPathType,
-      }),
+      makeTypeParams({ K: this.ids.requestType }),
       makePromise(
         f.createIndexedAccessTypeNode(
           f.createTypeReferenceNode(this.ids.responseInterface),

tests/unit/__snapshots__/integration.spec.ts.snap
@@ -61,7 +61,10 @@ export interface Response {
     | NegativeResponse["post /v1/test-with-dashes"];
 }
 
-export type MethodPath = keyof Input;
+export type Request = keyof Input;
+
+/** @deprecated use Request instead */
+export type MethodPath = Request;
 
 /** @deprecated use content-type header of an actual response */
 export const jsonEndpoints = { "post /v1/test-with-dashes": true };
@@ -86,7 +89,7 @@ export class ExpressZodAPIClient {
   ): Promise<
     \`\${M} \${P}\` extends keyof Response ? Response[\`\${M} \${P}\`] : unknown
   >;
-  public provide<K extends MethodPath>(
+  public provide<K extends Request>(
     request: K,
     params: Input[K],
   ): Promise<Response[K]>;
@@ -205,7 +208,10 @@ export interface Response {
     | NegativeResponse["post /v1/test-with-dashes"];
 }
 
-export type MethodPath = keyof Input;
+export type Request = keyof Input;
+
+/** @deprecated use Request instead */
+export type MethodPath = Request;
 
 /** @deprecated use content-type header of an actual response */
 export const jsonEndpoints = { "post /v1/test-with-dashes": true };
@@ -230,7 +236,7 @@ export class ExpressZodAPIClient {
   ): Promise<
     \`\${M} \${P}\` extends keyof Response ? Response[\`\${M} \${P}\`] : unknown
   >;
-  public provide<K extends MethodPath>(
+  public provide<K extends Request>(
     request: K,
     params: Input[K],
   ): Promise<Response[K]>;
@@ -349,7 +355,10 @@ export interface Response {
     | NegativeResponse["post /v1/test-with-dashes"];
 }
 
-export type MethodPath = keyof Input;
+export type Request = keyof Input;
+
+/** @deprecated use Request instead */
+export type MethodPath = Request;
 
 /** @deprecated use content-type header of an actual response */
 export const jsonEndpoints = { "post /v1/test-with-dashes": true };
@@ -374,7 +383,7 @@ export class ExpressZodAPIClient {
   ): Promise<
     \`\${M} \${P}\` extends keyof Response ? Response[\`\${M} \${P}\`] : unknown
   >;
-  public provide<K extends MethodPath>(
+  public provide<K extends Request>(
     request: K,
     params: Input[K],
   ): Promise<Response[K]>;
@@ -494,7 +503,10 @@ export interface Response {
     | NegativeResponse["post /v1/custom"];
 }
 
-export type MethodPath = keyof Input;
+export type Request = keyof Input;
+
+/** @deprecated use Request instead */
+export type MethodPath = Request;
 "
 `;
 
@@ -900,7 +912,10 @@ export interface Response {
     | NegativeResponse["get /v1/events/time"];
 }
 
-export type MethodPath = keyof Input;
+export type Request = keyof Input;
+
+/** @deprecated use Request instead */
+export type MethodPath = Request;
 
 /** @deprecated use content-type header of an actual response */
 export const jsonEndpoints = {
@@ -943,7 +958,7 @@ export class ExpressZodAPIClient {
   ): Promise<
     \`\${M} \${P}\` extends keyof Response ? Response[\`\${M} \${P}\`] : unknown
   >;
-  public provide<K extends MethodPath>(
+  public provide<K extends Request>(
     request: K,
     params: Input[K],
   ): Promise<Response[K]>;
@@ -1403,7 +1418,10 @@ export interface Response {
     | NegativeResponse["get /v1/events/time"];
 }
 
-export type MethodPath = keyof Input;
+export type Request = keyof Input;
+
+/** @deprecated use Request instead */
+export type MethodPath = Request;
 "
 `;
 
@@ -1476,7 +1494,10 @@ export interface Response {
     | NegativeResponse["post /v1/mtpl"];
 }
 
-export type MethodPath = keyof Input;
+export type Request = keyof Input;
+
+/** @deprecated use Request instead */
+export type MethodPath = Request;
 "
 `;
 
@@ -1541,7 +1562,10 @@ export interface Response {
     | NegativeResponse["post /v1/test-with-dashes"];
 }
 
-export type MethodPath = keyof Input;
+export type Request = keyof Input;
+
+/** @deprecated use Request instead */
+export type MethodPath = Request;
 
 /** @deprecated use content-type header of an actual response */
 export const jsonEndpoints = { "post /v1/test-with-dashes": true };
@@ -1566,7 +1590,7 @@ export class ExpressZodAPIClient {
   ): Promise<
     \`\${M} \${P}\` extends keyof Response ? Response[\`\${M} \${P}\`] : unknown
   >;
-  public provide<K extends MethodPath>(
+  public provide<K extends Request>(
     request: K,
     params: Input[K],
   ): Promise<Response[K]>;

commit bdf12da2d729f3f1ab562f34f114b1cfb30a2120

Drop `MethodPath` type (#2276)
caused by #2275

example/example.client.ts
@@ -401,9 +401,6 @@ export interface Response {
 
 export type Request = keyof Input;
 
-/** @deprecated use Request instead */
-export type MethodPath = Request;
-
 export const endpointTags = {
   "get /v1/user/retrieve": ["users"],
   "delete /v1/user/:id/remove": ["users"],

src/integration.ts
@@ -106,8 +106,6 @@ export class Integration {
     pathType: f.createIdentifier("Path"),
     methodType: f.createIdentifier("Method"),
     requestType: f.createIdentifier("Request"),
-    /** @todo remove in v22 */
-    methodPathType: f.createIdentifier("MethodPath"),
     inputInterface: f.createIdentifier("Input"),
     posResponseInterface: f.createIdentifier("PositiveResponse"),
     negResponseInterface: f.createIdentifier("NegativeResponse"),
@@ -287,14 +285,6 @@ export class Integration {
       makeType(this.ids.requestType, makeKeyOf(this.ids.inputInterface), {
         isPublic: true,
       }),
-    );
-    // export type MethodPath = Request;
-    this.program.push(
-      makeType(
-        this.ids.methodPathType,
-        f.createTypeReferenceNode(this.ids.requestType),
-        { isPublic: true, comment: "@deprecated use Request instead" },
-      ),
     );
 
     if (variant === "types") return;

src/migration.ts
@@ -15,6 +15,7 @@ interface Queries {
     ];
   };
   splitResponse: TSESTree.Property & { key: TSESTree.Identifier };
+  methodPath: TSESTree.ImportSpecifier & { imported: TSESTree.Identifier };
 }
 
 type Listener = keyof Queries;
@@ -24,6 +25,7 @@ const queries: Record<Listener, string> = {
     `${NT.CallExpression}[callee.property.name='provide'][arguments.length=3]` +
     `:has(${NT.Literal}[value=/^${methods.join("|")}$/] + ${NT.Literal} + ${NT.ObjectExpression})`,
   splitResponse: `${NT.NewExpression}[callee.name='Integration'] > ${NT.ObjectExpression} > ${NT.Property}[key.name='splitResponse']`,
+  methodPath: `${NT.ImportDeclaration} > ${NT.ImportSpecifier}[imported.name='MethodPath']`,
 };
 
 const listen = <
@@ -76,6 +78,15 @@ const v22 = ESLintUtils.RuleCreator.withoutDocs({
           data: { subject: "property", name: node.key.name },
           fix: (fixer) => fixer.remove(node),
         }),
+      methodPath: (node) => {
+        const replacement = "Request";
+        ctx.report({
+          messageId: "change",
+          node: node.imported,
+          data: { subject: "type", from: node.imported.name, to: replacement },
+          fix: (fixer) => fixer.replaceText(node.imported, replacement),
+        });
+      },
     }),
 });
 

tests/unit/__snapshots__/integration.spec.ts.snap
@@ -63,9 +63,6 @@ export interface Response {
 
 export type Request = keyof Input;
 
-/** @deprecated use Request instead */
-export type MethodPath = Request;
-
 export const endpointTags = { "post /v1/test-with-dashes": [] };
 
 export type Implementation = (
@@ -189,9 +186,6 @@ export interface Response {
 }
 
 export type Request = keyof Input;
-
-/** @deprecated use Request instead */
-export type MethodPath = Request;
 
 export const endpointTags = { "post /v1/test-with-dashes": [] };
 
@@ -317,9 +311,6 @@ export interface Response {
 
 export type Request = keyof Input;
 
-/** @deprecated use Request instead */
-export type MethodPath = Request;
-
 export const endpointTags = { "post /v1/test-with-dashes": [] };
 
 export type Implementation = (
@@ -444,9 +435,6 @@ export interface Response {
 }
 
 export type Request = keyof Input;
-
-/** @deprecated use Request instead */
-export type MethodPath = Request;
 "
 `;
 
@@ -853,9 +841,6 @@ export interface Response {
 }
 
 export type Request = keyof Input;
-
-/** @deprecated use Request instead */
-export type MethodPath = Request;
 
 export const endpointTags = {
   "get /v1/user/retrieve": ["users"],
@@ -1332,9 +1317,6 @@ export interface Response {
 }
 
 export type Request = keyof Input;
-
-/** @deprecated use Request instead */
-export type MethodPath = Request;
 "
 `;
 
@@ -1408,9 +1390,6 @@ export interface Response {
 }
 
 export type Request = keyof Input;
-
-/** @deprecated use Request instead */
-export type MethodPath = Request;
 "
 `;
 
@@ -1476,9 +1455,6 @@ export interface Response {
 }
 
 export type Request = keyof Input;
-
-/** @deprecated use Request instead */
-export type MethodPath = Request;
 
 export const endpointTags = { "post /v1/test-with-dashes": [] };
 

tests/unit/migration.spec.ts
@@ -21,6 +21,7 @@ describe("Migration", () => {
     valid: [
       `client.provide("get /v1/test", {id: 10});`,
       `new Integration({ routing });`,
+      `import { Request } from "./client.ts";`,
     ],
     invalid: [
       {
@@ -44,6 +45,16 @@ describe("Migration", () => {
           {
             messageId: "remove",
             data: { subject: "property", name: "splitResponse" },
+          },
+        ],
+      },
+      {
+        code: `import { MethodPath } from "./client.ts";`,
+        output: `import { Request } from "./client.ts";`,
+        errors: [
+          {
+            messageId: "change",
+            data: { subject: "type", from: "MethodPath", to: "Request" },
           },
         ],
       },