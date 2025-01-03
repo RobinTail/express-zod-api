b78a5ef6120e9da99f929214daa2fc58820f692c
Robin Tail <robin_tail@me.com>
2025-01-03 13:58:48 +0100

    Shortening.

--- src/integration.ts
+++ src/integration.ts
@@ -487,10 +487,7 @@ export class Integration {
           f.createIdentifier("connection"),
           f.createObjectLiteralExpression(
             [
-              f.createShorthandPropertyAssignment(
-                f.createIdentifier("source"),
-                undefined,
-              ),
+              f.createShorthandPropertyAssignment("source"),
               f.createPropertyAssignment(
                 f.createIdentifier("on"),
                 f.createArrowFunction(
@@ -498,15 +495,11 @@ export class Integration {
                   [
                     f.createTypeParameterDeclaration(
                       undefined,
-                      f.createIdentifier("E"),
+                      "E",
                       f.createIndexedAccessTypeNode(
-                        f.createTypeReferenceNode(
-                          f.createIdentifier("Res"),
-                          undefined,
-                        ),
+                        f.createTypeReferenceNode("Res"),
                         f.createLiteralTypeNode(f.createStringLiteral("event")),
                       ),
-                      undefined,
                     ),
                   ],
                   makeParams({
