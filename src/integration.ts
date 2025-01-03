17eac8cd9bf910136328dcd01af1784eaabd65c6
Robin Tail <robin_tail@me.com>
2025-01-03 13:46:05 +0100

    Reusing makePromise() for void.

--- src/integration.ts
+++ src/integration.ts
@@ -538,11 +538,11 @@ export class Integration {
                                 f.createKeywordTypeNode(
                                   ts.SyntaxKind.VoidKeyword,
                                 ),
-                                f.createTypeReferenceNode("Promise", [
+                                makePromise(
                                   f.createKeywordTypeNode(
                                     ts.SyntaxKind.VoidKeyword,
                                   ),
-                                ]),
+                                ),
                               ]),
                             ),
                           }),
