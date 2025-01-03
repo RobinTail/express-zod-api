6d3a19b78ee5c989dfd54ad4d0b0a3fb01e1f891
Robin Tail <robin_tail@me.com>
2025-01-03 13:23:42 +0100

    Reusing makeParams() for msg.

--- src/integration.ts
+++ src/integration.ts
@@ -578,16 +578,7 @@ export class Integration {
                                     f.createArrowFunction(
                                       undefined,
                                       undefined,
-                                      [
-                                        f.createParameterDeclaration(
-                                          undefined,
-                                          undefined,
-                                          f.createIdentifier("msg"),
-                                          undefined,
-                                          undefined,
-                                          undefined,
-                                        ),
-                                      ],
+                                      makeParams({ msg: undefined }),
                                       undefined,
                                       f.createToken(
                                         ts.SyntaxKind.EqualsGreaterThanToken,
