56c4e2389953fcac1107cbc2cd75a3abbcbea8e7
Anna Bocharova <robin_tail@me.com>
2025-01-11 15:28:09 +0100

    Create empty interfaces upon instantiation of `Integration` (#2298)
    Due to #2225

--- src/integration.ts
+++ src/integration.ts
@@ -167,7 +167,13 @@ export class Integration {
     id: ts.Identifier;
     kind: IOKind;
     props: ts.PropertySignature[];
-  }> = [];
+  }> = [
+    { id: this.ids.inputInterface, kind: "input", props: [] },
+    { id: this.ids.posResponseInterface, kind: "positive", props: [] },
+    { id: this.ids.negResponseInterface, kind: "negative", props: [] },
+    { id: this.ids.encResponseInterface, kind: "encoded", props: [] },
+    { id: this.ids.responseInterface, kind: "response", props: [] },
+  ];
 
   protected makeAlias(
     schema: z.ZodTypeAny,
@@ -275,22 +281,6 @@ export class Integration {
 
     // export type Method = "get" | "post" | "put" | "delete" | "patch";
     this.program.push(makePublicLiteralType(this.ids.methodType, methods));
-
-    this.interfaces.push(
-      {
-        id: this.ids.inputInterface,
-        kind: "input",
-        props: [],
-      },
-      { id: this.ids.posResponseInterface, kind: "positive", props: [] },
-      { id: this.ids.negResponseInterface, kind: "negative", props: [] },
-      { id: this.ids.encResponseInterface, kind: "encoded", props: [] },
-      {
-        id: this.ids.responseInterface,
-        kind: "response",
-        props: [],
-      },
-    );
 
     // Single walk through the registry for making properties for the next three objects
     const jsonEndpoints: ts.PropertyAssignment[] = [];
