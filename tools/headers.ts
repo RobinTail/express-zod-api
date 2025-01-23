e004f19a20d2be5ab41aad9db131133077a373ce
Robin Tail <robin_tail@me.com>
2025-01-23 18:00:11 +0100

    tool: collecting well-known headers from IANA.ORG.

---
+++ tools/headers.ts
@@ -0,0 +1,28 @@
+import { writeFile } from "node:fs/promises";
+import { z } from "zod";
+
+// https://github.com/ladjs/message-headers/blob/master/cron.js
+const csv = await (
+  await fetch("https://www.iana.org/assignments/http-fields/field-names.csv")
+).text();
+
+const schema = z.object({
+  name: z.string().regex(/^[\w-]+$/),
+  category: z.enum(["permanent", "deprecated", "provisional", "obsoleted"]),
+});
+
+const lines = csv.split("\n").slice(1, -1);
+const headers = lines
+  .map((line) => {
+    const [name, category] = line.split(",").slice(0, 2);
+    return { name, category };
+  })
+  .filter((entry) => {
+    const { success } = schema.safeParse(entry);
+    if (!success) console.debug("excluding", entry);
+    return success;
+  });
+
+console.debug("CRC:", headers.length);
+
+await writeFile("src/headers.json", JSON.stringify(headers), "utf-8");
