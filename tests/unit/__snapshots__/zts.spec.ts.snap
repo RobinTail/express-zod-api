RobinTail/express-zod-api

commit 390b17059fda9ecd32f5de6038cfe1e2f99c154f

Merge intersected objects types in generated client (#2131)
This should prevent the "empty object type" in the generated client,
which is not really a good type.
`Record<string, never>` can not be used in this case, so I'm trying to
eliminate the root cause.

src/zts.ts
@@ -171,9 +171,15 @@ const onRecord: Producer = (
   );
 
 const onIntersection: Producer = (
-  { _def }: z.ZodIntersection<z.ZodTypeAny, z.ZodTypeAny>,
+  { _def: { left, right } }: z.ZodIntersection<z.ZodTypeAny, z.ZodTypeAny>,
   { next },
-) => f.createIntersectionTypeNode([_def.left, _def.right].map(next));
+) => {
+  const nodes = [left, right].map(next);
+  const areObjects = nodes.every(ts.isTypeLiteralNode);
+  return areObjects
+    ? f.createTypeLiteralNode(nodes.flatMap(({ members }) => members))
+    : f.createIntersectionTypeNode(nodes);
+};
 
 const onDefault: Producer = ({ _def }: z.ZodDefault<z.ZodTypeAny>, { next }) =>
   next(_def.innerType);

example/example.client.ts
@@ -3,7 +3,7 @@ type Type1 = {
   features: Type1;
 }[];
 
-type GetV1UserRetrieveInput = {} & {
+type GetV1UserRetrieveInput = {
   /** a numeric string containing the id of the user */
   id: string;
 };
@@ -29,7 +29,6 @@ type GetV1UserRetrieveResponse =
 
 type PatchV1UserIdInput = {
   key: string;
-} & {
   id: string;
   name: string;
   birthday: string;

tests/unit/__snapshots__/integration.spec.ts.snap
@@ -430,7 +430,7 @@ exports[`Integration > Should generate a client for example API 1`] = `
   features: Type1;
 }[];
 
-type GetV1UserRetrieveInput = {} & {
+type GetV1UserRetrieveInput = {
   /** a numeric string containing the id of the user */
   id: string;
 };
@@ -456,7 +456,6 @@ type GetV1UserRetrieveResponse =
 
 type PatchV1UserIdInput = {
   key: string;
-} & {
   id: string;
   name: string;
   birthday: string;
@@ -673,7 +672,7 @@ exports[`Integration > Should generate a types for example API 1`] = `
   features: Type1;
 }[];
 
-type GetV1UserRetrieveInput = {} & {
+type GetV1UserRetrieveInput = {
   /** a numeric string containing the id of the user */
   id: string;
 };
@@ -699,7 +698,6 @@ type GetV1UserRetrieveResponse =
 
 type PatchV1UserIdInput = {
   key: string;
-} & {
   id: string;
   name: string;
   birthday: string;

commit d3fe110927e5ab722ac58dc8d4848c4337824e74

Integration: handling duplicated props (#2353)
Fixes #2352 

The issue was introduced by
https://github.com/RobinTail/express-zod-api/pull/2131 and published as
[v20.15.3](https://github.com/RobinTail/express-zod-api/releases/tag/v20.15.3)

src/zts.ts
@@ -1,4 +1,4 @@
-import { chain, prop } from "ramda";
+import { chain, eqBy, path, prop, uniqWith } from "ramda";
 import ts from "typescript";
 import { z } from "zod";
 import { hasCoercion, tryToTransform } from "./common-helpers";
@@ -27,6 +27,18 @@ const samples = {
   [ts.SyntaxKind.StringKeyword]: "",
   [ts.SyntaxKind.UndefinedKeyword]: undefined,
 } satisfies Partial<Record<ts.KeywordTypeSyntaxKind, unknown>>;
+
+const nodePath = {
+  name: path([
+    "name" satisfies keyof ts.TypeElement,
+    "text" satisfies keyof Exclude<
+      NonNullable<ts.TypeElement["name"]>,
+      ts.ComputedPropertyName
+    >,
+  ]),
+  type: path(["type" satisfies keyof ts.PropertySignature]),
+  optional: path(["questionToken" satisfies keyof ts.TypeElement]),
+};
 
 const onLiteral: Producer = ({ value }: z.ZodLiteral<LiteralType>) =>
   makeLiteralType(value);
@@ -135,15 +147,29 @@ const onRecord: Producer = (
   { next },
 ) => ensureTypeNode("Record", [keySchema, valueSchema].map(next));
 
+/** @throws Error */
+const tryFlattenIntersection = (nodes: ts.TypeNode[]) => {
+  const areObjects = nodes.every(ts.isTypeLiteralNode);
+  if (!areObjects) throw new Error("Not objects");
+  const members = chain(prop("members"), nodes);
+  const uniqs = uniqWith((...props) => {
+    if (!eqBy(nodePath.name, ...props)) return false;
+    if (eqBy(nodePath.type, ...props) && eqBy(nodePath.optional, ...props))
+      return true;
+    throw new Error("Has conflicting prop");
+  }, members);
+  return f.createTypeLiteralNode(uniqs);
+};
+
 const onIntersection: Producer = (
   { _def: { left, right } }: z.ZodIntersection<z.ZodTypeAny, z.ZodTypeAny>,
   { next },
 ) => {
   const nodes = [left, right].map(next);
-  const areObjects = nodes.every(ts.isTypeLiteralNode);
-  return areObjects
-    ? f.createTypeLiteralNode(chain(prop("members"), nodes)) // similar to flattened pluck()
-    : f.createIntersectionTypeNode(nodes);
+  try {
+    return tryFlattenIntersection(nodes);
+  } catch {}
+  return f.createIntersectionTypeNode(nodes);
 };
 
 const onDefault: Producer = ({ _def }: z.ZodDefault<z.ZodTypeAny>, { next }) =>

tests/unit/__snapshots__/documentation-helpers.spec.ts.snap
@@ -369,6 +369,23 @@ exports[`Documentation helpers > depictIntersection() > should fall back to allO
 }
 `;
 
+exports[`Documentation helpers > depictIntersection() > should flatten objects with same prop of same type 1`] = `
+{
+  "properties": {
+    "one": {
+      "format": "double",
+      "maximum": 1.7976931348623157e+308,
+      "minimum": -1.7976931348623157e+308,
+      "type": "number",
+    },
+  },
+  "required": [
+    "one",
+  ],
+  "type": "object",
+}
+`;
+
 exports[`Documentation helpers > depictIntersection() > should flatten three object schemas with examples 1`] = `
 {
   "examples": [

tests/unit/__snapshots__/zts.spec.ts.snap
@@ -79,6 +79,66 @@ exports[`zod-to-ts > Example > should produce the expected results 1`] = `
 }"
 `;
 
+exports[`zod-to-ts > Issue #2352: intersection of objects having same prop %# > should deduplicate the prop with a same name 1`] = `
+"{
+    query: string;
+}"
+`;
+
+exports[`zod-to-ts > Issue #2352: intersection of objects having same prop %# > should deduplicate the prop with a same name 2`] = `
+"{
+    query: string;
+}"
+`;
+
+exports[`zod-to-ts > Issue #2352: intersection of objects having same prop %# > should deduplicate the prop with a same name 3`] = `
+"{
+    query: string;
+}"
+`;
+
+exports[`zod-to-ts > Issue #2352: intersection of objects having same prop %# > should deduplicate the prop with a same name 4`] = `
+"{
+    query: {};
+}"
+`;
+
+exports[`zod-to-ts > Issue #2352: intersection of objects having same prop %# > should not flatten the result for objects with a conflicting prop 0 1`] = `
+"{
+    query: string;
+} & {
+    query: number;
+}"
+`;
+
+exports[`zod-to-ts > Issue #2352: intersection of objects having same prop %# > should not flatten the result for objects with a conflicting prop 1 1`] = `
+"{
+    query: {
+        sub: string;
+    };
+} & {
+    query: {
+        sub: number;
+    };
+}"
+`;
+
+exports[`zod-to-ts > Issue #2352: intersection of objects having same prop %# > should not flatten the result for objects with a conflicting prop 2 1`] = `
+"{
+    query: string;
+} & {
+    query?: string | undefined;
+}"
+`;
+
+exports[`zod-to-ts > Issue #2352: intersection of objects having same prop %# > should not flatten the result for objects with a conflicting prop 3 1`] = `
+"{
+    query: string;
+} & {
+    query?: string | undefined;
+}"
+`;
+
 exports[`zod-to-ts > PrimitiveSchema > outputs correct typescript 1`] = `
 "{
     string: string;

tests/unit/documentation-helpers.spec.ts
@@ -294,6 +294,15 @@ describe("Documentation helpers", () => {
       ).toMatchSnapshot();
     });
 
+    test("should flatten objects with same prop of same type", () => {
+      expect(
+        depictIntersection(
+          z.object({ one: z.number() }).and(z.object({ one: z.number() })),
+          requestCtx,
+        ),
+      ).toMatchSnapshot();
+    });
+
     test("should NOT flatten object schemas having conflicting props", () => {
       expect(
         depictIntersection(

tests/unit/zts.spec.ts
@@ -267,6 +267,44 @@ describe("zod-to-ts", () => {
     });
   });
 
+  describe("Issue #2352: intersection of objects having same prop %#", () => {
+    test.each([
+      [z.string(), z.string()],
+      [z.string().nonempty(), z.string().email()],
+      [z.string().transform(Number), z.string().pipe(z.coerce.date())],
+      [z.object({}), z.object({})],
+    ])("should deduplicate the prop with a same name", (a, b) => {
+      const schema = z.object({ query: a }).and(z.object({ query: b }));
+      const node = zodToTs(schema, { ctx });
+      expect(printNodeTest(node)).toMatchSnapshot();
+    });
+
+    test.each([
+      [
+        z.object({ query: z.string() }),
+        z.object({ query: z.number() }), // prop type
+      ],
+      [
+        z.object({ query: z.object({ sub: z.string() }) }),
+        z.object({ query: z.object({ sub: z.number() }) }), // different complex
+      ],
+      [
+        z.object({ query: z.string() }),
+        z.object({ query: z.string().optional() }), // question mark
+      ],
+      [
+        z.object({ query: z.string() }),
+        z.object({ query: z.string() }).partial(), // also question mark
+      ],
+    ])(
+      "should not flatten the result for objects with a conflicting prop %#",
+      (a, b) => {
+        const node = zodToTs(a.and(b), { ctx });
+        expect(printNodeTest(node)).toMatchSnapshot();
+      },
+    );
+  });
+
   describe("PrimitiveSchema", () => {
     const primitiveSchema = z.object({
       string: z.string(),