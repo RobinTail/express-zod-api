9b4cd686e85cad94584f22f12e65ee2b17488172
Robin Tail <robin_tail@me.com>
2025-01-31 08:10:34 +0100

    Updating compat test.

--- tests/compat/migration.spec.ts
+++ tests/compat/migration.spec.ts
@@ -3,6 +3,6 @@ import { readFile } from "node:fs/promises";
 describe("Migration", () => {
   test("should fix the import", async () => {
     const fixed = await readFile("./sample.ts", "utf-8");
-    expect(fixed).toBe(`placeholder\n`);
+    expect(fixed).toBe(`const test: HeaderSecurity = {};\n`);
   });
 });
