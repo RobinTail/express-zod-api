RobinTail/express-zod-api

commit cb506a13a1e80343d04938792d784c444c66bcca

feat: 405: Method not allowed (#2366)
There is an HTTP code 405 for routes that exist, but require different
method.
https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/405

Unfortunately, `req.route` is `undefined` when within middlewares, so
have to use `app.all()` as recommended.

src/config-type.ts
@@ -38,6 +38,14 @@ export interface CommonConfig {
    */
   cors: boolean | HeadersProvider;
   /**
+   * @desc How to respond to a request that uses a wrong method to an existing endpoint
+   * @example 404 — Not found
+   * @example 405 — Method not allowed, incl. the "Allowed" header with a list of methods
+   * @default 404
+   * @todo consider changing default to 405 in v23
+   * */
+  wrongMethodBehavior?: 404 | 405;
+  /**
    * @desc The ResultHandler to use for handling routing, parsing and upload errors
    * @default defaultResultHandler
    * @see defaultResultHandler

src/routing.ts
@@ -1,4 +1,5 @@
 import { IRouter, RequestHandler } from "express";
+import createHttpError from "http-errors";
 import { isProduction } from "./common-helpers";
 import { CommonConfig } from "./config-type";
 import { ContentType } from "./content-type";
@@ -16,6 +17,18 @@ export interface Routing {
 
 export type Parsers = Partial<Record<ContentType, RequestHandler[]>>;
 
+/** @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/405 */
+export const createWrongMethodHandler =
+  (allowedMethods: Array<Method | AuxMethod>): RequestHandler =>
+  ({ method }, res, next) => {
+    const Allow = allowedMethods.join(", ").toUpperCase();
+    res.set({ Allow }); // in case of a custom errorHandler configured that does not care about headers in error
+    const error = createHttpError(405, `${method} is not allowed`, {
+      headers: { Allow },
+    });
+    next(error);
+  };
+
 export const initRouting = ({
   app,
   getLogger,
@@ -30,7 +43,7 @@ export const initRouting = ({
   parsers?: Parsers;
 }) => {
   const doc = new Diagnostics(getLogger());
-  const corsedPaths = new Set<string>();
+  const familiar = new Map<string, Array<Method | AuxMethod>>();
   const onEndpoint: OnEndpoint = (endpoint, path, method, siblingMethods) => {
     if (!isProduction()) doc.check(endpoint, { path, method });
     const matchingParsers = parsers?.[endpoint.getRequestType()] || [];
@@ -56,11 +69,18 @@ export const initRouting = ({
       }
       return endpoint.execute({ request, response, logger, config });
     };
-    if (config.cors && !corsedPaths.has(path)) {
-      app.options(path, ...matchingParsers, handler);
-      corsedPaths.add(path);
+    if (!familiar.has(path)) {
+      familiar.set(path, []);
+      if (config.cors) {
+        app.options(path, ...matchingParsers, handler);
+        familiar.get(path)?.push("options");
+      }
     }
+    familiar.get(path)?.push(method);
     app[method](path, ...matchingParsers, handler);
   };
   walkRouting({ routing, onEndpoint, onStatic: app.use.bind(app) });
+  if (config.wrongMethodBehavior !== 405) return;
+  for (const [path, allowedMethods] of familiar.entries())
+    app.all(path, createWrongMethodHandler(allowedMethods));
 };

tests/system/system.spec.ts
@@ -112,6 +112,7 @@ describe("App in production mode", async () => {
   const config = createConfig({
     http: { listen: port },
     compression: { threshold: 1 },
+    wrongMethodBehavior: 405,
     beforeRouting: ({ app, getLogger }) => {
       depd("express")("Sample deprecation message");
       app.use((req, {}, next) => {
@@ -300,6 +301,13 @@ describe("App in production mode", async () => {
   });
 
   describe("Protocol", () => {
+    test("Should fail on invalid path", async () => {
+      const response = await fetch(`http://127.0.0.1:${port}/v1/wrong`);
+      expect(response.status).toBe(404);
+      const json = await response.json();
+      expect(json).toMatchSnapshot();
+    });
+
     test("Should fail on invalid method", async () => {
       const response = await fetch(`http://127.0.0.1:${port}/v1/test`, {
         method: "PUT",
@@ -311,7 +319,8 @@ describe("App in production mode", async () => {
           something: "joke",
         }),
       });
-      expect(response.status).toBe(404);
+      expect(response.status).toBe(405);
+      expect(response.headers.get("Allow")).toBe("GET, POST");
       const json = await response.json();
       expect(json).toMatchSnapshot();
     });

src/result-handler.ts
@@ -116,10 +116,13 @@ export const defaultResultHandler = new ResultHandler({
     if (error) {
       const httpError = ensureHttpError(error);
       logServerError(httpError, logger, request, input);
-      return void response.status(httpError.statusCode).json({
-        status: "error",
-        error: { message: getPublicErrorMessage(httpError) },
-      });
+      return void response
+        .status(httpError.statusCode)
+        .set(httpError.headers)
+        .json({
+          status: "error",
+          error: { message: getPublicErrorMessage(httpError) },
+        });
     }
     response
       .status(defaultStatusCodes.positive)

src/server-helpers.ts
@@ -27,7 +27,7 @@ interface HandlerCreatorParams {
   getLogger: GetLogger;
 }
 
-export const createParserFailureHandler =
+export const createCatcher =
   ({ errorHandler, getLogger }: HandlerCreatorParams): ErrorRequestHandler =>
   async (error, request, response, next) => {
     if (!error) return next();

src/server.ts
@@ -16,7 +16,7 @@ import { Parsers, Routing, initRouting } from "./routing";
 import {
   createLoggingMiddleware,
   createNotFoundHandler,
-  createParserFailureHandler,
+  createCatcher,
   createUploadParsers,
   makeGetLogger,
   installDeprecationListener,
@@ -40,12 +40,12 @@ const makeCommonEntities = (config: CommonConfig) => {
   const getLogger = makeGetLogger(logger);
   const commons = { getLogger, errorHandler };
   const notFoundHandler = createNotFoundHandler(commons);
-  const parserFailureHandler = createParserFailureHandler(commons);
+  const catcher = createCatcher(commons);
   return {
     ...commons,
     logger,
     notFoundHandler,
-    parserFailureHandler,
+    catcher,
     loggingMiddleware,
   };
 };
@@ -63,13 +63,8 @@ export const attachRouting = (config: AppConfig, routing: Routing) => {
 };
 
 export const createServer = async (config: ServerConfig, routing: Routing) => {
-  const {
-    logger,
-    getLogger,
-    notFoundHandler,
-    parserFailureHandler,
-    loggingMiddleware,
-  } = makeCommonEntities(config);
+  const { logger, getLogger, notFoundHandler, catcher, loggingMiddleware } =
+    makeCommonEntities(config);
   const app = express().disable("x-powered-by").use(loggingMiddleware);
 
   if (config.compression) {
@@ -91,7 +86,7 @@ export const createServer = async (config: ServerConfig, routing: Routing) => {
 
   await config.beforeRouting?.({ app, getLogger });
   initRouting({ app, routing, getLogger, config, parsers });
-  app.use(parserFailureHandler, notFoundHandler);
+  app.use(catcher, notFoundHandler);
 
   const created: Array<http.Server | https.Server> = [];
   const makeStarter =

tests/express-mock.ts
@@ -19,6 +19,7 @@ const appMock = {
   delete: vi.fn(),
   options: vi.fn(),
   init: vi.fn(),
+  all: vi.fn(),
 };
 
 const expressMock = () => appMock;

tests/system/__snapshots__/system.spec.ts.snap
@@ -60,7 +60,16 @@ exports[`App in production mode > Positive > Should compress the response in cas
 exports[`App in production mode > Protocol > Should fail on invalid method 1`] = `
 {
   "error": {
-    "message": "Can not PUT /v1/test",
+    "message": "PUT is not allowed",
+  },
+  "status": "error",
+}
+`;
+
+exports[`App in production mode > Protocol > Should fail on invalid path 1`] = `
+{
+  "error": {
+    "message": "Can not GET /v1/wrong",
   },
   "status": "error",
 }

commit b394427fe7f11c816ac5ae1d59f67b4d9b1620a3

Change default response to `405` for wrong method (#2372)
Feature was introduced in #2366 .
Making it default behavior in next major.

src/config-type.ts
@@ -41,8 +41,7 @@ export interface CommonConfig {
    * @desc How to respond to a request that uses a wrong method to an existing endpoint
    * @example 404 — Not found
    * @example 405 — Method not allowed, incl. the "Allow" header with a list of methods
-   * @default 404
-   * @todo consider changing default to 405 in v23
+   * @default 405
    * */
   wrongMethodBehavior?: 404 | 405;
   /**

src/migration.ts
@@ -7,12 +7,14 @@ import {
 
 interface Queries {
   headerSecurity: TSESTree.Identifier;
+  createConfig: TSESTree.ObjectExpression;
 }
 
 type Listener = keyof Queries;
 
 const queries: Record<Listener, string> = {
   headerSecurity: `${NT.Identifier}[name='CustomHeaderSecurity']`,
+  createConfig: `${NT.CallExpression}[callee.name='createConfig'] > ${NT.ObjectExpression}`,
 };
 
 const listen = <
@@ -35,6 +37,7 @@ const v23 = ESLintUtils.RuleCreator.withoutDocs({
     schema: [],
     messages: {
       change: "change {{ subject }} from {{ from }} to {{ to }}",
+      add: `add {{ subject }} to {{ to }}`,
     },
   },
   defaultOptions: [],
@@ -47,6 +50,28 @@ const v23 = ESLintUtils.RuleCreator.withoutDocs({
           data: { subject: "interface", from: node.name, to: "HeaderSecurity" },
           fix: (fixer) => fixer.replaceText(node, "HeaderSecurity"),
         }),
+      createConfig: (node) => {
+        const wmProp = node.properties.find(
+          (prop) =>
+            prop.type === NT.Property &&
+            prop.key.type === NT.Identifier &&
+            prop.key.name === "wrongMethodBehavior",
+        );
+        if (wmProp) return;
+        ctx.report({
+          node,
+          messageId: "add",
+          data: {
+            subject: "wrongMethodBehavior property",
+            to: "configuration",
+          },
+          fix: (fixer) =>
+            fixer.insertTextAfterRange(
+              [node.range[0], node.range[0] + 1],
+              "wrongMethodBehavior: 404,",
+            ),
+        });
+      },
     }),
 });
 

src/routing.ts
@@ -80,7 +80,7 @@ export const initRouting = ({
     app[method](path, ...matchingParsers, handler);
   };
   walkRouting({ routing, onEndpoint, onStatic: app.use.bind(app) });
-  if (config.wrongMethodBehavior !== 405) return;
+  if (config.wrongMethodBehavior === 404) return;
   for (const [path, allowedMethods] of familiar.entries())
     app.all(path, createWrongMethodHandler(allowedMethods));
 };

tests/system/system.spec.ts
@@ -112,7 +112,6 @@ describe("App in production mode", async () => {
   const config = createConfig({
     http: { listen: port },
     compression: { threshold: 1 },
-    wrongMethodBehavior: 405,
     beforeRouting: ({ app, getLogger }) => {
       depd("express")("Sample deprecation message");
       app.use((req, {}, next) => {

tests/unit/__snapshots__/migration.spec.ts.snap
@@ -9,6 +9,7 @@ exports[`Migration > should consist of one rule being the major version of the p
       "meta": {
         "fixable": "code",
         "messages": {
+          "add": "add {{ subject }} to {{ to }}",
           "change": "change {{ subject }} from {{ from }} to {{ to }}",
         },
         "schema": [],

tests/unit/migration.spec.ts
@@ -18,7 +18,10 @@ describe("Migration", () => {
   });
 
   tester.run("v23", migration.rules.v23, {
-    valid: [`import { HeaderSecurity } from "express-zod-api";`],
+    valid: [
+      `import { HeaderSecurity } from "express-zod-api";`,
+      `createConfig({ wrongMethodBehavior: 405 });`,
+    ],
     invalid: [
       {
         code: `const security: CustomHeaderSecurity = {};`,
@@ -30,6 +33,19 @@ describe("Migration", () => {
               subject: "interface",
               from: "CustomHeaderSecurity",
               to: "HeaderSecurity",
+            },
+          },
+        ],
+      },
+      {
+        code: `createConfig({});`,
+        output: `createConfig({wrongMethodBehavior: 404,});`,
+        errors: [
+          {
+            messageId: "add",
+            data: {
+              subject: "wrongMethodBehavior property",
+              to: "configuration",
             },
           },
         ],