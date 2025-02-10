46036adf575c6b376081a49c119a3fde30ead3ec
Robin Tail <robin_tail@me.com>
2025-02-10 14:52:01 +0100

    rm compat-test dir.

--- compat-test/eslint.config.js
+++
@@ -1,7 +0,0 @@
-import parser from "@typescript-eslint/parser";
-import migration from "express-zod-api/migration";
-
-export default [
-  { languageOptions: { parser }, plugins: { migration } },
-  { files: ["**/*.ts"], rules: { "migration/v22": "error" } },
-];
