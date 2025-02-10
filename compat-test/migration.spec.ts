9cbb9087ebadeb22f44ec26b256f79a9690fd206
Robin Tail <robin_tail@me.com>
2025-02-10 13:45:39 +0100

    Migration test.

--- compat-test/migration.spec.ts
+++ compat-test/migration.spec.ts
@@ -3,6 +3,6 @@ import { readFile } from "node:fs/promises";
 describe("Migration", () => {
   test("should fix the import", async () => {
     const fixed = await readFile("./sample.ts", "utf-8");
-    expect(fixed).toBe(`client.provide("get /v1/test", {id: 10});\n`);
+    expect(fixed).toBe('client.provide("get /v1/test", {id: 10});\n');
   });
 });
