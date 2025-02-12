756c9a4262e328b519b27271b34bf47ace19d0f6
Robin Tail <robin_tail@me.com>
2025-02-12 08:53:23 +0100

    Tests for Routable.

--- express-zod-api/tests/routable.spec.ts
+++ express-zod-api/tests/routable.spec.ts
@@ -1,21 +1,45 @@
 import { z } from "zod";
-import { defaultEndpointsFactory, DependsOnMethod } from "../src";
+import { defaultEndpointsFactory, DependsOnMethod, ServeStatic } from "../src";
 
 const endpoint = defaultEndpointsFactory.build({
   output: z.object({}),
   handler: vi.fn(),
 });
 
-// @todo add tests for Routable
-describe.each([new DependsOnMethod({ get: endpoint }), endpoint])(
-  "Nestable mixin %#",
+const methodDepending = new DependsOnMethod({ get: endpoint });
+
+const staticServer = new ServeStatic("assets", {
+  dotfiles: "deny",
+  index: false,
+  redirect: false,
+});
+
+describe.each([methodDepending, endpoint, staticServer])(
+  "Routable mixin %#",
   (subject) => {
-    test("should have .nest() method returning Routing arrangement", () => {
+    describe(".clone()", () => {
+      test("should return a copy of the entity", () => {
+        expect(subject.clone()).toEqual(subject);
+      });
+    });
+
+    describe(".deprecated()", () => {
+      test("should deprecate the entity", () => {
+        expect(subject.isDeprecated).toBe(false);
+        expect(subject.deprecated().isDeprecated).toBe(true);
+      });
+    });
+  },
+);
+
+describe.each([methodDepending, endpoint])("Nestable mixin %#", (subject) => {
+  describe(".nest()", () => {
+    test("should return Routing arrangement", () => {
       expect(subject).toHaveProperty("nest", expect.any(Function));
       expect(subject.nest({ subpath: endpoint })).toEqual({
         "": subject,
         subpath: endpoint,
       });
     });
-  },
-);
+  });
+});
