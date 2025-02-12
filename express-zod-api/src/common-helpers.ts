d311a395ba48a6f19d5d2b1106b0d0bf1c9b8e2f
Robin Tail <robin_tail@me.com>
2025-02-12 18:26:50 +0100

    Fixed metadata constraints for pullExampleProps().

--- express-zod-api/src/common-helpers.ts
+++ express-zod-api/src/common-helpers.ts
@@ -83,12 +83,14 @@ export const getMessageFromError = (error: Error): string => {
 /** Takes the original unvalidated examples from the properties of ZodObject schema shape */
 export const pullExampleProps = <T extends z.SomeZodObject>(subject: T) =>
   Object.entries(subject.shape).reduce<Partial<z.input<T>>[]>(
-    (acc, [key, schema]) =>
-      combinations(
+    (acc, [key, schema]) => {
+      const { _def } = schema as z.ZodType;
+      return combinations(
         acc,
-        (schema._def[metaSymbol]?.examples || []).map(objOf(key)),
+        (_def[metaSymbol]?.examples || []).map(objOf(key)),
         ([left, right]) => ({ ...left, ...right }),
-      ),
+      );
+    },
     [],
   );
 
