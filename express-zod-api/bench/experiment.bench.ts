d9a2b1302402bc76d9436ff4b0f3d443e95cacc8
Robin Tail <robin_tail@me.com>
2025-03-27 09:38:33 +0100

    Feat: async option for BuiltinLogger, env depending defaults.

--- express-zod-api/bench/experiment.bench.ts
+++ express-zod-api/bench/experiment.bench.ts
@@ -1,31 +1,11 @@
-import * as R from "ramda";
 import { bench } from "vitest";
 
 describe("Experiment for key lookup", () => {
-  const subject = {
-    a: 1,
-    b: 2,
-    c: 3,
-  };
-
-  bench("in", () => {
-    return void ("a" in subject && "b" in subject && "c" in subject);
+  bench("sync", () => {
+    console.log("test", [1, 2, 3]);
   });
 
-  bench("R.has", () => {
-    return void (
-      R.has("a", subject) &&
-      R.has("b", subject) &&
-      R.has("c", subject)
-    );
-  });
-
-  bench("Object.keys + includes", () => {
-    const keys = Object.keys(subject);
-    return void (
-      keys.includes("a") &&
-      keys.includes("b") &&
-      keys.includes("c")
-    );
+  bench("async", () => {
+    setImmediate(console.log, "test", [1, 2, 3]);
   });
 });
