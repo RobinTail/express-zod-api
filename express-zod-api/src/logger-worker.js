4f8b0047f5065d1b568990457e30cbd176940df5
Robin Tail <robin_tail@me.com>
2025-03-30 08:46:52 +0200

    Batching logs within worker.

--- express-zod-api/src/logger-worker.js
+++ express-zod-api/src/logger-worker.js
@@ -3,9 +3,19 @@ import { parentPort } from "node:worker_threads";
 if (!parentPort)
   throw new Error("Logger worker must be run as a Worker Thread.");
 
+const buffer = [];
+
+function flush() {
+  if (buffer.length > 0) {
+    console.log(buffer.join("\n"));
+    buffer.length = 0;
+  }
+}
+
 /**
  * @param {string[]} output
  */
-const onMessage = (output) => console.log(output.join(" "));
+const onMessage = (output) => buffer.push(output.join(" "));
 
 parentPort.on("message", onMessage);
+setInterval(flush, 1000);
