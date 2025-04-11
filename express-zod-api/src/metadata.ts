3850990e275957f132f0362f8fe5f0f1126011ca
Robin Tail <robin_tail@me.com>
2025-04-11 11:54:44 +0200

    Hacking into metadata system.

--- express-zod-api/src/metadata.ts
+++ express-zod-api/src/metadata.ts
@@ -1,8 +1,15 @@
-import { combinations } from "./common-helpers";
-import { z } from "zod";
 import * as R from "ramda";
+import { z } from "zod";
+import { combinations } from "./common-helpers";
 
 export const metaSymbol = Symbol.for("express-zod-api");
+
+// @todo mv to plugin
+declare module "@zod/core" {
+  interface GlobalMeta {
+    [metaSymbol]?: Metadata;
+  }
+}
 
 export interface Metadata {
   examples: unknown[];
@@ -12,27 +19,37 @@ export interface Metadata {
   isDeprecated?: boolean;
 }
 
-/** @link https://github.com/colinhacks/zod/blob/3e4f71e857e75da722bd7e735b6d657a70682df2/src/types.ts#L485 */
-export const cloneSchema = <T extends z.ZodType>(schema: T) => {
-  const copy = schema.describe(schema.description as string);
-  copy._def[metaSymbol] = // clone for deep copy, issue #827
-    R.clone(copy._def[metaSymbol]) || ({ examples: [] } satisfies Metadata);
-  return copy;
-};
+/**
+ * @link https://github.com/colinhacks/zod/blob/3e4f71e857e75da722bd7e735b6d657a70682df2/src/types.ts#L485
+ * @todo probably no longer needed
+ * */
+export const cloneSchema = <T extends z.ZodType>(schema: T) =>
+  schema.meta({
+    description: schema.description,
+    // clone for deep copy, issue #827
+    [metaSymbol]: R.clone(schema.meta()?.[metaSymbol]) || { examples: [] },
+  });
 
 export const copyMeta = <A extends z.ZodType, B extends z.ZodType>(
   src: A,
   dest: B,
 ): B => {
-  if (!(metaSymbol in src._def)) return dest; // ensure metadata in src below
-  const result = cloneSchema(dest); // ensures metadata in result below
-  result._def[metaSymbol]!.examples = combinations(
-    result._def[metaSymbol]!.examples,
-    src._def[metaSymbol]!.examples,
-    ([destExample, srcExample]) =>
-      typeof destExample === "object" && typeof srcExample === "object"
-        ? R.mergeDeepRight({ ...destExample }, { ...srcExample })
-        : srcExample, // not supposed to be called on non-object schemas
-  );
-  return result;
+  const srcMeta = src.meta()?.[metaSymbol];
+  const destMeta = dest.meta()?.[metaSymbol];
+  if (!srcMeta) return dest; // ensure metadata in src below
+
+  return dest.meta({
+    description: dest.description,
+    [metaSymbol]: {
+      ...(destMeta || {}),
+      examples: combinations(
+        srcMeta.examples || [],
+        destMeta?.examples || [],
+        ([destExample, srcExample]) =>
+          typeof destExample === "object" && typeof srcExample === "object"
+            ? R.mergeDeepRight({ ...destExample }, { ...srcExample })
+            : srcExample, // not supposed to be called on non-object schemas
+      ),
+    },
+  });
 };
