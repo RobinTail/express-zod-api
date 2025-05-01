RobinTail/express-zod-api

commit fb39add7ca836f3ffabf92eccfe3b5b2b609bbe1

Replace `extractObjectSchema` with JSON Schema flattening (#2595)
In most cases we are only interested in property names of the flattened
IOSchema.
I came to realization that it might be possible to use `toJSONSchema`
for that.

express-zod-api/src/documentation-helpers.ts
@@ -1,4 +1,5 @@
 import type {
+  $ZodObject,
   $ZodPipe,
   $ZodTransform,
   $ZodTuple,
@@ -44,7 +45,8 @@ import { ezDateOutBrand } from "./date-out-schema";
 import { contentTypes } from "./content-type";
 import { DocumentationError } from "./errors";
 import { ezFileBrand } from "./file-schema";
-import { extractObjectSchema, IOSchema } from "./io-schema";
+import { IOSchema } from "./io-schema";
+import { flattenIO } from "./json-schema-helpers";
 import { Alternatives } from "./logical-container";
 import { metaSymbol } from "./metadata";
 import { Method } from "./method";
@@ -80,13 +82,7 @@ export type IsHeader = (
 
 export type BrandHandling = Record<string | symbol, Depicter>;
 
-interface ReqResHandlingProps<S extends $ZodType>
-  extends Omit<OpenAPIContext, "isResponse"> {
-  schema: S;
-  composition: "inline" | "components";
-  description?: string;
-  brandHandling?: BrandHandling;
-}
+type ReqResCommons = Omit<OpenAPIContext, "isResponse">;
 
 const shortDescriptionLimit = 50;
 const isoDateDocumentationUrl =
@@ -164,6 +160,7 @@ const canMerge = R.pipe(
   R.isEmpty,
 );
 
+/** @todo DNRY with flattenIO() */
 const intersect = (
   children: Array<JSONSchema.BaseSchema>,
 ): JSONSchema.ObjectSchema => {
@@ -208,6 +205,21 @@ export const depictLiteral: Depicter = ({ jsonSchema }) => ({
   type: typeof (jsonSchema.const || jsonSchema.enum?.[0]),
   ...jsonSchema,
 });
+
+export const depictObject: Depicter = (
+  { zodSchema, jsonSchema },
+  { isResponse },
+) => {
+  if (isResponse) return jsonSchema;
+  if (!isSchema<$ZodObject>(zodSchema, "object")) return jsonSchema;
+  const { required = [] } = jsonSchema as JSONSchema.ObjectSchema;
+  const result: string[] = [];
+  for (const key of required) {
+    const valueSchema = zodSchema._zod.def.shape[key];
+    if (valueSchema && !doesAccept(valueSchema, undefined)) result.push(key);
+  }
+  return { ...jsonSchema, required: result };
+};
 
 const ensureCompliance = ({
   $ref,
@@ -306,7 +318,7 @@ export const depictPipeline: Depicter = ({ zodSchema, jsonSchema }, ctx) => {
     ctx.isResponse ? "in" : "out"
   ];
   if (!isSchema<$ZodTransform>(target, "transform")) return jsonSchema;
-  const opposingDepiction = depict(opposite, { ctx });
+  const opposingDepiction = ensureCompliance(depict(opposite, { ctx }));
   if (isSchemaObject(opposingDepiction)) {
     if (!ctx.isResponse) {
       const { type: opposingType, ...rest } = opposingDepiction;
@@ -347,39 +359,6 @@ const enumerateExamples = (examples: unknown[]): ExamplesObject | undefined =>
       )
     : undefined;
 
-export const depictExamples = (
-  schema: $ZodType,
-  isResponse: boolean,
-  omitProps: string[] = [],
-): ExamplesObject | undefined =>
-  R.pipe(
-    getExamples,
-    R.map(
-      R.when(
-        (one): one is FlatObject => isObject(one) && !Array.isArray(one),
-        R.omit(omitProps),
-      ),
-    ),
-    enumerateExamples,
-  )({
-    schema,
-    variant: isResponse ? "parsed" : "original",
-    validate: true,
-    pullProps: true,
-  });
-
-export const depictParamExamples = (
-  schema: z.ZodType,
-  param: string,
-): ExamplesObject | undefined => {
-  return R.pipe(
-    getExamples,
-    R.filter(R.both(isObject, R.has(param))),
-    R.pluck(param),
-    enumerateExamples,
-  )({ schema, variant: "original", validate: true, pullProps: true });
-};
-
 export const defaultIsHeader = (
   name: string,
   familiar?: string[],
@@ -391,20 +370,22 @@ export const defaultIsHeader = (
 export const depictRequestParams = ({
   path,
   method,
-  schema,
+  request,
   inputSources,
   makeRef,
   composition,
-  brandHandling,
   isHeader,
   security,
   description = `${method.toUpperCase()} ${path} Parameter`,
-}: ReqResHandlingProps<IOSchema> & {
+}: ReqResCommons & {
+  composition: "inline" | "components";
+  description?: string;
+  request: JSONSchema.BaseSchema;
   inputSources: InputSource[];
   isHeader?: IsHeader;
   security?: Alternatives<Security>;
 }) => {
-  const objectSchema = extractObjectSchema(schema);
+  const flat = flattenIO(request);
   const pathParams = getRoutePathParams(path);
   const isQueryEnabled = inputSources.includes("query");
   const areParamsEnabled = inputSources.includes("params");
@@ -419,8 +400,8 @@ export const depictRequestParams = ({
     areHeadersEnabled &&
     (isHeader?.(name, method, path) ?? defaultIsHeader(name, securityHeaders));
 
-  return Object.entries(objectSchema.shape).reduce<ParameterObject[]>(
-    (acc, [name, paramSchema]) => {
+  return Object.entries(flat.properties).reduce<ParameterObject[]>(
+    (acc, [name, jsonSchema]) => {
       const location = isPathParam(name)
         ? "path"
         : isHeaderParam(name)
@@ -429,22 +410,26 @@ export const depictRequestParams = ({
             ? "query"
             : undefined;
       if (!location) return acc;
-      const depicted = depict(paramSchema, {
-        rules: { ...brandHandling, ...depicters },
-        ctx: { isResponse: false, makeRef, path, method },
-      });
+      const depicted = ensureCompliance(jsonSchema);
       const result =
         composition === "components"
-          ? makeRef(paramSchema, depicted, makeCleanId(description, name))
+          ? makeRef(jsonSchema, depicted, makeCleanId(description, name))
           : depicted;
       return acc.concat({
         name,
         in: location,
-        deprecated: globalRegistry.get(paramSchema)?.deprecated,
-        required: !doesAccept(paramSchema, undefined),
+        deprecated: jsonSchema.deprecated,
+        required: flat.required.includes(name),
         description: depicted.description || description,
         schema: result,
-        examples: depictParamExamples(objectSchema, name),
+        examples: enumerateExamples(
+          isSchemaObject(depicted) && depicted.examples?.length
+            ? depicted.examples // own examples or from the flat:
+            : R.pluck(
+                name,
+                flat.examples.filter(R.both(isObject, R.has(name))),
+              ),
+        ),
       });
     },
     [],
@@ -462,6 +447,7 @@ const depicters: Partial<Record<FirstPartyKind | ProprietaryBrand, Depicter>> =
     pipe: depictPipeline,
     literal: depictLiteral,
     enum: depictEnum,
+    object: depictObject,
     [ezDateInBrand]: depictDateIn,
     [ezDateOutBrand]: depictDateOut,
     [ezUploadBrand]: depictUpload,
@@ -477,6 +463,7 @@ const onEach: Depicter = ({ zodSchema, jsonSchema }, { isResponse }) => {
     schema: zodSchema,
     variant: isResponse ? "parsed" : "original",
     validate: true,
+    pullProps: true,
   });
   if (examples.length) result.examples = examples.slice();
   return result;
@@ -506,7 +493,7 @@ const fixReferences = (
     }
     if (R.is(Array, entry)) stack.push(...R.values(entry));
   }
-  return ensureCompliance(subject);
+  return subject;
 };
 
 /** @link https://github.com/colinhacks/zod/issues/4275 */
@@ -574,11 +561,6 @@ export const excludeParamsFromDepiction = (
   return [result, hasRequired || Boolean(result.required?.length)];
 };
 
-export const excludeExamplesFromDepiction = (
-  depicted: SchemaObject | ReferenceObject,
-): SchemaObject | ReferenceObject =>
-  isReferenceObject(depicted) ? depicted : R.omit(["examples"], depicted);
-
 export const depictResponse = ({
   method,
   path,
@@ -593,25 +575,34 @@ export const depictResponse = ({
   description = `${method.toUpperCase()} ${path} ${ucFirst(variant)} response ${
     hasMultipleStatusCodes ? statusCode : ""
   }`.trim(),
-}: ReqResHandlingProps<$ZodType> & {
+}: ReqResCommons & {
+  schema: $ZodType;
+  composition: "inline" | "components";
+  description?: string;
+  brandHandling?: BrandHandling;
   mimeTypes: ReadonlyArray<string> | null;
   variant: ResponseVariant;
   statusCode: number;
   hasMultipleStatusCodes: boolean;
 }): ResponseObject => {
   if (!mimeTypes) return { description };
-  const depictedSchema = excludeExamplesFromDepiction(
+  const response = ensureCompliance(
     depict(schema, {
       rules: { ...brandHandling, ...depicters },
       ctx: { isResponse: true, makeRef, path, method },
     }),
   );
+  const examples = [];
+  if (isSchemaObject(response) && response.examples) {
+    examples.push(...response.examples);
+    delete response.examples; // moving them up
+  }
   const media: MediaTypeObject = {
     schema:
       composition === "components"
-        ? makeRef(schema, depictedSchema, makeCleanId(description))
-        : depictedSchema,
-    examples: depictExamples(schema, true),
+        ? makeRef(schema, response, makeCleanId(description))
+        : response,
+    examples: enumerateExamples(examples),
   };
   return { description, content: R.fromPairs(R.xprod(mimeTypes, [media])) };
 };
@@ -708,34 +699,62 @@ export const depictSecurityRefs = (
     }, {}),
   );
 
+export const depictRequest = ({
+  schema,
+  brandHandling,
+  makeRef,
+  path,
+  method,
+}: ReqResCommons & {
+  schema: IOSchema;
+  brandHandling?: BrandHandling;
+}) =>
+  depict(schema, {
+    rules: { ...brandHandling, ...depicters },
+    ctx: { isResponse: false, makeRef, path, method },
+  });
+
 export const depictBody = ({
   method,
   path,
   schema,
+  request,
   mimeType,
   makeRef,
   composition,
-  brandHandling,
   paramNames,
   description = `${method.toUpperCase()} ${path} Request body`,
-}: ReqResHandlingProps<IOSchema> & {
+}: ReqResCommons & {
+  schema: IOSchema;
+  composition: "inline" | "components";
+  description?: string;
+  request: JSONSchema.BaseSchema;
   mimeType: string;
   paramNames: string[];
 }) => {
   const [withoutParams, hasRequired] = excludeParamsFromDepiction(
-    depict(schema, {
-      rules: { ...brandHandling, ...depicters },
-      ctx: { isResponse: false, makeRef, path, method },
-    }),
+    ensureCompliance(request),
     paramNames,
   );
-  const bodyDepiction = excludeExamplesFromDepiction(withoutParams);
+  const examples = [];
+  if (isSchemaObject(withoutParams) && withoutParams.examples) {
+    examples.push(...withoutParams.examples);
+    delete withoutParams.examples; // pull up
+  }
   const media: MediaTypeObject = {
     schema:
       composition === "components"
-        ? makeRef(schema, bodyDepiction, makeCleanId(description))
-        : bodyDepiction,
-    examples: depictExamples(extractObjectSchema(schema), false, paramNames),
+        ? makeRef(schema, withoutParams, makeCleanId(description))
+        : withoutParams,
+    examples: enumerateExamples(
+      examples.length
+        ? examples
+        : flattenIO(request)
+            .examples.filter(
+              (one): one is FlatObject => isObject(one) && !Array.isArray(one),
+            )
+            .map(R.omit(paramNames)),
+    ),
   };
   const body: RequestBodyObject = {
     description,

express-zod-api/src/json-schema-helpers.ts
@@ -1,0 +1,68 @@
+import type { JSONSchema } from "@zod/core";
+import { combinations, isObject } from "./common-helpers";
+
+const isJsonObjectSchema = (
+  subject: JSONSchema.BaseSchema,
+): subject is JSONSchema.ObjectSchema => subject.type === "object";
+
+/** @todo DNRY with intersect() */
+export const flattenIO = (jsonSchema: JSONSchema.BaseSchema) => {
+  const stack = [{ entry: jsonSchema, isOptional: false }];
+  const flat: Required<
+    Pick<
+      JSONSchema.ObjectSchema,
+      "type" | "properties" | "required" | "examples"
+    >
+  > = {
+    type: "object",
+    properties: {},
+    required: [],
+    examples: [],
+  };
+  while (stack.length) {
+    const { entry, isOptional } = stack.shift()!;
+    if (entry.allOf)
+      stack.push(...entry.allOf.map((one) => ({ entry: one, isOptional })));
+    if (entry.anyOf) {
+      stack.push(
+        ...entry.anyOf.map((one) => ({ entry: one, isOptional: true })),
+      );
+    }
+    if (entry.oneOf) {
+      stack.push(
+        ...entry.oneOf.map((one) => ({ entry: one, isOptional: true })),
+      );
+    }
+    if (!isJsonObjectSchema(entry)) continue;
+    if (entry.properties) {
+      Object.assign(flat.properties, entry.properties);
+      if (!isOptional && entry.required) flat.required.push(...entry.required);
+    }
+    if (entry.examples) {
+      if (isOptional) {
+        flat.examples.push(...entry.examples);
+      } else {
+        flat.examples = combinations(
+          flat.examples.filter(isObject),
+          entry.examples.filter(isObject),
+          ([a, b]) => ({ ...a, ...b }),
+        );
+      }
+    }
+    if (entry.propertyNames) {
+      const keys: string[] = [];
+      if (typeof entry.propertyNames.const === "string")
+        keys.push(entry.propertyNames.const);
+      if (entry.propertyNames.enum) {
+        keys.push(
+          ...entry.propertyNames.enum.filter((one) => typeof one === "string"),
+        );
+      }
+      const value = { ...Object(entry.additionalProperties) }; // it can be bool
+      for (const key of keys) flat.properties[key] = value;
+      if (!isOptional) flat.required.push(...keys);
+    }
+  }
+  if (flat.required.length > 1) flat.required = [...new Set(flat.required)]; // drop duplicates
+  return flat;
+};

express-zod-api/tests/__snapshots__/documentation-helpers.spec.ts.snap
@@ -62,40 +62,6 @@ exports[`Documentation helpers > depictEnum() > should set type 1`] = `
 }
 `;
 
-exports[`Documentation helpers > depictExamples() > should 'pass' examples in case of 'request' 1`] = `
-{
-  "example1": {
-    "value": {
-      "one": "test",
-      "two": 123,
-    },
-  },
-  "example2": {
-    "value": {
-      "one": "test2",
-      "two": 456,
-    },
-  },
-}
-`;
-
-exports[`Documentation helpers > depictExamples() > should 'transform' examples in case of 'response' 1`] = `
-{
-  "example1": {
-    "value": {
-      "one": 4,
-      "two": "123",
-    },
-  },
-  "example2": {
-    "value": {
-      "one": 5,
-      "two": "456",
-    },
-  },
-}
-`;
-
 exports[`Documentation helpers > depictFile() > should set type:string and format accordingly 0 1`] = `
 {
   "format": "file",
@@ -253,14 +219,36 @@ exports[`Documentation helpers > depictNullable() > should not add null type whe
 }
 `;
 
-exports[`Documentation helpers > depictParamExamples() > should pass examples for the given parameter 1`] = `
+exports[`Documentation helpers > depictObject() > should remove optional props from required for request 0 1`] = `
 {
-  "example1": {
-    "value": 123,
+  "properties": {
+    "a": {
+      "type": "number",
+    },
+    "b": {
+      "type": "string",
+    },
   },
-  "example2": {
-    "value": 456,
+  "required": [
+    "a",
+    "b",
+  ],
+  "type": "object",
+}
+`;
+
+exports[`Documentation helpers > depictObject() > should remove optional props from required for request 1 1`] = `
+{
+  "properties": {
+    "a": {
+      "type": "number",
+    },
+    "b": {
+      "type": "string",
+    },
   },
+  "required": [],
+  "type": "object",
 }
 `;
 
@@ -280,6 +268,24 @@ exports[`Documentation helpers > depictRaw() > should extract the raw property 1
 {
   "format": "binary",
   "type": "string",
+}
+`;
+
+exports[`Documentation helpers > depictRequest > should simply delegate it all to Zod 4 1`] = `
+{
+  "properties": {
+    "id": {
+      "type": "string",
+    },
+    "test": {
+      "type": "boolean",
+    },
+  },
+  "required": [
+    "id",
+    "test",
+  ],
+  "type": "object",
 }
 `;
 
@@ -657,13 +663,6 @@ DocumentationError({
   "cause": "Response schema of an Endpoint assigned to GET method of /v1/user/:id path.",
   "message": "Please use ez.upload() only for input.",
 })
-`;
-
-exports[`Documentation helpers > excludeExamplesFromDepiction() > should remove example property of supplied object 1`] = `
-{
-  "description": "test",
-  "type": "string",
-}
 `;
 
 exports[`Documentation helpers > excludeParamsFromDepiction() > should handle the ReferenceObject 1`] = `

express-zod-api/tests/__snapshots__/documentation.spec.ts.snap
@@ -1291,7 +1291,7 @@ paths:
         content:
           application/json:
             schema:
-              $ref: "#/components/schemas/Schema2"
+              $ref: "#/components/schemas/Schema1"
       responses:
         "200":
           description: POST /v1/getSomething Positive response
@@ -1307,7 +1307,7 @@ paths:
                     type: object
                     properties:
                       zodExample:
-                        $ref: "#/components/schemas/Schema1"
+                        $ref: "#/components/schemas/Schema2"
                     required:
                       - zodExample
                 required:
@@ -3456,6 +3456,11 @@ paths:
               required:
                 - key
                 - str
+            examples:
+              example1:
+                value:
+                  key: 1234-56789-01
+                  str: test
         required: true
       responses:
         "200":
@@ -3755,6 +3760,8 @@ paths:
                           - "123"
                     required:
                       - numericStr
+                    examples:
+                      - numericStr: "123"
                 required:
                   - status
                   - data

express-zod-api/tests/__snapshots__/json-schema-helpers.spec.ts.snap
@@ -1,0 +1,102 @@
+// Vitest Snapshot v1, https://vitest.dev/guide/snapshot.html
+
+exports[`JSON Schema helpers > flattenIO() > should handle records 1`] = `
+{
+  "examples": [
+    {
+      "one": "test",
+      "two": "jest",
+    },
+    {
+      "one": "some",
+      "two": "another",
+    },
+    {
+      "four": 456,
+      "three": 123,
+    },
+  ],
+  "properties": {
+    "four": {
+      "type": "number",
+    },
+    "one": {
+      "type": "string",
+    },
+    "three": {
+      "type": "number",
+    },
+    "two": {
+      "type": "string",
+    },
+  },
+  "required": [],
+  "type": "object",
+}
+`;
+
+exports[`JSON Schema helpers > flattenIO() > should pass the object schema through 1`] = `
+{
+  "examples": [
+    {
+      "one": "test",
+    },
+  ],
+  "properties": {
+    "one": {
+      "type": "string",
+    },
+  },
+  "required": [
+    "one",
+  ],
+  "type": "object",
+}
+`;
+
+exports[`JSON Schema helpers > flattenIO() > should return object schema for the intersection of object schemas 1`] = `
+{
+  "examples": [
+    {
+      "one": "test",
+      "two": "jest",
+    },
+  ],
+  "properties": {
+    "one": {
+      "type": "string",
+    },
+    "two": {
+      "type": "number",
+    },
+  },
+  "required": [
+    "one",
+    "two",
+  ],
+  "type": "object",
+}
+`;
+
+exports[`JSON Schema helpers > flattenIO() > should return object schema for the union of object schemas 1`] = `
+{
+  "examples": [
+    {
+      "one": "test",
+    },
+    {
+      "two": "jest",
+    },
+  ],
+  "properties": {
+    "one": {
+      "type": "string",
+    },
+    "two": {
+      "type": "number",
+    },
+  },
+  "required": [],
+  "type": "object",
+}
+`;

commit ab19eb623edd76603002372091a2d68b15c0350a

Ref: Unify flatten and intersect (#2598)
Caused by #2595 
`flattenIO` and `intersect` are doing very similar things.
first one is coercive, last one is suggestive.
Seeking for a way to unify them into one

express-zod-api/src/documentation-helpers.ts
@@ -26,7 +26,6 @@ import * as R from "ramda";
 import { globalRegistry, z } from "zod";
 import { ResponseVariant } from "./api-response";
 import {
-  combinations,
   doesAccept,
   FlatObject,
   getExamples,
@@ -133,55 +132,11 @@ export const depictUnion: Depicter = ({ zodSchema, jsonSchema }) => {
   };
 };
 
-const propsMerger = (a: unknown, b: unknown) => {
-  if (Array.isArray(a) && Array.isArray(b)) return R.concat(a, b);
-  if (a === b) return b;
-  throw new Error("Can not flatten properties");
-};
-const approaches = {
-  type: R.always("object"),
-  properties: ({ properties: left = {} }, { properties: right = {} }) =>
-    R.mergeDeepWith(propsMerger, left, right),
-  required: ({ required: left = [] }, { required: right = [] }) =>
-    R.union(left, right),
-  examples: ({ examples: left = [] }, { examples: right = [] }) =>
-    combinations(left.filter(isObject), right.filter(isObject), ([a, b]) =>
-      R.mergeDeepRight({ ...a }, { ...b }),
-    ),
-  description: ({ description: left }, { description: right }) => left || right,
-} satisfies {
-  [K in keyof JSONSchema.ObjectSchema]: (
-    ...subj: JSONSchema.ObjectSchema[]
-  ) => JSONSchema.ObjectSchema[K];
-};
-const canMerge = R.pipe(
-  Object.keys,
-  R.without(Object.keys(approaches)),
-  R.isEmpty,
-);
-
-/** @todo DNRY with flattenIO() */
-const intersect = (
-  children: Array<JSONSchema.BaseSchema>,
-): JSONSchema.ObjectSchema => {
-  const [left, right] = children
-    .map(unref)
-    .filter(
-      (schema): schema is JSONSchema.ObjectSchema => schema.type === "object",
-    )
-    .filter(canMerge);
-  if (!left || !right) throw new Error("Can not flatten objects");
-  const suitable: typeof approaches = R.pickBy(
-    (_, prop) => (left[prop] || right[prop]) !== undefined,
-    approaches,
-  );
-  return R.map((fn) => fn(left, right), suitable);
-};
-
 export const depictIntersection = R.tryCatch<Depicter>(
   ({ jsonSchema }) => {
-    if (!jsonSchema.allOf) throw new Error("Missing allOf");
-    return intersect(jsonSchema.allOf);
+    if (!jsonSchema.allOf) throw "no allOf";
+    for (const entry of jsonSchema.allOf) unref(entry);
+    return flattenIO(jsonSchema, "throw");
   },
   (_err, { jsonSchema }) => jsonSchema,
 );
@@ -419,7 +374,7 @@ export const depictRequestParams = ({
         name,
         in: location,
         deprecated: jsonSchema.deprecated,
-        required: flat.required.includes(name),
+        required: flat.required?.includes(name) || false,
         description: depicted.description || description,
         schema: result,
         examples: enumerateExamples(
@@ -427,7 +382,7 @@ export const depictRequestParams = ({
             ? depicted.examples // own examples or from the flat:
             : R.pluck(
                 name,
-                flat.examples.filter(R.both(isObject, R.has(name))),
+                flat.examples?.filter(R.both(isObject, R.has(name))) || [],
               ),
         ),
       });
@@ -750,10 +705,10 @@ export const depictBody = ({
       examples.length
         ? examples
         : flattenIO(request)
-            .examples.filter(
+            .examples?.filter(
               (one): one is FlatObject => isObject(one) && !Array.isArray(one),
             )
-            .map(R.omit(paramNames)),
+            .map(R.omit(paramNames)) || [],
     ),
   };
   const body: RequestBodyObject = {

express-zod-api/src/json-schema-helpers.ts
@@ -1,28 +1,51 @@
 import type { JSONSchema } from "@zod/core";
+import * as R from "ramda";
 import { combinations, isObject } from "./common-helpers";
 
 const isJsonObjectSchema = (
   subject: JSONSchema.BaseSchema,
 ): subject is JSONSchema.ObjectSchema => subject.type === "object";
 
-/** @todo DNRY with intersect() */
-export const flattenIO = (jsonSchema: JSONSchema.BaseSchema) => {
+const propsMerger = R.mergeDeepWith((a: unknown, b: unknown) => {
+  if (Array.isArray(a) && Array.isArray(b)) return R.concat(a, b);
+  if (a === b) return b;
+  throw new Error("Can not flatten properties");
+});
+
+const canMerge = R.pipe(
+  Object.keys,
+  R.without([
+    "type",
+    "properties",
+    "required",
+    "examples",
+    "description",
+  ] satisfies Array<keyof JSONSchema.ObjectSchema>),
+  R.isEmpty,
+);
+
+export const flattenIO = (
+  jsonSchema: JSONSchema.BaseSchema,
+  mode: "coerce" | "throw" = "coerce",
+) => {
   const stack = [{ entry: jsonSchema, isOptional: false }];
-  const flat: Required<
-    Pick<
-      JSONSchema.ObjectSchema,
-      "type" | "properties" | "required" | "examples"
-    >
-  > = {
+  const flat: JSONSchema.ObjectSchema &
+    Required<Pick<JSONSchema.ObjectSchema, "properties">> = {
     type: "object",
     properties: {},
-    required: [],
-    examples: [],
   };
   while (stack.length) {
     const { entry, isOptional } = stack.shift()!;
-    if (entry.allOf)
-      stack.push(...entry.allOf.map((one) => ({ entry: one, isOptional })));
+    if (entry.description) flat.description ??= entry.description;
+    if (entry.allOf) {
+      stack.push(
+        ...entry.allOf.map((one) => {
+          if (mode === "throw" && !(one.type == "object" && canMerge(one)))
+            throw new Error("Can not merge");
+          return { entry: one, isOptional };
+        }),
+      );
+    }
     if (entry.anyOf) {
       stack.push(
         ...entry.anyOf.map((one) => ({ entry: one, isOptional: true })),
@@ -35,17 +58,21 @@ export const flattenIO = (jsonSchema: JSONSchema.BaseSchema) => {
     }
     if (!isJsonObjectSchema(entry)) continue;
     if (entry.properties) {
-      Object.assign(flat.properties, entry.properties);
-      if (!isOptional && entry.required) flat.required.push(...entry.required);
+      flat.properties = (mode === "throw" ? propsMerger : R.mergeDeepRight)(
+        flat.properties,
+        entry.properties,
+      );
+      if (!isOptional && entry.required?.length)
+        flat.required = R.union(flat.required || [], entry.required);
     }
-    if (entry.examples) {
+    if (entry.examples?.length) {
       if (isOptional) {
-        flat.examples.push(...entry.examples);
+        flat.examples = R.concat(flat.examples || [], entry.examples);
       } else {
         flat.examples = combinations(
-          flat.examples.filter(isObject),
+          flat.examples?.filter(isObject) || [],
           entry.examples.filter(isObject),
-          ([a, b]) => ({ ...a, ...b }),
+          ([a, b]) => R.mergeDeepRight(a, b),
         );
       }
     }
@@ -59,10 +86,9 @@ export const flattenIO = (jsonSchema: JSONSchema.BaseSchema) => {
         );
       }
       const value = { ...Object(entry.additionalProperties) }; // it can be bool
-      for (const key of keys) flat.properties[key] = value;
-      if (!isOptional) flat.required.push(...keys);
+      for (const key of keys) flat.properties[key] ??= value;
+      if (!isOptional) flat.required = R.union(flat.required || [], keys);
     }
   }
-  if (flat.required.length > 1) flat.required = [...new Set(flat.required)]; // drop duplicates
   return flat;
 };

express-zod-api/tests/__snapshots__/documentation-helpers.spec.ts.snap
@@ -154,6 +154,9 @@ exports[`Documentation helpers > depictIntersection() > should maintain uniquene
       "type": "number",
     },
   },
+  "required": [
+    "test",
+  ],
   "type": "object",
 }
 `;

express-zod-api/tests/__snapshots__/documentation.spec.ts.snap
@@ -400,7 +400,6 @@ paths:
             schema:
               type: object
               properties: {}
-              required: []
       security:
         - HTTP_1: []
           OAUTH2_1:
@@ -459,7 +458,6 @@ paths:
             schema:
               type: object
               properties: {}
-              required: []
       security:
         - HTTP_2: []
       responses:

express-zod-api/tests/__snapshots__/json-schema-helpers.spec.ts.snap
@@ -30,7 +30,6 @@ exports[`JSON Schema helpers > flattenIO() > should handle records 1`] = `
       "type": "string",
     },
   },
-  "required": [],
   "type": "object",
 }
 `;
@@ -96,7 +95,6 @@ exports[`JSON Schema helpers > flattenIO() > should return object schema for the
       "type": "number",
     },
   },
-  "required": [],
   "type": "object",
 }
 `;

express-zod-api/tests/documentation-helpers.spec.ts
@@ -254,8 +254,16 @@ describe("Documentation helpers", () => {
     test("should maintain uniqueness in the array of required props", () => {
       const jsonSchema: JSONSchema.BaseSchema = {
         allOf: [
-          { type: "object", properties: { test: { type: "number" } } },
-          { type: "object", properties: { test: { const: 5 } } },
+          {
+            type: "object",
+            properties: { test: { type: "number" } },
+            required: ["test"],
+          },
+          {
+            type: "object",
+            properties: { test: { const: 5 } },
+            required: ["test"],
+          },
         ],
       };
       expect(