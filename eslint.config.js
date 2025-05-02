e9e1f0c96aadedce5338f6e331d85fe10aa1a17d
Robin Tail <robin_tail@me.com>
2025-05-02 15:33:37 +0200

    Changing implementation to Set.

--- eslint.config.js
+++ eslint.config.js
@@ -50,7 +50,7 @@ const performanceConcerns = [
     message: "flatMap() is about 1.3x slower than R.chain()",
   },
   {
-    selector: "MemberExpression[object.name='R'] > Identifier[name='union']",
+    selector: "MemberExpression[object.name='R'] > Identifier[name='union']", // #2599
     message: "R.union() is 1.5x slower than [...Set().add()]",
   },
 ];
