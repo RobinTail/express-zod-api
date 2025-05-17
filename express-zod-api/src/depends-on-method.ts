9b024104fc2a9838178c8ce8db6905fcbfb8dd77
Robin Tail <robin_tail@me.com>
2025-05-17 08:57:18 +0200

    Ref: simpler DependsOnMethod::entries().

--- express-zod-api/src/depends-on-method.ts
+++ express-zod-api/src/depends-on-method.ts
@@ -13,17 +13,14 @@ export class DependsOnMethod extends Routable {
 
   /**
    * @desc [method, endpoint]
-   * @todo can make it simpler?
    * @internal
    * */
-  public get entries(): ReadonlyArray<[Method, AbstractEndpoint]> {
-    const entries: Array<(typeof this.entries)[number]> = [];
-    const methods = R.keys(this.#endpoints); // eslint-disable-line no-restricted-syntax -- literal type required
-    for (const method of methods) {
-      const endpoint = this.#endpoints[method];
-      if (endpoint) entries.push([method, endpoint]);
-    }
-    return Object.freeze(entries);
+  public get entries() {
+    const nonempty = R.filter(
+      (pair): pair is [Method, AbstractEndpoint] => Boolean(pair[1]),
+      Object.entries(this.#endpoints),
+    );
+    return Object.freeze(nonempty);
   }
 
   public override deprecated() {
