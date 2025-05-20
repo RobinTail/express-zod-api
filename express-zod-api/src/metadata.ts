6d3b713916715a17c4aa7e0824df29af7dd20c61
Robin Tail <robin_tail@me.com>
2025-05-20 10:30:55 +0200

    Restoring noop case for mixExamples.

--- express-zod-api/src/metadata.ts
+++ express-zod-api/src/metadata.ts
@@ -9,11 +9,12 @@ export const mixExamples = <A extends z.ZodType, B extends z.ZodType>(
   src: A,
   dest: B,
 ): B => {
-  const srcMeta = src.meta();
+  const srcExamples = src.meta()?.examples;
+  if (!srcExamples) return dest;
   const destMeta = dest.meta();
   const examples = combinations<z.output<A> & z.output<B>>(
     destMeta?.examples || [],
-    srcMeta?.examples ||
+    srcExamples ||
       (isSchema<$ZodObject>(src, "object") ? pullExampleProps(src) : []),
     ([destExample, srcExample]) =>
       typeof destExample === "object" &&
@@ -23,7 +24,7 @@ export const mixExamples = <A extends z.ZodType, B extends z.ZodType>(
         ? R.mergeDeepRight(destExample, srcExample)
         : srcExample, // not supposed to be called on non-object schemas
   );
-  return dest.meta({ ...destMeta, examples });
+  return dest.meta({ ...destMeta, examples }); // @todo might not be required to spread since .meta() does it now
 };
 
 export const getBrand = (subject: $ZodType) => {
