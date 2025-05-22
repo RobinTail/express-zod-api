76aead22896be071535e07816646c4be31b2010c
Robin Tail <robin_tail@me.com>
2025-05-22 11:20:52 +0200

    rm mixExamples, failsafe getBrand with tests.

--- express-zod-api/src/metadata.ts
+++ express-zod-api/src/metadata.ts
@@ -1,38 +1,9 @@
-import type { $ZodType, $ZodObject } from "zod/v4/core";
-import { combinations, isSchema, pullExampleProps } from "./common-helpers";
-import { z } from "zod/v4";
-import * as R from "ramda";
+import type { $ZodType } from "zod/v4/core";
 
 export const metaSymbol = Symbol.for("express-zod-api");
 
-/** @todo move or remove */
-export const mixExamples = <A extends z.ZodType, B extends z.ZodType>(
-  src: A,
-  dest: B,
-): B => {
-  const {
-    examples: srcExamples = isSchema<$ZodObject>(src, "object")
-      ? pullExampleProps(src)
-      : undefined,
-  } = src.meta() || {};
-  if (!srcExamples?.length) return dest;
-  const { examples: destExamples = [] } = dest.meta() || {};
-  const examples = combinations<z.output<A> & z.output<B>>(
-    destExamples,
-    srcExamples,
-    ([destExample, srcExample]) =>
-      typeof destExample === "object" &&
-      typeof srcExample === "object" &&
-      destExample &&
-      srcExample
-        ? R.mergeDeepRight(destExample, srcExample)
-        : srcExample, // not supposed to be called on non-object schemas
-  );
-  return dest.meta({ examples });
-};
-
 export const getBrand = (subject: $ZodType) => {
-  const { brand } = subject._zod.bag;
+  const { brand } = subject._zod.bag || {};
   if (
     typeof brand === "symbol" ||
     typeof brand === "string" ||
