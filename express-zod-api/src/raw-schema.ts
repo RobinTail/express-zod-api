a5e5740d22550cbfac3e4c7ef18c3c62b91444f2
Robin Tail <robin_tail@me.com>
2025-05-23 11:52:58 +0200

    Ref: DNRY for raw schema.

--- express-zod-api/src/raw-schema.ts
+++ express-zod-api/src/raw-schema.ts
@@ -5,12 +5,13 @@ import { file } from "./file-schema";
 export const ezRawBrand = Symbol("Raw");
 
 const base = z.object({ raw: file("buffer") });
+type Base = ReturnType<typeof base.brand<symbol>>;
 
 const extended = <S extends $ZodShape>(extra: S) =>
   base.extend(extra).brand(ezRawBrand as symbol);
 
 /** Shorthand for z.object({ raw: ez.file("buffer") }) */
-export function raw(): ReturnType<typeof base.brand<symbol>>;
+export function raw(): Base;
 export function raw<S extends $ZodShape>(
   extra: S,
 ): ReturnType<typeof extended<S>>;
@@ -18,4 +19,4 @@ export function raw(extra?: $ZodShape) {
   return extra ? extended(extra) : base.brand(ezRawBrand as symbol);
 }
 
-export type RawSchema = ReturnType<typeof base.brand<symbol>>;
+export type RawSchema = Base;
