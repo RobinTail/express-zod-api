b5ffcad6962b2e8a929d52f9597ee5ea153814eb
Robin Tail <robin_tail@me.com>
2025-05-26 08:46:17 +0200

    impr. redability in OutputValidationError::constructor.

--- express-zod-api/src/errors.ts
+++ express-zod-api/src/errors.ts
@@ -55,17 +55,13 @@ export class OutputValidationError extends IOSchemaError {
   public override name = "OutputValidationError";
 
   constructor(public override readonly cause: z.ZodError) {
-    super(
-      getMessageFromError(
-        new z.ZodError(
-          cause.issues.map(({ path, ...rest }) => ({
-            ...rest,
-            path: ["output", ...path],
-          })),
-        ),
-      ),
-      { cause },
+    const prefixedPath = new z.ZodError(
+      cause.issues.map(({ path, ...rest }) => ({
+        ...rest,
+        path: ["output", ...path],
+      })),
     );
+    super(getMessageFromError(prefixedPath), { cause });
   }
 }
 
