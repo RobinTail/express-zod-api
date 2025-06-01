d04fe9615e12bd5e41d42b801c232a5efe856413
Robin Tail <robin_tail@me.com>
2025-06-01 09:37:38 +0200

    Ref: simpler getExamples().

--- express-zod-api/src/metadata.ts
+++ express-zod-api/src/metadata.ts
@@ -1,6 +1,5 @@
 import { globalRegistry } from "zod/v4";
 import type { $ZodType } from "zod/v4/core";
-import { isObject } from "./common-helpers";
 
 export const metaSymbol = Symbol.for("express-zod-api");
 
@@ -21,8 +20,10 @@ export const getBrand = (subject: $ZodType) => {
  * */
 export const getExamples = (subject: $ZodType): ReadonlyArray<unknown> => {
   const { examples, example } = globalRegistry.get(subject) || {};
-  if (Array.isArray(examples)) return examples;
-  if (isObject(examples))
-    return Object.values(examples).map(({ value }) => value);
+  if (examples) {
+    return Array.isArray(examples)
+      ? examples
+      : Object.values(examples).map(({ value }) => value);
+  }
   return example === undefined ? [] : [example];
 };
