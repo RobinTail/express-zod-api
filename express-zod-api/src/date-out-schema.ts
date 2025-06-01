5de3fea7b888aa41e711dc49d59930bf4ce20900
Robin Tail <robin_tail@me.com>
2025-06-01 09:29:13 +0200

    Even simpler dateOut meta.

--- express-zod-api/src/date-out-schema.ts
+++ express-zod-api/src/date-out-schema.ts
@@ -2,12 +2,9 @@ import { z } from "zod/v4";
 
 export const ezDateOutBrand = Symbol("DateOut");
 
-export const dateOut = ({
-  examples,
-  ...rest
-}: Parameters<z.ZodString["meta"]>[0] = {}) =>
+export const dateOut = (meta: Parameters<z.ZodString["meta"]>[0] = {}) =>
   z
     .date()
     .transform((date) => date.toISOString())
     .brand(ezDateOutBrand as symbol)
-    .meta({ ...rest, examples: examples });
+    .meta(meta);
