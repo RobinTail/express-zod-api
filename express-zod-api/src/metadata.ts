c039d77cf77754869cdce0092bb380bfc99063d7
Robin Tail <robin_tail@me.com>
2025-06-01 08:12:36 +0200

    getExamples() metadata helper.

--- express-zod-api/src/metadata.ts
+++ express-zod-api/src/metadata.ts
@@ -1,4 +1,6 @@
+import { globalRegistry } from "zod/v4";
 import type { $ZodType } from "zod/v4/core";
+import { isObject } from "./common-helpers";
 
 export const metaSymbol = Symbol.for("express-zod-api");
 
@@ -11,4 +13,15 @@ export const getBrand = (subject: $ZodType) => {
   )
     return brand;
   return undefined;
+};
+
+/**
+ * @since zod 3.25.44
+ * @link https://github.com/colinhacks/zod/pull/4586
+ * */
+export const getExamples = (subject: $ZodType): ReadonlyArray<unknown> => {
+  const { examples = [], example } = globalRegistry.get(subject) || {};
+  if (Array.isArray(examples)) return examples;
+  if (isObject(examples)) return Object.values(examples);
+  return example === undefined ? [] : [example];
 };
