d4a85011a3ab24db0af03fc720cac1208df7792c
Anna Bocharova <anna.bocharova@grover.com>
2025-07-20 09:35:38 +0200

    minor: jsdoc for different kinds of methods.

--- express-zod-api/src/method.ts
+++ express-zod-api/src/method.ts
@@ -8,14 +8,26 @@ export const methods = [
   "patch",
 ] satisfies Array<keyof IRouter>;
 
+/**
+ * @desc Methods supported by the framework API to produce Endpoints on EndpointsFactory.
+ * @see BuildProps
+ * */
 export type Method = (typeof methods)[number];
 
+/**
+ * @desc Additional methods having some technical handling in the framework
+ * @see makeCorsHeaders
+ * */
 export type AuxMethod = Extract<keyof IRouter, "options" | "head">;
 
 export const clientMethods = [...methods, "head"] satisfies Array<
   Method | Extract<AuxMethod, "head">
 >;
 
+/**
+ * @desc Methods usable on the client side, available via generated Integration and Documentation
+ * @see withHead
+ * */
 export type ClientMethod = (typeof clientMethods)[number];
 
 export const isMethod = (subject: string): subject is Method =>
