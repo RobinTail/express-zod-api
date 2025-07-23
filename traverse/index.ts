76775aea4bd277fc13a7fb32dbc8f20dbb553aa8
Anna Bocharova <anna.bocharova@grover.com>
2025-07-23 22:50:51 +0200

    rm onEach - unused.

--- traverse/index.ts
+++ traverse/index.ts
@@ -4,44 +4,29 @@ export interface NextHandlerInc<U> {
   next: (schema: $ZodType) => U;
 }
 
-interface PrevInc<U> {
-  prev: U;
-}
-
 export type SchemaHandler<
   U,
   Context extends object,
-  Variant extends "regular" | "each" | "last" = "regular",
+  Variant extends "regular" | "last" = "regular",
 > = (
   schema: $ZodType,
-  ctx: Context &
-    (Variant extends "regular"
-      ? NextHandlerInc<U>
-      : Variant extends "each"
-        ? PrevInc<U>
-        : Context),
+  ctx: Context & (Variant extends "regular" ? NextHandlerInc<U> : Context),
 ) => U;
 
 export const walkSchema = <U extends object, Context extends object>(
   schema: $ZodType,
   {
-    onEach,
     getHandler,
     onMissing,
     ctx = {} as Context,
   }: {
     ctx?: Context;
-    onEach?: SchemaHandler<U, Context, "each">;
     getHandler: (schema: $ZodType) => SchemaHandler<U, Context> | undefined;
     onMissing: SchemaHandler<U, Context, "last">;
   },
 ): U => {
   const handler = getHandler(schema);
   const next = (subject: $ZodType) =>
-    walkSchema(subject, { ctx, onEach, getHandler, onMissing });
-  const result = handler
-    ? handler(schema, { ...ctx, next })
-    : onMissing(schema, ctx);
-  const overrides = onEach && onEach(schema, { prev: result, ...ctx });
-  return overrides ? { ...result, ...overrides } : result;
+    walkSchema(subject, { ctx, getHandler, onMissing });
+  return handler ? handler(schema, { ...ctx, next }) : onMissing(schema, ctx);
 };
