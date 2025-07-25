15e6d9819df9f3d802b970e81b405733a8a25c04
Anna Bocharova <anna.bocharova@grover.com>
2025-07-25 12:16:55 +0200

    Copy plugin code and mapping helpers.

---
+++ zod-plugin/mapping-helpers.ts
@@ -0,0 +1,20 @@
+/**
+ * @fileoverview Mapping utils for Zod Runtime Plugin (remap)
+ * @link https://stackoverflow.com/questions/55454125/typescript-remapping-object-properties-in-typesafe
+ */
+type TuplesFromObject<T> = {
+  [P in keyof T]: [P, T[P]];
+}[keyof T];
+
+type GetKeyByValue<T, V> =
+  TuplesFromObject<T> extends infer TT
+    ? TT extends [infer P, V]
+      ? P
+      : never
+    : never;
+
+export type Remap<T, U extends { [P in keyof T]?: V }, V extends string> = {
+  [P in NonNullable<U[keyof U]>]: T[GetKeyByValue<U, P>];
+};
+
+export type Intact<T, U> = { [K in Exclude<keyof T, keyof U>]: T[K] };
