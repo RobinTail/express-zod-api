1fa6501b3e5bcaddf06ea2a7931f0a6449ccdd5d
Anna Bocharova <anna.bocharova@grover.com>
2025-08-03 12:31:24 +0200

    Adjusting remapping case for IOSchema test.

--- express-zod-api/tests/io-schema.spec.ts
+++ express-zod-api/tests/io-schema.spec.ts
@@ -107,7 +107,10 @@ describe("I/O Schema and related helpers", () => {
             .pipe(z.object({ n: z.number() })),
         ).toExtend<IOSchema>();
         expectTypeOf(
-          z.object({ user_id: z.string() }).remap({ user_id: "userId" }),
+          z
+            .object({ user_id: z.string() })
+            .transform(({ user_id: userId }) => ({ userId }))
+            .pipe(z.object({ userId: z.string() })),
         ).toExtend<IOSchema>();
       });
       test("does not accept transformation to another type", () => {
