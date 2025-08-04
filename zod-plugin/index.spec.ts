f0b06c175fddff2a1a1bcf6fcc7ad05f550e81a8
Anna Bocharova <anna.bocharova@grover.com>
2025-08-04 20:12:42 +0200

    Using loose type assertions for the plugin entrypoint test (augmented twice).

--- zod-plugin/index.spec.ts
+++ zod-plugin/index.spec.ts
@@ -4,13 +4,13 @@ describe("Entrypoint", () => {
   test("Extended Zod prototypes", () => {
     expectTypeOf<z.ZodAny>()
       .toHaveProperty("example")
-      .toEqualTypeOf<(value: any) => z.ZodAny>();
+      .toExtend<(value: any) => z.ZodAny>();
     expectTypeOf<z.ZodDefault<z.ZodString>>()
       .toHaveProperty("example")
-      .toEqualTypeOf<(value: string) => z.ZodDefault<z.ZodString>>();
+      .toExtend<(value: string) => z.ZodDefault<z.ZodString>>();
     expectTypeOf<z.ZodDefault<z.ZodString>>()
       .toHaveProperty("label")
-      .toEqualTypeOf<(value: string) => z.ZodDefault<z.ZodString>>();
+      .toExtend<(value: string) => z.ZodDefault<z.ZodString>>();
     expectTypeOf<z.ZodObject>().toHaveProperty("remap");
   });
 });
