4956e05ade8a9e92a1e4c6049de4c5cff3605f76
Anna Bocharova <anna.bocharova@grover.com>
2025-08-10 12:41:26 +0200

    Traverse draft.

---
+++ zod-to-ts/traverse.ts
@@ -0,0 +1,41 @@
+import type { z } from "zod";
+
+export interface NextHandlerInc<U> {
+  next: (schema: z.core.$ZodType) => U;
+}
+
+export type SchemaHandler<
+  U,
+  Context extends object,
+  Variant extends "regular" | "last" = "regular",
+> = (
+  schema: any, // eslint-disable-line @typescript-eslint/no-explicit-any -- for assignment compatibility
+  ctx: Context & (Variant extends "regular" ? NextHandlerInc<U> : Context),
+) => U;
+
+export type HandlingRules<
+  U,
+  Context extends object,
+  K extends string | symbol = string | symbol,
+> = Partial<Record<K, SchemaHandler<U, Context>>>;
+
+export const traverse = <U extends object, Context extends object>(
+  schema: z.core.$ZodType,
+  {
+    rules,
+    onMissing,
+    ctx = {} as Context,
+    getRule = (one) => one._zod.def.type,
+  }: {
+    rules: HandlingRules<U, Context>;
+    onMissing: SchemaHandler<U, Context, "last">;
+    ctx?: Context;
+    /** @todo consider combining/replacing rules */
+    getRule?: (subject: typeof schema) => keyof typeof rules;
+  },
+): U => {
+  const handler = rules[getRule(schema)];
+  const next = (subject: z.core.$ZodType) =>
+    traverse(subject, { ctx, rules, onMissing });
+  return handler ? handler(schema, { ...ctx, next }) : onMissing(schema, ctx);
+};
