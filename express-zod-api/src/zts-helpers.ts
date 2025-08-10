79851045afe2c9a180349ec17b44eab64bf20340
Anna Bocharova <anna.bocharova@grover.com>
2025-08-10 21:04:49 +0200

    rm extension from zts context.

--- express-zod-api/src/zts-helpers.ts
+++ express-zod-api/src/zts-helpers.ts
@@ -1,8 +1,7 @@
 import type ts from "typescript";
-import { FlatObject } from "./common-helpers";
 import { SchemaHandler } from "@express-zod-api/zod-to-ts";
 
-export interface ZTSContext extends FlatObject {
+export interface ZTSContext {
   isResponse: boolean;
   makeAlias: (key: object, produce: () => ts.TypeNode) => ts.TypeNode;
 }
