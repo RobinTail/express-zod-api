9f0559e33aa0fe30a89d25187753adccaef5ed3a
Anna Bocharova <anna.bocharova@grover.com>
2025-10-04 19:51:56 +0200

    Reapply "Adjusting example workspace."
    This reverts commit 9c25776fa636c9768c88418f20d92eb6ca121901.

--- example/middlewares.ts
+++ example/middlewares.ts
@@ -1,7 +1,7 @@
 import createHttpError from "http-errors";
 import assert from "node:assert/strict";
 import { z } from "zod";
-import { Method, Middleware } from "express-zod-api";
+import { type Method, Middleware } from "express-zod-api";
 
 export const authMiddleware = new Middleware({
   security: {
