13b5558b5ecadd7007ed1aa2b0c1fdd1378601ff
Anna Bocharova <anna.bocharova@grover.com>
2025-11-24 09:48:44 +0100

    Draft solution: Patch CJS package of zod distribution.

--- zod-plugin/src/runtime.ts
+++ zod-plugin/src/runtime.ts
@@ -1,6 +1,7 @@
 import { globalRegistry, z } from "zod";
 import { setBrand } from "./brand";
 import { remap } from "./remap";
+import { createRequire } from "node:module";
 
 const exampleSetter = function (this: z.ZodType, value: z.output<typeof this>) {
   const examples = globalRegistry.get(this)?.examples?.slice() || [];
@@ -21,37 +22,42 @@ const pluginFlag = Symbol.for(process.env.TSDOWN_SELF!);
 
 if (!(pluginFlag in globalThis)) {
   (globalThis as Record<symbol, unknown>)[pluginFlag] = true;
-  for (const entry of Object.keys(z)) {
-    if (!entry.startsWith("Zod")) continue;
-    if (/(Success|Error|Function)$/.test(entry)) continue;
-    const Cls = z[entry as keyof typeof z];
-    if (typeof Cls !== "function") continue;
-    Object.defineProperties(Cls.prototype, {
-      ["example" satisfies keyof z.ZodType]: {
-        value: exampleSetter,
-        writable: false,
-      },
-      ["deprecated" satisfies keyof z.ZodType]: {
-        value: deprecationSetter,
-        writable: false,
-      },
-      ["brand" satisfies keyof z.ZodType]: {
-        set() {}, // this is required to override the existing method
-        get() {
-          return setBrand.bind(this) as z.ZodType["brand"];
+  const packages = [z];
+  const { z: zCJS } = createRequire(import.meta.url)("zod") as { z: typeof z };
+  if (z !== zCJS) packages.push(zCJS);
+  for (const pkg of packages) {
+    for (const entry of Object.keys(pkg)) {
+      if (!entry.startsWith("Zod")) continue;
+      if (/(Success|Error|Function)$/.test(entry)) continue;
+      const Cls = pkg[entry as keyof typeof pkg];
+      if (typeof Cls !== "function") continue;
+      Object.defineProperties(Cls.prototype, {
+        ["example" satisfies keyof z.ZodType]: {
+          value: exampleSetter,
+          writable: false,
         },
-      },
-    });
-  }
+        ["deprecated" satisfies keyof z.ZodType]: {
+          value: deprecationSetter,
+          writable: false,
+        },
+        ["brand" satisfies keyof z.ZodType]: {
+          set() {}, // this is required to override the existing method
+          get() {
+            return setBrand.bind(this) as z.ZodType["brand"];
+          },
+        },
+      });
+    }
 
-  Object.defineProperty(
-    z.ZodDefault.prototype,
-    "label" satisfies keyof z.ZodDefault,
-    { value: labelSetter, writable: false },
-  );
-  Object.defineProperty(
-    z.ZodObject.prototype,
-    "remap" satisfies keyof z.ZodObject,
-    { value: remap, writable: false },
-  );
+    Object.defineProperty(
+      pkg.ZodDefault.prototype,
+      "label" satisfies keyof z.ZodDefault,
+      { value: labelSetter, writable: false },
+    );
+    Object.defineProperty(
+      pkg.ZodObject.prototype,
+      "remap" satisfies keyof z.ZodObject,
+      { value: remap, writable: false },
+    );
+  }
 }
