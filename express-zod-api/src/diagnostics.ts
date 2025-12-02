c4d2619ac9ed2974ff1a3122b0f7322d23912fd7
Anna Bocharova <anna.bocharova@grover.com>
2025-12-02 19:25:35 +0100

    Restoring original shortcut to avoid expensive path params check.

--- express-zod-api/src/diagnostics.ts
+++ express-zod-api/src/diagnostics.ts
@@ -75,23 +75,22 @@ export class Diagnostics {
     // Path params check
     if (ref.paths.includes(path)) return;
     const params = getRoutePathParams(path);
-    if (params.length > 0) {
-      const flat =
-        ref.flat ||
-        flattenIO(
-          z.toJSONSchema(endpoint.inputSchema, {
-            unrepresentable: "any",
-            io: "input",
-          }),
-        );
-      ref.flat = flat;
-      for (const param of params) {
-        if (param in flat.properties) continue;
-        this.logger.warn(
-          "The input schema of the endpoint is most likely missing the parameter of the path it's assigned to.",
-          Object.assign(ctx, { path, param }),
-        );
-      }
+    if (params.length === 0) return; // next statement can be expensive
+    const flat =
+      ref.flat ||
+      flattenIO(
+        z.toJSONSchema(endpoint.inputSchema, {
+          unrepresentable: "any",
+          io: "input",
+        }),
+      );
+    ref.flat = flat;
+    for (const param of params) {
+      if (param in flat.properties) continue;
+      this.logger.warn(
+        "The input schema of the endpoint is most likely missing the parameter of the path it's assigned to.",
+        Object.assign(ctx, { path, param }),
+      );
     }
     ref.paths.push(path);
   }
