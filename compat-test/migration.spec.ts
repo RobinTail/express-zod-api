b5ccb20aa8334562e5fadbbdebe1a6734a9be099
Robin Tail <robin_tail@me.com>
2025-12-28 06:45:54 +0100

    Fix compat test (lint).

--- compat-test/migration.spec.ts
+++ compat-test/migration.spec.ts
@@ -4,6 +4,8 @@ import { describe, test, expect } from "vitest";
 describe("Migration", () => {
   test("should fix the import", async () => {
     const fixed = await readFile("./sample.ts", "utf-8");
-    expect(fixed).toBe(`import typescript from "typescript";\n\nnew Integration({ typescript, routing });\n`);
+    expect(fixed).toBe(
+      `import typescript from "typescript";\n\nnew Integration({ typescript, routing });\n`,
+    );
   });
 });
