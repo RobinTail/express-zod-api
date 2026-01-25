904b56c51c254c6c2387b83999ee7f6a8f0e8a11
Robin Tail <robin_tail@me.com>
2026-01-25 11:48:26 +0100

    feat: custom plugin to make DTS more human readable.

--- express-zod-api/tsdown.config.ts
+++ express-zod-api/tsdown.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "tsdown";
 import manifest from "./package.json" with { type: "json" };
+import { format } from "prettier";
 
 export default defineConfig({
   entry: "src/index.ts",
@@ -7,6 +8,29 @@ export default defineConfig({
   minify: true,
   attw: { profile: "esm-only", level: "error" },
   external: ["express-serve-static-core", "qs"],
+  plugins: [
+    {
+      name: "human-readable-dts-rolldown-plugin",
+      generateBundle: async (_opt, bundle) => {
+        for (const [name, file] of Object.entries(bundle)) {
+          if (name.endsWith(".d.ts") && "code" in file) {
+            // ensure newlines around jsdoc comments
+            file.code = file.code.replaceAll(
+              /(\/\*\*[^\r\n]*?\*\/)/g,
+              "\n$1\n",
+            );
+            // rm double newlines
+            file.code = file.code.replaceAll(/\n\s*\n/g, "\n");
+            // ensure readable formatting
+            file.code = await format(file.code, {
+              parser: "typescript",
+              printWidth: 120,
+            });
+          }
+        }
+      },
+    },
+  ],
   define: {
     "process.env.TSDOWN_SELF": `"${manifest.name}"`, // used by localsID
     "process.env.TSDOWN_BUILD": `"v${manifest.version}"`, // @since v25.0.0 is pure ESM
