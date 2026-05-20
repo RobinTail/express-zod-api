bbde2f57b9bef097a298fb02ef3cc76dbd6416e6
Robin Tail <robin_tail@me.com>
2026-05-20 10:56:32 +0200

    fix(example): using scrypt instead of hash sha1.

--- example/endpoints/login.ts
+++ example/endpoints/login.ts
@@ -1,8 +1,9 @@
 import { cookieAssistedFactory } from "../factories.ts";
 import { z } from "zod";
-import { randomUUID, hash } from "node:crypto";
+import { randomUUID, scrypt } from "node:crypto";
 import createHttpError from "http-errors";
 import assert from "node:assert/strict";
+import { promisify } from "node:util";
 
 /** @desc The endpoint demonstrates setting a cookie */
 export const loginEndpoint = cookieAssistedFactory.build({
@@ -14,10 +15,14 @@ export const loginEndpoint = cookieAssistedFactory.build({
   }),
   output: z.object({ message: z.string() }),
   handler: async ({ input: { username, password }, ctx: { setCookie } }) => {
+    const key = await promisify<string, string, number, Buffer>(scrypt)(
+      password,
+      "kinda salt",
+      16,
+    );
     assert(
       username === "admin" &&
-        password &&
-        hash("sha1", password) === "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3",
+        key.toString("hex") === "79ad19b8c03bc92a2f25ed865400264e",
       createHttpError(401, "Invalid credentials"),
     );
     setCookie("session", { token: randomUUID() });
