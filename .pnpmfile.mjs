58226c39d5de339ad048a037697da64251636f21
Robin Tail <robin_tail@me.com>
2026-05-27 13:54:28 +0200

    fix(CI): add whole babel scope to the patch.

--- .pnpmfile.mjs
+++ .pnpmfile.mjs
@@ -2,14 +2,18 @@
  * Babel v8 changed their engines compatibility in rc5 from 24.0.0 to 24.11.0 with no good reason
  * @todo remove this hack in next major
  */
-function readPackage(pkg, context) {
+function readPackage(pkg, ctx) {
   // Target the specific breaking version of babel types
-  if (pkg.name === "@babel/types" && pkg.version.startsWith("8.0.0")) {
+  if (
+    pkg.name &&
+    pkg.name.startsWith("@babel/") &&
+    pkg.version.startsWith("8.0.0")
+  ) {
     pkg.engines = {
       ...pkg.engines,
       node: "^22.18.0 || >=24.0.0", // Force compatibility
     };
-    context.log(`Patched ${pkg.name}@${pkg.version}.`);
+    ctx.log(`Patched ${pkg.name}@${pkg.version}.`);
   }
   return pkg;
 }
