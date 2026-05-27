a46c062b7c70655a42a198c617c4ebb156dcf735
Robin Tail <robin_tail@me.com>
2026-05-27 13:45:41 +0200

    fix: restore v22 compat as is.

--- .pnpmfile.mjs
+++ .pnpmfile.mjs
@@ -1,5 +1,5 @@
 /**
- * Babel v8 changed their engines in rc5 from 24.0.0 to 24.11.0
+ * Babel v8 changed their engines compatibility in rc5 from 24.0.0 to 24.11.0 with no good reason
  * @todo remove this hack in next major
  */
 function readPackage(pkg, context) {
@@ -7,7 +7,7 @@ function readPackage(pkg, context) {
   if (pkg.name === "@babel/types" && pkg.version.startsWith("8.0.0")) {
     pkg.engines = {
       ...pkg.engines,
-      node: ">=24.0.0", // Force compatibility
+      node: "^22.18.0 || >=24.0.0", // Force compatibility
     };
     context.log(`Patched ${pkg.name}@${pkg.version}.`);
   }
