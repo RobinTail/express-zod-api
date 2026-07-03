991b629df5bd9c58aedc6a9bf59ea76adf2bcd99
Robin Tail <robin_tail@me.com>
2026-07-03 09:03:51 +0200

    fix(todo): Add PR URL.

--- express-zod-api/tests/common-helpers.spec.ts
+++ express-zod-api/tests/common-helpers.spec.ts
@@ -167,7 +167,7 @@ describe("Common Helpers", () => {
           makeRequestMock({
             query: { q: "search" },
             body: { filters: "active" },
-            /** @todo rm type casting when node-mocks-http supports QUERY method officially */
+            /** @todo rm when merged: https://github.com/eugef/node-mocks-http/pull/335 */
             method: "QUERY" as "GET",
           }),
           undefined,
