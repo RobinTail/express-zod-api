469f7e19db47ecfff484ddf047dc4396f15af9c0
Robin Tail <robin_tail@me.com>
2026-07-31 12:09:08 +0200

    fix(test): updating benchmark to confirm no much overhead when no params.

--- express-zod-api/bench/normalize-params.bench.ts
+++ express-zod-api/bench/normalize-params.bench.ts
@@ -8,13 +8,25 @@ const normalizeParams = (path: string) => {
 };
 
 describe("Experiment for checkDuplicate normalization", () => {
+  const pathNoParams = "/users/check/some/stuff";
   const pathWithParams = "/users/:userId/books/:bookId/pages/:pageNum";
   const method = "get" as const;
   const visitedWith = new Set<string>();
   const visitedWithout = new Set<string>();
 
-  bench("with normalization", () => {
-    const normalized = normalizeParams(pathWithParams);
+  bench("with normalization and params", () => {
+    const normalized = pathWithParams.includes(":")
+      ? normalizeParams(pathWithParams)
+      : pathWithParams;
+    const key = `${method} ${normalized}`;
+    void visitedWith.has(key);
+    visitedWith.add(key);
+  });
+
+  bench("with normalization no params", () => {
+    const normalized = pathNoParams.includes(":")
+      ? normalizeParams(pathNoParams)
+      : pathNoParams;
     const key = `${method} ${normalized}`;
     void visitedWith.has(key);
     visitedWith.add(key);
