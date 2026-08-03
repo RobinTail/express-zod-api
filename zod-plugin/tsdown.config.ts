2ce2ee80c8dd7336a8b8d6ba3ac60f2f514d94a8
Robin Tail <robin_tail@me.com>
2026-08-03 11:15:08 +0200

    fix(CI): inline dts plugin to config.

--- zod-plugin/tsdown.config.ts
+++ zod-plugin/tsdown.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from "tsdown";
 import manifest from "./package.json" with { type: "json" };
 import { fixDtsPlugin } from "../tools/fixDts.ts";
 
-const plugins = [fixDtsPlugin()];
-
 export default defineConfig({
   entry: "src/index.ts",
   fixedExtension: false,
   minify: true,
   attw: { profile: "esm-only", level: "error" },
-  plugins,
+  plugins: [fixDtsPlugin()],
   define: {
     "process.env.TSDOWN_SELF": `"${manifest.name}"`, // used by pluginFlag
   },
