44109d62c31508a3c9b10ddd220515ad8429e87f
Robin Tail <robin_tail@me.com>
2026-08-06 06:36:35 +0200

    Removing eslint, adjusting migration dependencies.

--- migration/index.spec.ts
+++ migration/index.spec.ts
@@ -1,5 +1,5 @@
 import { RuleTester } from "@typescript-eslint/rule-tester";
-import { parser } from "typescript-eslint";
+import parser from "@typescript-eslint/parser";
 import manifest from "./package.json";
 import assert from "node:assert/strict";
 
