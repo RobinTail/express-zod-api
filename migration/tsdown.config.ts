e93c3495bdda3461c55456f2d2e7eff2231a219f
Robin Tail <robin_tail@me.com>
2026-08-18 10:29:56 +0200

    fix(CI): removing the DTS fixing tool completely, OK now.

--- migration/tsdown.config.ts
+++ migration/tsdown.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "tsdown";
 import manifest from "./package.json" with { type: "json" };
-import { fixDtsPlugin } from "../tools/fixDts.ts";
 
 declare global {
   interface ImportMeta {
@@ -21,7 +20,6 @@ export default defineConfig({
     tsconfig: "./tsconfig.build.json",
   },
   attw: { profile: "esm-only", level: "error" },
-  plugins: [fixDtsPlugin()],
   define: {
     "import.meta.TSDOWN_VERSION": `"${manifest.version}"`, // used by ruleName
   },
