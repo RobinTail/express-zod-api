cd1d2dfe638947e2b3987bca5d6d90429b0871ae
Robin Tail <robin_tail@me.com>
2026-09-02 06:30:48 +0200

    fix(jdsoc): minor wording correction.

--- express-zod-api/src/config-type.ts
+++ express-zod-api/src/config-type.ts
@@ -92,7 +92,7 @@ export interface CommonConfig {
   /**
    * @desc Attempts using .parse() method first, falling back to .parseAsync() if fails due to async refinements.
    * @desc In that case, async refinements and transformations execute twice, so avoid side effects in them.
-   * @example true — parses sync schemas ~1.3x faster, but it's ~14x slower if it occasionally hits an async one.
+   * @example true — parses sync schemas ~1.3x faster, but it retries async ones (first request to the endpoint only).
    * @example false — always uses .parseAsync() and does not support compiled schemas (legacy behavior).
    * @default false
    * @todo remove in v30 or enable by default
