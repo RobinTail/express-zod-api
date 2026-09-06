0cbcf52a67538e20ccf67e45ac481c3fc7f297ae
Robin Tail <robin_tail@me.com>
2026-09-06 10:34:09 +0200

    feat(factory): Add flag forwarding.

--- express-zod-api/src/io-schema.ts
+++ express-zod-api/src/io-schema.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { compileOnce } from "./common-helpers";
+import { asyncProperty, isAsync } from "./metadata.ts";
 
 type Base = object & { [Symbol.iterator]?: never };
 
@@ -24,10 +25,11 @@ export const ensureExtension = <
   current: Current,
   inc: Inc,
 ) =>
-  (current && inc ? current.and(inc) : current || inc) as Extension<
-    Current,
-    Inc
-  >;
+  (current && inc
+    ? current
+        .and(inc)
+        .meta({ [asyncProperty]: isAsync(inc) || isAsync(current) })
+    : current || inc) as Extension<Current, Inc>;
 
 /** The Endpoint input schema type, condition wrapped into schema to make it z.output-compatible */
 export type FinalInputSchema<
@@ -44,5 +46,9 @@ export const makeFinalInputSchema = <
   buildSchema: BIN,
 ) =>
   compileOnce(
-    factorySchema ? factorySchema.and(buildSchema) : buildSchema,
+    factorySchema
+      ? factorySchema.and(buildSchema).meta({
+          [asyncProperty]: isAsync(buildSchema) || isAsync(factorySchema),
+        })
+      : buildSchema,
   ) as FinalInputSchema<FIN, BIN>;
--- express-zod-api/tests/endpoint.spec.ts
+++ express-zod-api/tests/endpoint.spec.ts
@@ -548,7 +548,11 @@ describe("Endpoint", () => {
               str: "This is fine",
             });
             expect(mwRefinement).toHaveBeenCalledTimes(
-              configProps.trySyncValidation ? 4 : 2, // @todo explicit should impact
+              configProps.trySyncValidation
+                ? variant === "explicit"
+                  ? 3
+                  : 4
+                : 2,
             );
             expect(inputRefinement).toHaveBeenCalledTimes(1);
             expect(outputRefinement).toHaveBeenCalledTimes(
@@ -556,7 +560,11 @@ describe("Endpoint", () => {
             );
             await attempt();
             expect(mwRefinement).toHaveBeenCalledTimes(
-              configProps.trySyncValidation ? 6 : 4,
+              configProps.trySyncValidation
+                ? variant === "explicit"
+                  ? 5
+                  : 6
+                : 4,
             );
             expect(inputRefinement).toHaveBeenCalledTimes(2);
             expect(outputRefinement).toHaveBeenCalledTimes(
